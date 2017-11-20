261e2d5840d3b0509b2caab864ce6ca493286b6c
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-11-20 15:47:31 -0500

    allow overriding the api

--- src/common/injectGlobals.ts
+++ src/common/injectGlobals.ts
@@ -1,7 +1,15 @@
 import 'index.css';
+import queryString from 'querystring';
 
 localStorage.setItem('debug', process.env.REACT_APP_DEBUG || ''); // manually set because CRA doesn't allow arbitrary env variable names.
 const debug = require('debug') as Function;
 global.log = debug('app');
 
-export const apiRoot = process.env.REACT_APP_API;
+const qs = queryString.parse(global.location.search.replace(/^\?/, ''));
+const apiOverride = qs.APP_API as string;
+
+if (apiOverride) {
+  global.log('warning: using api override');
+}
+
+export const apiRoot: string = apiOverride || process.env.REACT_APP_API || '';
