5a0f653b99181a20558dc141f73a8618cbf04dfe
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2018-02-01 17:20:35 -0500

    add arranger
    update schema

--- src/components/FileRepo.js
+++ src/components/FileRepo.js
@@ -2,6 +2,9 @@ import * as React from 'react';
 import { compose } from 'recompose';
 import { injectState } from 'freactal';
 
+import Arranger, { Portal } from '@arranger/components/dist/Arranger';
+import '@arranger/components/public/themeStyles/beagle/beagle.css';
+
 const enhance = compose(injectState);
 
 const FileRepo = ({ state, effects }) => {
@@ -17,6 +20,18 @@ const FileRepo = ({ state, effects }) => {
         )}
       </pre>
       {!state.loggedInUser && <div>Seeing files as annon</div>}
+      <Arranger
+        index="models"
+        projectId="testing4"
+        render={props => {
+          return (
+            <div style={{ display: 'flex' }}>
+              <Portal {...props} style={{ flexGrow: 1 }} />
+              <div style={{ width: 100, flex: 'none' }}>sidebar</div>
+            </div>
+          );
+        }}
+      />
     </div>
   );
 };
