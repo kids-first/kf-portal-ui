130327be416a2e9231ba56fde21d7737e9bf19db
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2018-02-12 12:42:38 -0500

    ✨ add arranger (#35)
    * add arranger
    
    update schema
    
    * point to kf index
    
    * add redirects
    
    * update for new version
    
    * update components version
    
    * update arranger components

--- src/components/FileRepo.js
+++ src/components/FileRepo.js
@@ -2,6 +2,14 @@ import * as React from 'react';
 import { compose } from 'recompose';
 import { injectState } from 'freactal';
 
+import Arranger, {
+  Aggregations,
+  CurrentSQON,
+  Table,
+  DetectNewVersion,
+} from '@arranger/components/dist/Arranger';
+import '@arranger/components/public/themeStyles/beagle/beagle.css';
+
 const enhance = compose(injectState);
 
 const FileRepo = ({ state, effects }) => {
@@ -17,6 +25,32 @@ const FileRepo = ({ state, effects }) => {
         )}
       </pre>
       {!state.loggedInUser && <div>Seeing files as annon</div>}
+      <Arranger
+        index="file"
+        projectId="jan31"
+        render={props => {
+          return (
+            <div>
+              <DetectNewVersion {...props} />
+              <div style={{ display: 'flex' }}>
+                <Aggregations {...props} />
+                <div
+                  style={{
+                    position: 'relative',
+                    flexGrow: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                  }}
+                >
+                  <CurrentSQON {...props} />
+                  <Table {...props} />
+                </div>
+                <div style={{ width: 100, flex: 'none' }}>sidebar</div>
+              </div>
+            </div>
+          );
+        }}
+      />
     </div>
   );
 };
