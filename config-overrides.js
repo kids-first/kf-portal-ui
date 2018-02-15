5e96f95f5e072c6c9724388ca67864707a6ed657
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2018-02-15 17:05:32 -0500

    ⬆️ update @arranger/components (#53)
    * ⬆️ update @arranger/components
    
    * include some node_modules in bable

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
 const { injectBabelPlugin } = require('react-app-rewired');
 
-function rewireEmotion(config) {
+function rewireEmotion(config, env) {
+  config.module.rules[1].oneOf[1].include = [
+    config.module.rules[1].oneOf[1].include,
+    resolveApp('node_modules/graphql-fields'),
+    resolveApp('node_modules/streamsaver'),
+  ];
+
   return injectBabelPlugin('emotion', config);
 }
 
