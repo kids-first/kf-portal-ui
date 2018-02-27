7c46c78a9a1a759b26a87db1e118779d8b0ce042
Jon Eubank <joneubank@gmail.com>
2018-02-27 13:12:24 -0500

    State updates fixed for modifying integrationTokens

--- src/stateProviders/provideLoggedInUser.js
+++ src/stateProviders/provideLoggedInUser.js
@@ -74,7 +74,7 @@ export default provideState({
           delete state.integrationTokens[service];
         }
       }
-      return state;
+      return { ...state, integrationTokens: { ...state.integrationTokens } };
     },
     clearIntegrationTokens: (effects) => state => {
       SERVICES.forEach(service =>
