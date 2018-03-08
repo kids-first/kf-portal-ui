db1cc46e5329e9f8db07174f4f6011e2590fafc2
Jon Eubank <joneubank@gmail.com>
2018-03-08 12:56:24 -0500

    Create Project flow complete

--- src/components/cavatica/CavaticaConnectButton.js
+++ src/components/cavatica/CavaticaConnectButton.js
@@ -11,40 +11,28 @@ const enhance = compose(injectState);
 
 const CavaticaConnectButton = ({ state, effects, ...props }) => {
   return (
-    <button
-      css={`
-        border-radius: 19px;
-        background-color: #ffffff;
-        border: solid 1px #cacbcf;
-        font-size: 11px;
-        letter-spacing: 0.2px;
-        color: #008199;
-        padding: 5px 18px 5px 5px;
-        display: flex;
-        align-items: center;
-
-        :hover {
-          cursor: pointer;
+    <div css={props.styles}>
+      <button
+        className="niceWhiteButton"
+        onClick={() =>
+          effects.setModal({
+            title: 'How to Connect to Cavatica',
+            component: (
+              <CavaticaInput onComplete={effects.unsetModal} onCancel={effects.unsetModal} />
+            ),
+          })
         }
-      `}
-      onClick={() =>
-        effects.setModal({
-          title: 'How to Connect to Cavatica',
-          component: (
-            <CavaticaInput onComplete={effects.unsetModal} onCancel={effects.unsetModal} />
-          ),
-        })
-      }
-    >
-      <img
-        alt=""
-        src={cavaticaLogo}
-        css={`
-          width: 28px;
-          margin-right: 7px;
-        `}
-      />Export files to Cavatica
-    </button>
+      >
+        <img
+          alt=""
+          src={cavaticaLogo}
+          css={`
+            width: 28px;
+            margin-right: 7px;
+          `}
+        />Export files to Cavatica
+      </button>
+    </div>
   );
 };
 
