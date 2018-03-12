520e5fe898429af71e5640a24a3f3582c4dd9590
Alex <lepsalex@gmail.com>
2018-03-12 13:54:45 -0400

    Points 1 through 8 of Github Issue

--- src/components/UserDashboard/MySavedQueries.js
+++ src/components/UserDashboard/MySavedQueries.js
@@ -39,122 +39,154 @@ const MySavedQueries = ({ loggedInUser }) => (
           />
         </div>
       ) : (
-        <div
-          css={`
+          <div
+            css={`
             display: flex;
             flex-direction: column;
             margin-top: 15px;
-            flex: 2;
-            border-top: 6px solid #41a7d5;
+            flex: 3;
+            border: 1px solid #e0e1e6;
+            border-top: 0;
+            border-bottom-right-radius: 10px;
+            border-bottom-left-radius: 10px;
+            padding: 0 10px;
           `}
-        >
-          <div
-            css={`
+          >
+            <div css={`
+              display: block;
+              width: calc(100% + 22px);
+              margin-left: -11px;
+              height: 6px;
+              background-image: linear-gradient(to right, #404c9a, #009bb8 51%, #02b0ed);
+            `} />
+            <div
+              css={`
               display: flex;
               flex-grow: 1;
               padding: 10px 20px;
               margin-bottom: 20px;
             `}
-          >
-            <StyledH3
-              css={`
+            >
+              <StyledH3
+                css={`
                 margin-top: 6px;
                 font-weight: 300;
               `}
-            >
-              My Saved Queries
+              >
+                Saved Queries
             </StyledH3>
-            <div
-              css={`
+              <div
+                css={`
                 margin-left: auto;
                 align-items: end;
                 display: flex;
                 line-height: 27px;
               `}
-            >
-              <span
-                css={`
+              >
+                <span
+                  css={`
                   margin-top: -2px;
                 `}
-              >
-                <SaveIcon />
-              </span>
-              <span
-                css={`
+                >
+                  <SaveIcon />
+                </span>
+                <span
+                  css={`
                   padding: 0 5px;
                   font-size: 22px;
                 `}
-              >
-                {state.queries.length}
-              </span>
-              <span>Queries</span>
-            </div>
-          </div>
-          <div
-            css={`
-              overflow: auto;
-              margin-bottom: 30px;
-            `}
-          >
-            {state.queries
-              .filter(q => q.alias)
-              .map(q => ({
-                ...q,
-                date: +new Date(q.creationDate),
-                // TODO: save origin + pathname separately in dynamo
-                link: `/search${q.content.longUrl.split('/search')[1]}`,
-              }))
-              .slice()
-              .sort((a, b) => b.date - a.date)
-              .map(q => (
-                <div
-                  key={q.id}
-                  css={`
-                    height: 75px;
-                    display: flex;
-                    padding: 10px;
-                  `}
                 >
+                  {state.queries.length}
+                </span>
+                <span>Queries</span>
+              </div>
+            </div>
+            {state.queries.length > 0 ?
+              <div css={`
+              display: block;
+              width: 100%;
+              height: 1px;
+              background-image: linear-gradient(to right, #a9adc0 10%, rgba(255, 255, 255, 0) 0%);
+              background-position: top;
+              background-size: 5px 1px;
+              background-repeat: repeat-x;
+            `} /> : null}
+            <div
+              css={`
+              overflow: auto;
+              margin: 6px 0 16px;
+            `}
+            >
+              {state.queries
+                .filter(q => q.alias)
+                .map(q => ({
+                  ...q,
+                  date: +new Date(q.creationDate),
+                  // TODO: save origin + pathname separately in dynamo
+                  link: `/search${q.content.longUrl.split('/search')[1]}`,
+                }))
+                .slice()
+                .sort((a, b) => b.date - a.date)
+                .map(q => (
                   <div
+                    key={q.id}
                     css={`
+                    display: flex;
+                    padding: 10px 10px 10px 25px;
+                    border: 1px solid #e0e1e6;
+                    border-bottom: 0;
+
+                    &:last-child {
+                      border-bottom: 1px solid #e0e1e6;
+                    }
+                  `}
+                  >
+                    <div
+                      css={`
                       display: flex;
                       flex-direction: column;
                     `}
-                  >
-                    <Link
-                      to={q.link}
-                      css={`
-                        color: purple;
+                    >
+                      <Link
+                        to={q.link}
+                        css={`
+                        font-size: 0.875em;
+                        color: #a42c90;
                         font-weight: bold;
                       `}
-                    >
-                      {q.alias}
-                    </Link>
-                    <div
-                      css={`
+                      >
+                        {q.alias}
+                      </Link>
+                      <div
+                        css={`
                         margin: 10px 0;
-                        color: #6f6f6f;
-                        font-size: 0.8em;
+                        color: #74757d;
+                        font-size: 0.75em;
                         letter-spacing: 0.3px;
+
+                        span {
+                          color: #343434;
+                        }
                       `}
-                    >
-                      {q.content.Participants.toLocaleString()} Participants |{' '}
-                      {q.content.Families.toLocaleString()} Families |{' '}
-                      {q.content.Files.toLocaleString()} Files | {q.content.Size}
-                    </div>
-                    <div
-                      css={`
-                        font-size: 0.8em;
+                      >
+                        <span>{q.content.Files.toLocaleString()}</span> Files |{' '}
+                        <span>{q.content.Participants.toLocaleString()}</span> Participants |{' '}
+                        <span>{q.content.Families.toLocaleString()}</span> Families |{' '}
+                        <span>{q.content.Size}</span>
+                      </div>
+                      <div
+                        css={`
+                        font-size: 0.75em;
                       `}
-                    >
-                      Saved {distanceInWords(new Date(), new Date(q.creationDate))}
+                      >
+                        Saved {distanceInWords(new Date(), new Date(q.creationDate))}
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
+            </div>
           </div>
-        </div>
-      )
+        )
     }
   />
 );
