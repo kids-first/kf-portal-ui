de5c45b3ce5b499d5631b4c651c13501afe3709e
Alex Wilmer <djazium@gmail.com>
2018-03-12 19:51:11 -0400

    🔥 remove service worker to test share url cache fix (#216)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { getAppElement } from './services/globalDomNodes.js';
 
 ReactDOM.render(<App />, getAppElement());
-registerServiceWorker();
