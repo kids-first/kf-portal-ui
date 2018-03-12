675a6835b17b0e5503cb43d5192c2189fa6d111f
Christine Yu <cyu@olm-loaner3858.local>
2018-03-12 13:25:12 -0400

    179 hidden delete all saved queries by id

--- src/components/loginButtons/DeleteButton.js
+++ src/components/loginButtons/DeleteButton.js
@@ -4,6 +4,8 @@ import { deleteProfile } from 'services/profiles';
 import { injectState } from 'freactal';
 import { compose } from 'recompose';
 import { uiLogout } from 'components/LogoutButton';
+import { shortUrlApi } from 'common/injectGlobals';
+import urlJoin from 'url-join';
 
 export default compose(injectState, withRouter)(
   ({
@@ -18,6 +20,20 @@ export default compose(injectState, withRouter)(
       className={className}
       onClick={async () => {
         await deleteProfile({ user: loggedInUser });
+
+        let response = await fetch(urlJoin(shortUrlApi, 'user', loggedInUser.egoId)).then(r =>
+          r.json(),
+        );
+
+        response.value.forEach(async ({ id }) => {
+          await fetch(urlJoin(shortUrlApi, id), {
+            method: 'DELETE',
+            headers: {
+              'Content-type': 'application/json',
+            },
+          });
+        });
+
         uiLogout({ history, setUser, setToken, clearIntegrationTokens });
       }}
       {...props}
