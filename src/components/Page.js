ac69639e5f60c724633fe504d89a2ff25d39faac
Alex Wilmer <djazium@gmail.com>
2018-04-05 11:53:02 -0400

    ♻️ cleanup App.js (#158)
    * ♻️ cleanup App.js
    
    * .

---
+++ src/components/Page.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+
+const Page = ({
+  Head = Header,
+  Foot = Footer,
+  Component,
+  backgroundImageUrl,
+  containerStyle,
+  ...props
+}) => (
+  <div
+    css={`
+      position: relative;
+      height: 100vh;
+      min-width: 1024px;
+      background-image: url(${backgroundImageUrl});
+      ${containerStyle};
+    `}
+  >
+    <div
+      css={`
+        background-repeat: repeat;
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        background-image: linear-gradient(to bottom, #fff 400px, transparent 100%);
+      `}
+    >
+      <Head />
+      <Component {...props} />
+      <Foot />
+    </div>
+  </div>
+);
+
+export default Page;
