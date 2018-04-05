ec67fd1981da2a6dde18965936545313e960e849
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2018-04-05 11:15:41 -0400

    ✨ update completion circle (#253)
    * ✨ update completion circle
    
    * back to css
    
    * remove unused variables
    
    * :wrench: fixing merge conflicts

--- src/components/CompleteOMeter.js
+++ src/components/CompleteOMeter.js
@@ -1,77 +1,37 @@
 import * as React from 'react';
-import { css } from 'react-emotion';
 import { compose } from 'recompose';
 import { withTheme } from 'emotion-theming';
-import DonutChart from 'react-svg-donut-chart';
-import { get } from 'lodash';
 
-import { ROLES } from 'common/constants';
-
-const CompleteOMeter = compose(withTheme)(({ theme, role, percentage, className }) => (
+const CompleteOMeter = compose(withTheme)(({ theme, percentage, className }) => (
   <div
     css={`
-      width: 310px;
+      background: #f4f5f8;
+      height: 100%;
       ${theme.column};
+      justify-content: center;
       align-items: center;
+      line-height: 1;
       ${className};
     `}
   >
-    <div
+    <span
       css={`
-        margin-left: -20px;
+        color: ${theme.active};
+        font-size: 24px;
+        font-weight: 500;
       `}
     >
-      <div
-        className={css`
-          ${theme.column} color: ${theme.active};
-          background: #f4f5f8;
-          text-align: center;
-          justify-content: center;
-          height: 116px;
-          width: 116px;
-          border-radius: 50%;
-        `}
-      >
-        {get(ROLES.reduce((acc, { type, icon }) => ({ ...acc, [type]: icon }), {}), role, () => {})(
-          {
-            height: '27px',
-            fill: theme.active,
-          },
-        )}
-        <span
-          css={`
-            padding: 8px 0;
-            font-size: 24px;
-            font-weight: 500px;
-            line-height: 0.75;
-          `}
-        >
-          {(percentage * 100).toFixed(0)}%
-        </span>
-        <span
-          css={`
-            font-size: 12px;
-            font-weight: 300;
-          `}
-        >
-          Complete
-        </span>{' '}
-      </div>
-      <div
-        className={css`
-          width: 166px;
-          margin-top: -137px;
-          margin-left: -23px;
-        `}
-      >
-        <DonutChart
-          data={[
-            { value: 100 - percentage * 100, stroke: '#cdd0d9' },
-            { value: percentage * 100, stroke: theme.highlight },
-          ]}
-        />
-      </div>
-    </div>
+      {Math.trunc(percentage * 100)}%
+    </span>
+    <span
+      css={`
+        color: #74757d;
+        font-size: 12px;
+        font-weight: 300;
+      `}
+    >
+      Complete
+    </span>
   </div>
 ));
 
