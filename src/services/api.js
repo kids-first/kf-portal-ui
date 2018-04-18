9255beb577e86e2afeb175b4c905556dee6d756e
Minh <hlminh2000@gmail.com>
2018-04-18 14:17:31 -0400

    custom url for api

--- src/services/api.js
+++ src/services/api.js
@@ -2,16 +2,22 @@ import { arrangerApiRoot } from 'common/injectGlobals';
 import urlJoin from 'url-join';
 import ajax from './ajax';
 
-const initializeApi = ({ onUnauthorized }) => ({ endpoint = '', body, headers }) => {
-  const url = urlJoin(arrangerApiRoot, endpoint);
+const initializeApi = ({ onUnauthorized }) => ({
+  method = 'post',
+  endpoint = '',
+  body,
+  headers,
+  url,
+}) => {
+  const uri = url || urlJoin(arrangerApiRoot, endpoint);
 
-  return ajax
-    .post(url, body)
+  return ajax[method.toLowerCase()](uri, body)
     .then(response => {
       console.log('response: ', response);
       return response.data;
     })
     .catch(({ response }) => {
+      console.log('error response: ', response);
       if (response.status === 401) {
         return onUnauthorized(response);
       }
