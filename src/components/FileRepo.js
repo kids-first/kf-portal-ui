f3f27b46792b9e8e5750183c066b7dd09589ce77
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-04-20 17:27:37 -0400

    :art: move upload ids button to its own component

--- src/components/FileRepo.js
+++ src/components/FileRepo.js
@@ -58,6 +58,72 @@ const arrangerStyles = css`
   }
 `;
 
+const UploadIdsButton = ({ theme, state, effects, setSQON, ...props }) => (
+  <div
+    className={css`
+      display: flex;
+      justify-content: flex-end;
+      margin-top: 10px;
+    `}
+  >
+    <LightButton
+      className={css`
+        border-top-right-radius: 0px;
+        border-bottom-right-radius: 0px;
+      `}
+      onClick={() =>
+        effects.setModal({
+          title: 'Upload a List of Identifiers',
+          component: <UploadIdsModal {...{ ...props, setSQON }} closeModal={effects.unsetModal} />,
+        })
+      }
+    >
+      <Trans css={theme.uppercase}>Upload Ids</Trans>
+    </LightButton>
+    <Select
+      className={css`
+        border-top-left-radius: 0px;
+        border-bottom-left-radius: 0px;
+        border-left: none;
+        padding-left: 0;
+      `}
+      align="right"
+      items={state.loggedInUser.sets.map(x => x.setId)}
+      itemContainerClassName={css`
+        padding: 0px;
+        box-shadow: 1px 1px 1px 0px rgba(0, 0, 0, 0.43);
+      `}
+      itemClassName={css`
+        &:hover {
+          background-color: ${theme.optionSelected};
+        }
+      `}
+      onChange={(setId, { clearSelection }) => {
+        if (setId) {
+          setSQON(
+            toggleSQON(
+              {
+                op: 'and',
+                content: [
+                  {
+                    op: 'in',
+                    content: {
+                      field: 'kf_id',
+                      value: [`set_id:${setId}`],
+                    },
+                  },
+                ],
+              },
+              props.sqon,
+            ),
+          );
+        }
+        clearSelection();
+      }}
+    />
+  </div>
+);
+
 const AggregationsWrapper = enhance(({ state, effects, theme, setSQON, ...props }) => {
   return (
     <div
@@ -128,70 +194,7 @@ const AggregationsWrapper = enhance(({ state, effects, theme, setSQON, ...props 
             />
           }
         />
-        <div
-          className={css`
-            display: flex;
-            justify-content: flex-end;
-            margin-top: 10px;
-          `}
-        >
-          <LightButton
-            className={css`
-              border-top-right-radius: 0px;
-              border-bottom-right-radius: 0px;
-            `}
-            onClick={() =>
-              effects.setModal({
-                title: 'Upload a List of Identifiers',
-                component: (
-                  <UploadIdsModal {...{ ...props, setSQON }} closeModal={effects.unsetModal} />
-                ),
-              })
-            }
-          >
-            <Trans css={theme.uppercase}>Upload Ids</Trans>
-          </LightButton>
-          <Select
-            className={css`
-              border-top-left-radius: 0px;
-              border-bottom-left-radius: 0px;
-              border-left: none;
-              padding-left: 0;
-            `}
-            align="right"
-            items={state.loggedInUser.sets.map(x => x.setId)}
-            itemContainerClassName={css`
-              padding: 0px;
-            `}
-            itemClassName={css`
-              &:hover {
-                background-color: ${theme.optionSelected};
-              }
-            `}
-            onChange={(setId, { clearSelection }) => {
-              if (setId) {
-                setSQON(
-                  toggleSQON(
-                    {
-                      op: 'and',
-                      content: [
-                        {
-                          op: 'in',
-                          content: {
-                            field: 'kf_id',
-                            value: [`set_id:${setId}`],
-                          },
-                        },
-                      ],
-                    },
-                    props.sqon,
-                  ),
-                );
-              }
-              clearSelection();
-            }}
-          />
-        </div>
+        <UploadIdsButton {...{ theme, effects, state, setSQON, ...props }} />
       </div>
       <Aggregations {...{ ...props, setSQON }} />
     </div>
