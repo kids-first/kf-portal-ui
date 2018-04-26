6a0f99aea8635b82d43b010af348895feb5e9d9a
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-04-26 13:37:10 -0400

    :bug: handles non stringy values

--- src/common/translateSQONValue.js
+++ src/common/translateSQONValue.js
@@ -1,5 +1,5 @@
 export default ({ sets = [], defaultSetText = 'Saved Manifest' }) => value => {
-  if (value.includes('set_id:')) {
+  if (`${value}`.includes('set_id:')) {
     const setId = value.replace('set_id:', '');
     const set = sets.find(x => x.setId === setId);
     return (set || {}).name || defaultSetText;
