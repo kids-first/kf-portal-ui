8fa6b6ad55fea76fc9ed9d1335776e0262466fc2
Minh Ha <hlminh2000@gmail.com>
2018-05-01 13:54:13 -0400

    🐛 adds and implements default ajax headers (#326)

--- src/services/ajax.js
+++ src/services/ajax.js
@@ -10,9 +10,10 @@ ajax.interceptors.request.use(
   config => {
     // set Authorization headers on a per request basis
     // setting headers on axios get/put/post or common seems to be shared accross all axios instances
-    config.headers['Authorization'] = `Bearer ${
-      config.url.startsWith(gen3ApiRoot) ? gen3Token : token
-    }`;
+    config.headers = {
+      ...config.headers,
+      'Authorization': `Bearer ${config.url.startsWith(gen3ApiRoot) ? gen3Token : token}`,
+    };
     return config;
   },
   err => {
