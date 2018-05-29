95002f79181fcab5207a273ccec32f30ffcbaaa9
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-05-29 11:24:14 -0400

    :bug: previous version was not resolving the promise after load

--- src/services/facebookSDK.js
+++ src/services/facebookSDK.js
@@ -1,8 +1,10 @@
+import scriptjs from 'scriptjs';
+
 import { facebookAppId } from 'common/injectGlobals';
 
-const facebookSDK = async () => {
-  return new Promise((resolve, reject) => {
-    global.fbAsyncInit = function() {
+const facebookSDK = () =>
+  new Promise((resolve, reject) =>
+    scriptjs(`https://connect.facebook.net/en_US/sdk.js`, () => {
       global.FB.init({
         appId: facebookAppId,
         autoLogAppEvents: true,
@@ -10,20 +12,7 @@ const facebookSDK = async () => {
         version: 'v2.10',
       });
       resolve(global.FB);
-    };
-
-    (function(d: any, s: any, id: any) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {
-        return;
-      }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = 'https://connect.facebook.net/en_US/sdk.js';
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, 'script', 'facebook-jssdk');
-  });
-};
+    }),
+  );
 
 export default facebookSDK;
