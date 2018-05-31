b97bfe4bafa122e510f6e160b67ca92b5f19fa51
Minh <hlminh2000@gmail.com>
2018-05-31 14:29:18 -0400

    reuses Row

--- src/components/ShareSaveQuery/ui.js
+++ src/components/ShareSaveQuery/ui.js
@@ -1,10 +1,9 @@
 import styled from 'react-emotion';
 import { LightButton } from 'uikit/Button';
+import Row from 'uikit/Row';
 
-export const ButtonContainer = styled('div')`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+export const ButtonContainer = styled(Row)`
+  ${({ theme }) => theme.center};
   padding: 10px;
 `;
 
