49c5c28e913eca6fcdd0fd764513c67c80261024
Minh <hlminh2000@gmail.com>
2018-05-31 10:38:45 -0400

    afv style cleanup

--- src/components/AdvancedFacetViewModal/index.js
+++ src/components/AdvancedFacetViewModal/index.js
@@ -1,13 +1,45 @@
 import * as React from 'react';
+import styled from 'react-emotion';
 import { injectState } from 'freactal';
 import { compose } from 'recompose';
 
 import { ModalFooter } from '../Modal/index.js';
 import { AdvancedFacetView } from '@arranger/components/dist/Arranger';
 import { provideLocalSqon } from 'stateProviders';
-import './style.css';
 
 const enhance = compose(provideLocalSqon, injectState);
+
+const AfvContainer = styled('div')`
+  flex: 1;
+  display: flex;
+  position: relative;
+
+  * {
+    box-sizing: border-box;
+  }
+
+  .advancedFacetViewWrapper {
+    padding: 0px;
+  }
+  .advancedFacetViewWrapper .facetViewWrapper .treeViewPanel {
+    border-bottom: none;
+  }
+  .afvModalContent {
+    position: absolute;
+    width: 100%;
+  }
+
+  .afvStatContainer .statContainer {
+    background: none;
+    padding: none;
+    border: none;
+  }
+
+  .afvStatContainer .statContainer .stat {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+`;
 
 class AdvancedFacetViewModalContent extends React.Component {
   onOverlayClick = e => {
@@ -27,22 +59,13 @@ class AdvancedFacetViewModalContent extends React.Component {
     } = this.props;
     return (
       <React.Fragment>
-        <div
-          className={`afvModalContent`}
-          css={`
-            flex: 1;
-            display: flex;
-            height: calc(100% - 175px);
-          `}
-        >
-          <div style={{ position: 'relative', flex: 1 }}>
-            <AdvancedFacetView
-              {...props}
-              sqon={localSqon}
-              onSqonChange={({ sqon }) => effects.setAdvancedFacetSqon(sqon)}
-            />
-          </div>
-        </div>
+        <AfvContainer className={`afvModalContent`}>
+          <AdvancedFacetView
+            {...props}
+            sqon={localSqon}
+            onSqonChange={({ sqon }) => effects.setAdvancedFacetSqon(sqon)}
+          />
+        </AfvContainer>
         <ModalFooter
           {...{
             unsetModal: closeModal,
