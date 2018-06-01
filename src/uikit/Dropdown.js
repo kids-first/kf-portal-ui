498f71952a0cd275fb4ed0359f975c7709f5403d
Minh <hlminh2000@gmail.com>
2018-06-01 13:13:51 -0400

    uses row and column components

--- src/uikit/Dropdown.js
+++ src/uikit/Dropdown.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import Downshift from 'downshift';
 import styled from 'react-emotion';
+import Column from 'uikit/Column';
+import Row from 'uikit/Row';
 
 import downChevronIcon from '../assets/icon-chevron-down-grey.svg';
 
-export const DropdownContainer = styled('div')`
+export const DropdownContainer = styled(Row)`
   position: relative;
   white-space: nowrap;
   z-index: 100;
   border-radius: 10px;
-  background-color: #ffffff;
-  border: solid 1px #cacbcf;
-  color: #343434;
+  background-color: ${({ theme }) => theme.white};
+  border: solid 1px ${({ theme }) => theme.borderGrey};
+  color: ${({ theme }) => theme.greyScale1};
   font-size: 12px;
-  display: flex;
   align-items: center;
   padding-left: 10px;
 `;
@@ -23,8 +24,7 @@ const ItemWrapper = styled('div')`
   padding: 10px;
 `;
 
-const DropdownLabelContainer = styled('div')`
-  display: flex;
+const DropdownLabelContainer = styled(Row)`
   cursor: pointer;
   flex-grow: 1;
   height: 100%;
@@ -32,9 +32,9 @@ const DropdownLabelContainer = styled('div')`
   justify-content: space-between;
 `;
 
-const DropdownOptionsContainer = styled('div')`
+const DropdownOptionsContainer = styled(Column)`
   position: absolute;
-  background: white;
+  background: ${({ theme }) => theme.white};
   min-width: 100%;
   z-index: 1;
   cursor: pointer;
@@ -42,9 +42,7 @@ const DropdownOptionsContainer = styled('div')`
   right: ${({ align }) => (align === 'right' ? 0 : 'auto')};
   left: ${({ align }) => (align === 'right' ? 'auto' : 0)};
   top: 100%;
-  box-shadow: 0 0 4.9px 0.1px #bbbbbb;
-  display: flex;
-  flex-direction: column;
+  box-shadow: 0 0 4.9px 0.1px ${({ theme }) => theme.shadow};
   border-radius: 2px;
 
   &:after, &:before {
@@ -60,13 +58,13 @@ const DropdownOptionsContainer = styled('div')`
 
   &:after {
     border-color: rgba(255, 255, 255, 0);
-    border-bottom-color: #ffffff;
+    border-bottom-color: ${({ theme }) => theme.white};
     border-width: 6px;
     margin-left: -6px;
   }
   &:before {
     border-color: rgba(0, 0, 0, 0);
-    border-bottom-color: #bbb;
+    border-bottom-color: ${({ theme }) => theme.borderGrey};
     border-width: 7px;
     margin-left: -7px;
   }
