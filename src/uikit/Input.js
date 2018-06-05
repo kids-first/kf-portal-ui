a43649105bdcc9fa44c2e513ebf26217c808a01f
Minh <hlminh2000@gmail.com>
2018-06-05 17:33:16 -0400

    simulates event dispatch from browser api

--- src/uikit/Input.js
+++ src/uikit/Input.js
@@ -1,47 +1,59 @@
 import React from 'react';
 import styled from 'react-emotion';
-import { withTheme } from 'emotion-theming';
 import Row from 'uikit/Row';
 import SearchIcon from 'react-icons/lib/fa/search';
 import FaTimesCircleO from 'react-icons/lib/fa/times-circle';
 
 // this this component should implement the default <input> type from react-dom with some additional
 // props for styling
-export const FilterInput = withTheme(
-  ({
-    theme,
-    LeftIcon = SearchIcon,
-    RightIcon = FaTimesCircleO,
-    className,
-    value,
-    disabled,
-    ...props
-  }) => {
-    const Wrapper = styled(Row)`
-      ${({ theme }) => theme.input};
-      color: ${({ theme }) => theme.greyScale7};
-      align-items: center;
-      .icon-left {
-        margin-right: 5px;
-      }
-      .icon-right {
-        cursor: pointer;
-      }
-      input {
-        border: none;
-        flex: 1;
-      }
-    `;
-    const clearInput = () => props.onChange({ target: { value: '' } });
-    return (
-      <Wrapper disabled={disabled} className={className}>
-        <LeftIcon className={'icon-left'} />
-        <input {...{ value, disabled, ...props }} autoFocus />
-        {value && value.length && <RightIcon className={'icon-right'} onClick={clearInput} />}
-      </Wrapper>
-    );
-  },
-);
+const FilterInputWrapper = styled(Row)`
+  ${({ theme }) => theme.input};
+  color: ${({ theme }) => theme.greyScale7};
+  align-items: center;
+  .icon-left {
+    margin-right: 5px;
+  }
+  .icon-right {
+    cursor: pointer;
+  }
+  input {
+    border: none;
+    flex: 1;
+  }
+`;
+const setNativeValue = (element, value) => {
+  const valueSetter = Object.getOwnPropertyDescriptor(element, 'value').set;
+  const prototype = Object.getPrototypeOf(element);
+  const prototypeValueSetter = Object.getOwnPropertyDescriptor(prototype, 'value').set;
+
+  if (valueSetter && valueSetter !== prototypeValueSetter) {
+    prototypeValueSetter.call(element, value);
+  } else {
+    valueSetter.call(element, value);
+  }
+};
+export const FilterInput = ({
+  LeftIcon = SearchIcon,
+  RightIcon = FaTimesCircleO,
+  className,
+  value,
+  disabled,
+  ref = React.createRef(),
+  ...props
+}) => {
+  const clearInput = () => {
+    const input = ref.current;
+    setNativeValue(input, '');
+    input.dispatchEvent(new Event('input', { bubbles: true }));
+  };
+  return (
+    <FilterInputWrapper disabled={disabled} className={className}>
+      <LeftIcon className={'icon-left'} />
+      <input {...{ value, disabled, ...props }} ref={ref} autoFocus />
+      {value && value.length && <RightIcon className={'icon-right'} onClick={clearInput} />}
+    </FilterInputWrapper>
+  );
+};
 
 export default styled('input')`
   ${({ theme }) => theme.input};
