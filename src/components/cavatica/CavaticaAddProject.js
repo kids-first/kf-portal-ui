3f6543d4574ef3626519f8e54ed860189021da03
Minh Ha <hlminh2000@gmail.com>
2018-06-06 11:57:40 -0400

    🐛 Fix/update download id field (#454)
    * updates download gen3 id
    
    * styled components
    
    * improve the text input label
    
    * uses kf_id instead of uuid
    
    * latest_did
    
    * brings in styled-system
    
    * simplifies passing InputComponent

--- src/components/cavatica/CavaticaAddProject.js
+++ src/components/cavatica/CavaticaAddProject.js
@@ -1,14 +1,55 @@
-import * as React from 'react';
+import React, { Fragment } from 'react';
 import { compose, withState } from 'recompose';
 import { withTheme } from 'emotion-theming';
-import { css } from 'react-emotion';
+import styled from 'react-emotion';
 
+import Row from 'uikit/Row';
+import Input from 'uikit/Input';
+import { ActionButton } from 'uikit/Button';
+import NiceWhiteButton from 'uikit/NiceWhiteButton';
+import { CancelButton } from 'components/Modal/ui';
 import { injectState } from 'freactal';
-
 import { createProject, getBillingGroups } from 'services/cavatica';
 import LoadingOnClick from 'components/LoadingOnClick';
-
 import PlusIcon from 'icons/PlusCircleIcon';
+
+const Container = styled(Row)`
+  align-items: center;
+  flex: 1;
+  & > *:not(:first-child) {
+    margin-left: 10px;
+  }
+`;
+
+const CreateButton = styled(NiceWhiteButton)`
+  text-transform: uppercase;
+  font-size: 11.5px;
+  font-weight: normal;
+  color: ${({ theme }) => theme.tertiary};
+  border-radius: 19px;
+  padding: 10px;
+  ${({ theme }) => theme.row};
+  cursor: pointer;
+`;
+
+const InputLabel = styled(Row)`
+  font-size: 12px;
+  line-height: 2.39;
+  align-items: center;
+  color: ${({ theme }) => theme.tertiary};
+  text-transform: uppercase;
+  &::after {
+    margin-left: 13px;
+    font-size: 1.5rem;
+    content: '›';
+    margin-top: -4px;
+  }
+`;
+
+const AddIcon = styled(PlusIcon)`
+  margin-top: 1px;
+  margin-right: 4px;
+`;
 
 const enhance = compose(
   injectState,
@@ -21,41 +62,10 @@ const saveProject = async ({ projectName, onSuccess }) => {
   const billingGroups = await getBillingGroups();
   if (billingGroups && billingGroups.length > 0) {
     const groupId = billingGroups[0].id;
-
     createProject({ billing_group: groupId, name: projectName }).then(response => onSuccess());
   }
 };
 
-const styles = theme => css`
-  ${theme.row};
-
-  .inputLabel {
-    font-size:12px;
-    line-height:2.39
-    color: ${theme.tertiary};
-    text-transform: uppercase;
-  }
-
-  .projectNameInput {
-    font-size: 12px;
-    font-style: italic;
-    margin-left: 10px;
-  }
-
-  .saveButton {
-    ${theme.actionButton};
-    font-size: 11px;
-    line-height: 2.36;
-    letter-spacing: 0.2px;
-    text-transform: uppercase;
-  }
-
-  .cancelText {
-    font-size:12px;
-    color: ${theme.tertiary};
-    cursor: pointer;
-  }
-`;
 const CavaticaAddProject = ({
   state,
   theme,
@@ -64,73 +74,45 @@ const CavaticaAddProject = ({
   addingProject,
   setAddingProject,
   ...props
-}) => {
-  return (
-    <div css={styles(theme)}>
-      {addingProject ? (
-        <div css={theme.row}>
-          <PlusIcon
-            width={12}
-            height={12}
-            fill={theme.tertiary}
-            css={`
-              margin-top: 1px;
-              margin-right: 4px;
-            `}
-          />
-          <div className="inputLabel">Create a project</div>
-          <input
-            className="projectNameInput"
-            type="text"
-            placeholder="Enter name of project"
-            value={projectName}
-            onChange={e => setProjectName(e.target.value)}
-          />
-          <LoadingOnClick
-            onClick={async () => {
-              await saveProject({ projectName, onSuccess: props.onSuccess });
-              setAddingProject(false);
-              setProjectName('');
-            }}
-            render={({ loading, onClick }) => (
-              <button className="saveButton" disabled={loading} onClick={onClick}>
-                <span>Save</span>
-              </button>
-            )}
-          />
-          <a className="cancelText" onClick={() => setAddingProject(false)}>
-            Cancel
-          </a>
-        </div>
-      ) : (
-        <button
-          onClick={() => setAddingProject(true)}
-          css={`
-            text-transform: uppercase;
-            font-size: 11.5px;
-            font-weight: normal;
-            color: ${theme.tertiary};
-            border-radius: 19px;
-            border: solid 1px ${theme.greyScale10};
-            padding: 10px;
-            ${theme.row};
-            cursor: pointer;
-          `}
-        >
-          <PlusIcon
-            width={12}
-            height={12}
-            fill={theme.tertiary}
-            css={`
-              margin-top: 1px;
-              margin-right: 4px;
-            `}
-          />
-          <div>Create a project</div>
-        </button>
-      )}
-    </div>
-  );
-};
+}) => (
+  <Container>
+    {addingProject ? (
+      <Fragment>
+        <AddIcon width={15} height={15} fill={theme.tertiary} />
+        <InputLabel>Create a project</InputLabel>
+        <Input
+          italic
+          flex={1}
+          type="text"
+          placeholder="Enter name of project"
+          value={projectName}
+          onChange={e => setProjectName(e.target.value)}
+        />
+        <LoadingOnClick
+          onClick={async () => {
+            await saveProject({ projectName, onSuccess: props.onSuccess });
+            setAddingProject(false);
+            setProjectName('');
+          }}
+          render={({ loading, onClick }) => (
+            <ActionButton className="saveButton" disabled={loading} onClick={onClick}>
+              <span>Save</span>
+            </ActionButton>
+          )}
+        />
+        <CancelButton onClick={() => setAddingProject(false)}>Cancel</CancelButton>
+      </Fragment>
+    ) : (
+      <CreateButton onClick={() => setAddingProject(true)}>
+        <AddIcon width={12} height={12} fill={theme.tertiary} />
+        <div>Create a project</div>
+      </CreateButton>
+    )}
+  </Container>
+);
+
+// css={`
+
+// `}
 
 export default enhance(CavaticaAddProject);
