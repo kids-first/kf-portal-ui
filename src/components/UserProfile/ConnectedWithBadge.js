8596f6c1f2cceb5836ca63b0778058a4c9340c32
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-06-07 16:14:37 -0400

    :art: made the big white button re-usable

--- src/components/UserProfile/ConnectedWithBadge.js
+++ src/components/UserProfile/ConnectedWithBadge.js
@@ -6,7 +6,8 @@ import GIcon from 'react-icons/lib/fa/google';
 import FBIcon from 'react-icons/lib/fa/facebook';
 import Check from 'react-icons/lib/fa/check';
 
-import { Box, Flex } from 'uikit/Core';
+import { Box } from 'uikit/Core';
+import { BigWhiteButton } from 'uikit/Button';
 
 import { GOOGLE, FACEBOOK } from 'common/constants';
 
@@ -19,28 +20,22 @@ const icons = {
   [FACEBOOK]: x => <FBIcon color="#428bca" {...x} />,
 };
 
-const ConnectedBadgeContainer = styled(Flex)`
-  background: ${({ theme }) => theme.white};
-  border: 1px solid ${({ theme }) => theme.greyScale8};
-  box-shadow: 0 0 2.9px 0.1px ${({ theme }) => theme.lightShadow};
-  border-radius: 25px;
-`;
-
-const ConnectedSection = styled(x => <Box p={3} {...x} />)`
+const ButtonItem = styled(x => <Box p={3} {...x} />)`
   border-right: ${({ divider, theme }) => (divider ? `1px solid ${theme.greyScale8}` : `none`)};
   color: ${({ theme }) => theme.tertiary};
   white-space: nowrap;
   font-weight: 500;
+  font-size: 16px;
 `;
 
 const ConnectedWithBadge = withTheme(({ theme, provider, Icon = icons[provider] }) => (
-  <ConnectedBadgeContainer>
-    <ConnectedSection divider>
+  <BigWhiteButton>
+    <ButtonItem divider>
       <Check color={theme.active} />
       <Icon size={20} />
-    </ConnectedSection>
-    <ConnectedSection>Connected with {capitalize(provider)}</ConnectedSection>
-  </ConnectedBadgeContainer>
+    </ButtonItem>
+    <ButtonItem>Connected with {capitalize(provider)}</ButtonItem>
+  </BigWhiteButton>
 ));
 
 export default ConnectedWithBadge;
