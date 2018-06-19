2afc09187800e403c86a9992bf173d6596695df6
Minh Ha <hlminh2000@gmail.com>
2018-06-19 16:22:04 -0400

    ✨ better domain fallback (#493)

--- src/services/cookie.js
+++ src/services/cookie.js
@@ -1,14 +1,20 @@
 import Cookies from 'js-cookie';
 import { cookiesDomain } from 'common/injectGlobals';
 
+const hostName = window.location.hostname;
+const hostNamePaths = hostName.split('.');
+const defaultDomain =
+  hostNamePaths.length >= 2
+    ? `.${hostNamePaths.slice(hostNamePaths.length - 2, hostNamePaths.length).join('.')}`
+    : hostName;
+
 const defaultOptions = {
   secure: true,
-  domain: cookiesDomain,
+  domain: cookiesDomain || defaultDomain,
 };
 
-export const setCookie = (key, value, options = {}) => {
-  return Cookies.set(key, value, { ...defaultOptions, options });
-};
+export const setCookie = (key, value, options = {}) =>
+  Cookies.set(key, value, { ...defaultOptions, options });
 
 export const getCookie = Cookies.get;
 
