afc9fdde80b42be71f220a42de9912b0d264adb5
Minh Ha <hlminh2000@gmail.com>
2018-06-19 15:37:13 -0400

    ✨ adds ego jwt cookie (#492)
    * adds ego jwt cookie
    
    * remove manual cookie addition

---
+++ src/services/cookie.js
@@ -0,0 +1,15 @@
+import Cookies from 'js-cookie';
+import { cookiesDomain } from 'common/injectGlobals';
+
+const defaultOptions = {
+  secure: true,
+  domain: cookiesDomain,
+};
+
+export const setCookie = (key, value, options = {}) => {
+  return Cookies.set(key, value, { ...defaultOptions, options });
+};
+
+export const getCookie = Cookies.get;
+
+export const removeCookie = Cookies.remove;
