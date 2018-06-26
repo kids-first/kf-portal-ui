818c4812eda8efeb129010040e9fe18981e25e79
Minh Ha <hlminh2000@gmail.com>
2018-06-26 14:59:38 -0400

    ✨ Feat/form full scroll (#501)
    * 0.1.2
    
    * implemenst SelectRoleForm full scroll

--- src/uikit/Column.js
+++ src/uikit/Column.js
@@ -1,7 +1,10 @@
 import styled from 'react-emotion';
 import { Flex } from './Core';
+import { position } from 'styled-system';
 
 export default styled(Flex)`
   ${({ theme }) => theme.column};
   ${({ center, theme }) => (center ? theme.center : ``)};
+  ${({ scrollY }) => (scrollY ? `overflow-y: scroll` : ``)};
+  ${position};
 `;
