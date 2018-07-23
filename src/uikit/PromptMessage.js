c3f72ddc48ecd5e4a18835b56a7721b670f6af8f
Minh <hlminh2000@gmail.com>
2018-07-23 16:43:43 -0400

    implements the beta message

--- src/uikit/PromptMessage.js
+++ src/uikit/PromptMessage.js
@@ -1,5 +1,6 @@
 import styled from 'react-emotion';
 import Row from 'uikit/Column';
+import { justifyContent, space } from 'styled-system';
 
 export const PromptMessageContainer = styled(Row)`
   align-items: left;
@@ -15,13 +16,21 @@ export const PromptMessageContainer = styled(Row)`
         : warning ? theme.warningBorder : info ? theme.infoBorder : theme.infoBorder}};
   padding: 10px;
   margin-bottom: 1em;
+  ${space};
 `;
 
 export const PromptMessageHeading = styled('div')`
+  color: ${({ theme, error, warning, info }) =>
+    error
+      ? theme.errorDark
+      : warning ? theme.warningDark : info ? theme.secondary : theme.secondary}};
   padding-right: 10px;
+  font-size: 20px;
+  ${space};
 `;
 
 export const PromptMessageContent = styled('div')`
   padding-top: 2px;
   line-height: 1.6em;
+  ${space};
 `;
