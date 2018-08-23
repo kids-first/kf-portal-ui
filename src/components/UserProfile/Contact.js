e4571afe560936b6cb23007a7a62f50161470a21
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-08-23 10:50:29 -0400

    Remove debug

--- src/components/UserProfile/Contact.js
+++ src/components/UserProfile/Contact.js
@@ -22,50 +22,44 @@ const ContactItem = styled(Section)`
   line-height: 1.83;
 `;
 
-const Contact = compose(injectState, withApi)(({ effects: { setModal }, api, profile, mt }) => {
-  console.log('ciaran api');
-  console.log('ciaran profile', profile);
-  console.log('ciaran props');
+const Contact = compose(injectState, withApi)(({ effects: { setModal }, api, profile, mt }) => (
+  <Box mt={mt}>
+    <H2>
+      Contact Information
+      <EditButton
+        onClick={() => {
+          setModal({
+            title: 'Edit Basic Information',
+            component: <BasicInfoForm {...{ api }} />,
+          });
+        }}
+      />
+    </H2>
 
-  return (
-    <Box mt={mt}>
-      <H2>
-        Contact Information
-        <EditButton
-          onClick={() => {
-            setModal({
-              title: 'Edit Basic Information',
-              component: <BasicInfoForm {...{ api }} />,
-            });
-          }}
-        />
-      </H2>
+    <Row alignItems="flex-start" mb={'20px'}>
+      <MapMarkerIcon height={'17px'} style={{ position: 'relative', top: '4px' }} />
+      <ContactItem ml={'7px'}>
+        <H3>{profile.institution}</H3>
+        <div>{`${profile.addressLine1} ${profile.addressLine2}`}</div>
+        <div>{`${profile.city}, ${profile.state}, ${profile.country}`}</div>
+        <div>{profile.zip}</div>
+      </ContactItem>
+    </Row>
 
-      <Row alignItems="flex-start" mb={'20px'}>
-        <MapMarkerIcon height={'17px'} style={{ position: 'relative', top: '4px' }} />
-        <ContactItem ml={'7px'}>
-          <H3>{profile.institution}</H3>
-          <div>{`${profile.addressLine1} ${profile.addressLine2}`}</div>
-          <div>{`${profile.city}, ${profile.state}, ${profile.country}`}</div>
-          <div>{profile.zip}</div>
-        </ContactItem>
-      </Row>
+    <Row alignItems="center" mb={'20px'}>
+      <EnvelopeIcon height={'10px'} />
+      <ContactItem ml={'7px'}>
+        <EmailLink bare primary bold href="mailto:simonscientist@chop.edu">
+          {profile.email}
+        </EmailLink>
+      </ContactItem>
+    </Row>
 
-      <Row alignItems="center" mb={'20px'}>
-        <EnvelopeIcon height={'10px'} />
-        <ContactItem ml={'7px'}>
-          <EmailLink bare primary bold href="mailto:simonscientist@chop.edu">
-            {profile.email}
-          </EmailLink>
-        </ContactItem>
-      </Row>
-
-      <Row alignItems="center">
-        <PhoneIcon height={'12px'} />
-        <ContactItem ml={'7px'}>{formatPhoneNumber(profile.phone)}</ContactItem>
-      </Row>
-    </Box>
-  );
-});
+    <Row alignItems="center">
+      <PhoneIcon height={'12px'} />
+      <ContactItem ml={'7px'}>{formatPhoneNumber(profile.phone)}</ContactItem>
+    </Row>
+  </Box>
+));
 
 export default Contact;
