9dda9b1e6b5d4a3b7d0fe03daeaa3c3e08082f20
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-08-27 22:00:20 -0400

    edit flash message styling

--- src/uikit/FlashMessage.js
+++ src/uikit/FlashMessage.js
@@ -6,6 +6,9 @@ import { Box } from './Core';
 const FlashMessage = styled(Box)`
   border-left: 5px solid ${({ theme }) => theme.tertiary};
   background-color: ${({ theme }) => theme.optionSelected};
+  font-size: 15px;
+  line-height: 28px;
+  font-family: 'Open Sans', sans-serif;
 `;
 
 export default ({ children, ...props }) => (
