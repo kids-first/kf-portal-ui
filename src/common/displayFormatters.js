1a253d7f6603707371a138f998ab16c4c2dad50f
Ciarán Schütte <ciaranschutte@gmail.com>
2018-09-04 14:27:15 -0400

    💄 Contact info fixes (#722)
    * Add address formatter
    
    * Fix number formatting
    
    * Fix contact link to open in new window

--- src/common/displayFormatters.js
+++ src/common/displayFormatters.js
@@ -1,9 +1,25 @@
+/**
+ * Very basic number display formatting
+ * @param {String} phone
+ */
 export const formatPhoneNumber = phone => {
   if (!phone) {
     return '';
-  } else if (phone.length >= 10) {
-    return `${phone.slice(0, 3)}-${phone.slice(3, 6)}-${phone.slice(6)}`;
-  } else {
-    return phone;
   }
+
+  const num = phone.replace(/\D/g, '');
+  if (num.length <= 10) {
+    return `${num.slice(0, 3)}-${num.slice(3, 6)}-${num.slice(6)}`;
+  } else {
+    return num;
+  }
+};
+
+/**
+ * Address single line formatter
+ * @param {Array} addresses
+ * @returns {String}
+ */
+export const formatAddressLine = (addresses = []) => {
+  return addresses.filter(x => x).join(', ');
 };
