a94fd492455182275a8e1349bc117345c8d5cd87
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-09-05 00:11:58 -0400

    Async loading

--- src/components/UserDashboard/TwitterBlock.js
+++ src/components/UserDashboard/TwitterBlock.js
@@ -10,6 +10,10 @@ const StyledCard = styled(Card)`
   margin: 15px 0 15px 40px;
   padding: 31px 29px 29px 27px;
   height: 440px;
+
+  & #twitter-timeline {
+    width: 100%;
+  }
 `;
 
 class TwitterBlock extends Component {
@@ -18,23 +22,31 @@ class TwitterBlock extends Component {
   };
 
   componentDidMount() {
+    const setLoaded = () => setTimeout(() => this.setState({ loaded: true }), 1000);
+
     scriptjs('https://platform.twitter.com/widgets.js', () => {
-      window.twttr.widgets
-        .createTimeline(
-          {
-            sourceType: 'profile',
-            screenName: handle,
-          },
-          document.getElementById('twitter-timeline'),
-          {
-            chrome: 'nofooter',
-            height: 390,
-            width: 800,
-          },
-        )
-        .then(() => {
-          setTimeout(() => this.setState({ loaded: true }), 200);
+      const twttr = window.twttr;
+
+      twttr.ready(() => {
+        twttr.events.bind('loaded', function(event) {
+          console.log('eevent', event);
+          setLoaded();
         });
+
+        twttr.widgets
+          .createTimeline(
+            {
+              sourceType: 'profile',
+              screenName: handle,
+            },
+            document.getElementById('twitter-timeline'),
+            {
+              chrome: 'nofooter',
+              height: 390,
+            },
+          )
+          .then(() => twttr.widgets.load());
+      });
     });
   }
 
