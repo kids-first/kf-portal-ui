822356609c095c5d5475d37ec0660cc0744cc6c7
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-09-04 23:35:52 -0400

    Add twitter block

--- src/components/UserDashboard/TwitterBlock.js
+++ src/components/UserDashboard/TwitterBlock.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import { TwitterTimelineEmbed } from 'react-twitter-embed';
-
+import React, { Component } from 'react';
+import styled from 'react-emotion';
+import scriptjs from 'scriptjs';
+import { Div } from 'uikit/Core';
 import Card from 'uikit/Card';
 
 const handle = 'kidsfirstDRC';
 
-const TwitterBlock = () => (
-  <Card my="15px" ml="40px" pt="31px" pr="29px" pb="29px" pl="27px" height="440px">
-    <TwitterTimelineEmbed sourceType="profile" screenName={handle} options={{ noFooter: true }} />
-  </Card>
-);
+const StyledCard = styled(Card)`
+  margin: 15px 0 15px 40px;
+  padding: 31px 29px 29px 27px;
+  height: 440px;
+`;
+
+class TwitterBlock extends Component {
+  state = {
+    loaded: false,
+  };
+
+  componentDidMount() {
+    scriptjs('https://platform.twitter.com/widgets.js', () => {
+      window.twttr.widgets
+        .createTimeline(
+          {
+            sourceType: 'profile',
+            screenName: handle,
+          },
+          document.getElementById('twitter-timeline'),
+          {
+            chrome: 'nofooter',
+            height: 390,
+            width: 800,
+          },
+        )
+        .then(() => {
+          setTimeout(() => this.setState({ loaded: true }), 200);
+        });
+    });
+  }
+
+  render() {
+    return (
+      <StyledCard>
+        <Div id="twitter-timeline" display={!this.state.loaded ? 'none' : 'block'} />
+      </StyledCard>
+    );
+  }
+}
 
 export default TwitterBlock;
