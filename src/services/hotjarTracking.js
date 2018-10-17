c04abd45283e866fd8f298b9255ad561bf54d199
Ben Dolly <bromeodolly@gmail.com>
2018-10-17 10:42:47 -0400

    :sparkles: adds Hotjar trackVirtualPageView

--- src/services/hotjarTracking.js
+++ src/services/hotjarTracking.js
@@ -1,6 +1,14 @@
 import scriptjs from 'scriptjs';
 import urlJoin from 'url-join';
 import { hotjarHost, hotjarId } from 'common/injectGlobals';
+const hj = window.hj;
 
 export const init = () => scriptjs(`${hotjarHost}${hotjarId}.js?sv=6`);
-  
+
+export const trackVirtualPageView = path => {
+  if (hj) {
+    hj('vpv', path);
+  } else {
+    console.error('Unable to trackVirtualPageView, hj global not found');
+  }
+};
