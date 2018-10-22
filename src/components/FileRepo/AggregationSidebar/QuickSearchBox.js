3758dd980abee833b6fe0def9c31e2766098efa6
Minh <hlminh2000@gmail.com>
2018-10-22 13:37:10 -0400

    implements the new icons
    removes console.log

--- src/components/FileRepo/AggregationSidebar/QuickSearchBox.js
+++ src/components/FileRepo/AggregationSidebar/QuickSearchBox.js
@@ -2,35 +2,56 @@ import React from 'react';
 import { compose } from 'recompose';
 import { withTheme } from 'emotion-theming';
 import { injectState } from 'freactal';
+import styled from 'react-emotion';
 import Spinner from 'react-spinkit';
 
 import { QuickSearch, AggsWrapper } from '@arranger/components/dist/Arranger';
+import { TextHighlight } from '@arranger/components/dist';
 
 import Row from 'uikit/Row';
 import { FilterInput } from 'uikit/Input';
 import UploadIdsButton from './UploadIdsButton';
+import FlaskSvg from 'icons/FlaskIcon';
+import ParticipantSvg from 'icons/ParticipantIcon';
+import FileSvg from 'icons/FileIcon';
 
-const DropdownItemComponent = ({
-  entityName,
-  optionIndex,
-  inputValue,
-  result,
-  primaryKey,
-  onClick,
-}) => (
+const IconContainer = styled('div')`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 1000px;
+  margin: 10px;
+  height: 30px;
+  width: 30px;
+  border: solid 2px ${({ borderColor }) => borderColor};
+  background: ${({ backgroundColor }) => backgroundColor};
+`;
+
+const ParticipantIcon = withTheme(({ theme }) => (
+  <IconContainer borderColor={theme.white} backgroundColor={theme.highlight}>
+    <ParticipantSvg width={15} height={15} fill={theme.white} />
+  </IconContainer>
+));
+
+const BiospecimenIcon = withTheme(({ theme }) => (
+  <IconContainer borderColor={theme.white} backgroundColor={theme.orange}>
+    <FlaskSvg width={15} height={15} />
+  </IconContainer>
+));
+
+const FileIcon = withTheme(({ theme }) => (
+  <IconContainer borderColor={theme.white} backgroundColor={theme.tertiary}>
+    <FileSvg width={15} height={15} fill={theme.white} />
+  </IconContainer>
+));
+
+const DropdownItemComponent = ({ inputValue, result, primaryKey, onClick, IconComponent }) => (
   <div className={`quick-search-result`} onClick={onClick}>
-    <div className={`quick-search-result-entity quick-search-result-entity-${optionIndex}`}>
-      <div>{entityName.slice(0, 2).toUpperCase()}</div>
-    </div>
+    <IconComponent />
     <div className="quick-search-result-details">
       <div className="quick-search-result-key">{primaryKey}</div>
       <div className="quick-search-result-value">
-        {result}
-        {/* <TextHighlight
-          highlightClassName={`quick-search-result-value-highlight`}
-          highlightText={inputValue}
-          content={result}
-        /> */}
+        <TextHighlight highlightText={inputValue} content={result} />
       </div>
     </div>
   </div>
@@ -58,7 +79,19 @@ const QuickSearchBox = compose(withTheme, injectState)(
         LoadingIcon={
           <Spinner fadeIn="none" name="circle" color="#a9adc0" style={{ width: 15, height: 15 }} />
         }
-        DropdownItemComponent={DropdownItemComponent}
+        DropdownItemComponent={({ ...props }) => {
+          const { entityName } = props;
+          return (
+            <DropdownItemComponent
+              IconComponent={
+                entityName === 'Participants'
+                  ? ParticipantIcon
+                  : entityName === 'Biospecimens' ? BiospecimenIcon : FileIcon
+              }
+              {...props}
+            />
+          );
+        }}
       />
       <Row mt={'5px'}>
         <UploadIdsButton
