2bb167ead2f1a035c8007ef10638440b9003bb37
bdolly <bromeodolly@gmail.com>
2018-10-23 10:41:42 -0400

    🎨 Analytics tracking service reorg (#894)
    * :chart_with_upwards_trend: updates abandoned query tracking
    
    Track when a query abandonment when a user starts a query and navigates away from the page without saving or downloading it
    
    * :art: re-organizes the analyticsTracking service
    
    adds an index.js to allow for multiple tracking providers to be implemented from within the analyticsTracking service
    
    * 📈adds analyticsTrigger as currier for HJTrigger
    
    * :chart_with_upwards_trend: updates abandoned query tracking
    
    Track when a query abandonment when a user starts a query and navigates away from the page without saving or downloading it
    
    * udpates query abandonment sqon check method
    
    * cleans up some formating

---
+++ src/services/analyticsTracking/hotjarTracking.js
@@ -0,0 +1,34 @@
+import scriptjs from 'scriptjs';
+import urlJoin from 'url-join';
+import { lowerCase, snakeCase } from 'lodash';
+import { hotjarHost, hotjarId } from 'common/injectGlobals';
+
+const hj = () => {
+  window.hj =
+    window.hj ||
+    function() {
+      (hj.q = hj.q || []).push(arguments);
+    };
+  return window.hj;
+};
+
+export const init = () => scriptjs(`${hotjarHost}${hotjarId}.js?sv=6`);
+
+export const HJVirtualPageView = path => {
+  if (window.hj) {
+    hj('vpv', path);
+  } else {
+    console.error('Unable to trackVirtualPageView, "window.hj" global not found');
+  }
+};
+
+export const HJTrigger = ({ property, type, uiArea, action, label }) => {
+  // example triggerName: record_portal__user_profile__Integration_Connection_init__cavatica
+  const normalizeStr = str => snakeCase(lowerCase(str));
+
+  const triggerName = `${normalizeStr(type)}_${normalizeStr(property)}__${normalizeStr(
+    uiArea,
+  )}__${normalizeStr(action)}__${normalizeStr(label)}`;
+  console.info(`Hotjar Triggered: ${triggerName}`);
+  hj('trigger', triggerName);
+};
