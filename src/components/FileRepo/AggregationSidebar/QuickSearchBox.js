ff2e29f1f696ec825a2ac3f280fbdf49a34a54fa
Minh <hlminh2000@gmail.com>
2018-10-23 15:22:04 -0400

    removes console log

--- src/components/FileRepo/AggregationSidebar/QuickSearchBox.js
+++ src/components/FileRepo/AggregationSidebar/QuickSearchBox.js
@@ -81,7 +81,6 @@ const QuickSearchBox = compose(withTheme, injectState)(
         }
         DropdownItemComponent={({ ...props }) => {
           const { entityName } = props;
-          console.log('entityName: ', entityName);
           return (
             <DropdownItemComponent
               IconComponent={
