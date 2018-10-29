f03d037b118f6336fdc709862157fc157cd1627f
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-10-29 18:43:18 -0400

    Change to column layout for Card

--- src/components/UserDashboardV2/index.js
+++ src/components/UserDashboardV2/index.js
@@ -31,44 +31,6 @@ const DashboardCard = styled(Card)`
   margin: 30px;
 `;
 
-export const data = [
-  {
-    id: 'Pediatric Brain Tumors: CBTTC',
-    probands: 50,
-    familyMembers: 100,
-  },
-  {
-    id: 'Orofacial Cleft: European Ancestry',
-    probands: 102,
-    familyMembers: 167,
-  },
-  {
-    id: 'Ewing Sarcoma: Genetic Risk',
-    probands: 23,
-    familyMembers: 630,
-  },
-  {
-    id: 'Syndromic Cranial Dysinnervation',
-    probands: 430,
-    familyMembers: 500,
-  },
-  {
-    id: 'Congenital Heart Defects',
-    probands: 230,
-    familyMembers: 550,
-  },
-  {
-    id: 'Adolescent Idiopathic Scoliosis',
-    probands: 340,
-    familyMembers: 400,
-  },
-  {
-    id: 'Congenital Diaphragmatic Hernia',
-    probands: 360,
-    familyMembers: 420,
-  },
-];
-
 export default compose(
   injectState,
   withRouter,
