95d94f6bb55445f73ad7f44d44f1651d3042a80e
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-10-29 17:36:26 -0400

    Style loader

--- src/chartkit/components/ChartWrapper.js
+++ src/chartkit/components/ChartWrapper.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import Component from 'react-component-component';
 
+import LoadingSpinner from 'uikit/LoadingSpinner';
+
 const ChartWrapper = ({ endpoint = '', children }) => (
   <Component
+    style={{ height: '100%' }}
     initialState={{ data: null, isLoading: true }}
     didMount={({ setState }) => {
       fetch('http://localhost:3000/barChart')
         .then(res => res.json())
-        .then(data => setTimeout(() => setState({ data: data, isLoading: false }), 2000))
+        .then(data => setState({ data: data, isLoading: false }))
         .catch(err => console.log('err', err));
     }}
   >
     {({ state }) =>
       state.isLoading ? (
-        <div>Loading....</div>
+        <LoadingSpinner size="50px" />
       ) : state.data ? (
         React.cloneElement(children, { data: state.data })
       ) : (
