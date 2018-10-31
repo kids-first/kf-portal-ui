472c8ae1dd78b21a9577639bd89f4e4a8f041551
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-10-31 14:05:25 -0400

    Add CardsContainer to display a collection of Cards

--- src/components/UserDashboardV2/index.js
+++ src/components/UserDashboardV2/index.js
@@ -4,13 +4,15 @@ import { withRouter } from 'react-router-dom';
 import { injectState } from 'freactal';
 import { Helmet } from 'react-helmet';
 import styled from 'react-emotion';
-import LoadingSpinner from 'uikit/LoadingSpinner';
 
-import Row from 'uikit/Row';
+import LoadingSpinner from 'uikit/LoadingSpinner';
+import CardsContainer from 'uikit/Card/CardsContainer';
 import Card from 'uikit/Card';
+
 import ChartWrapper from 'chartkit/components/ChartWrapper';
 import HorizontalBar from 'chartkit/components/HorizontalBar';
 import { ChartColors } from 'chartkit/themes';
+
 import { initializeApi } from 'services/publicStats';
 
 const UserDashboard = styled('div')`
@@ -21,7 +23,7 @@ const UserDashboard = styled('div')`
 `;
 
 const DashboardCard = styled(Card)`
-  width: 33%;
+  width: calc(33% - 60px);
   height: 400px;
   margin: 30px;
 `;
@@ -36,7 +38,7 @@ export default compose(
       <Helmet>
         <title>Portal - User Dashboard</title>
       </Helmet>
-      <Row flexWrap="wrap" width="100%">
+      <CardsContainer>
         <DashboardCard title="Studies">
           <ChartWrapper endpoint="studies" api={initializeApi({})}>
             {({ data, isLoading }) =>
@@ -51,8 +53,8 @@ export default compose(
                   tickValues={[0, 250, 500, 750, 1000, 1250]}
                   xTickTextLength={22}
                   legends={[
-                    { title: '# Probands', color: '#1f9bb6' },
-                    { title: '# Family Members', color: '#e3429b' },
+                    { title: 'Probands', color: '#1f9bb6' },
+                    { title: 'Family Members', color: '#e3429b' },
                   ]}
                 />
               ) : (
@@ -61,7 +63,55 @@ export default compose(
             }
           </ChartWrapper>
         </DashboardCard>
-      </Row>
+        <DashboardCard title="Studies">
+          <ChartWrapper endpoint="studies" api={initializeApi({})}>
+            {({ data, isLoading }) =>
+              isLoading ? (
+                <LoadingSpinner />
+              ) : data ? (
+                <HorizontalBar
+                  data={data}
+                  indexBy="name"
+                  keys={['probands', 'familyMembers']}
+                  colors={[ChartColors.blue, ChartColors.purple]}
+                  tickValues={[0, 250, 500, 750, 1000, 1250]}
+                  xTickTextLength={22}
+                  legends={[
+                    { title: 'Probands', color: '#1f9bb6' },
+                    { title: 'Family Members', color: '#e3429b' },
+                  ]}
+                />
+              ) : (
+                <div>No data</div>
+              )
+            }
+          </ChartWrapper>
+        </DashboardCard>
+        <DashboardCard title="Studies">
+          <ChartWrapper endpoint="studies" api={initializeApi({})}>
+            {({ data, isLoading }) =>
+              isLoading ? (
+                <LoadingSpinner />
+              ) : data ? (
+                <HorizontalBar
+                  data={data}
+                  indexBy="name"
+                  keys={['probands', 'familyMembers']}
+                  colors={[ChartColors.blue, ChartColors.purple]}
+                  tickValues={[0, 250, 500, 750, 1000, 1250]}
+                  xTickTextLength={22}
+                  legends={[
+                    { title: 'Probands', color: '#1f9bb6' },
+                    { title: 'Family Members', color: '#e3429b' },
+                  ]}
+                />
+              ) : (
+                <div>No data</div>
+              )
+            }
+          </ChartWrapper>
+        </DashboardCard>
+      </CardsContainer>
     </UserDashboard>
   );
 });
