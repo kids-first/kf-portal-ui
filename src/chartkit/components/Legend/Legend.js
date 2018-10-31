475eaf2faf7fe6d5eddae1cb45bcece9051001b9
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-10-31 10:03:25 -0400

    Move legend into separate file

---
+++ src/chartkit/components/Legend/Legend.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import SvgSquare from './SvgSquare';
+import SvgText from './SvgText';
+
+/**
+ * Basic spacing for now, can be improved on
+ * based on nivo legend spacing
+ * 
+    let xStep = 0
+    let yStep = 0
+    if (direction === DIRECTION_ROW) {
+        xStep = itemWidth + itemsSpacing
+    } else if (direction === DIRECTION_COLUMN) {
+        yStep = itemHeight + itemsSpacing
+    } 
+ */
+
+const DIRECTION_ROW = 'ROW';
+
+const Legend = ({ legends = [], direction = DIRECTION_ROW, style, theme }) => {
+  const { itemWidth, itemsSpacing, iconSize, icon, text } = theme;
+
+  // Max height for our svg based on the largest child elements
+  const maxHeight = Math.max(iconSize, text.fontSize) + 1;
+
+  // Center text with our label. Decimals (put simply) don't work with SVG so ceil() it for visually pleasing results
+  const textOffsetY = Math.ceil(text.fontSize / 2);
+
+  return (
+    <div style={style}>
+      <svg height={maxHeight} xmlns="http://www.w3.org/2000/svg">
+        {legends.map((l, i) => {
+          let xOffset = 0;
+
+          xOffset = itemWidth + itemsSpacing;
+
+          const legendItem = (
+            <g key={i} height={maxHeight} transform={`translate(${i * xOffset}, 0)`}>
+              <SvgSquare
+                style={icon}
+                fill={l.color}
+                height={iconSize}
+                width={iconSize}
+                x={i * xOffset}
+                y={0}
+              />
+              <SvgText
+                style={text}
+                textValue={l.title}
+                x={i * xOffset + iconSize + 2}
+                y={textOffsetY}
+              >
+                {l.title}
+              </SvgText>
+            </g>
+          );
+
+          return legendItem;
+        })}
+      </svg>
+    </div>
+  );
+};
+
+export default Legend;
