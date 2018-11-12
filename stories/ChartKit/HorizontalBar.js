9e5fc7d1bdcc89374f59d940ef50f56f6d2b723a
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-11-12 17:35:05 -0500

    Add Horizontal Bar stories

--- stories/ChartKit/HorizontalBar.js
+++ stories/ChartKit/HorizontalBar.js
@@ -1,108 +1,127 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action, configureActions } from '@storybook/addon-actions';
 import _ from 'lodash';
+
+import { serverMock, randomMock, smallNumberMock, largeNumberMock } from './mocks';
 
 import HorizontalBar from '../../src/chartkit/components/HorizontalBar';
 import { chartColors } from '../../src/theme/defaultTheme';
-
-export const bar = {};
-
-export const actions = {};
 
 const FixedHeightWrapper = ({ children }) => (
   <div style={{ height: '300px', padding: '25px' }}>{children}</div>
 );
 
-/*
-old mock
-const mock = [
-  {
-    id: 'Pediatric Brain Tumors: CBTTC',
-    probands: 10, //_.random(0, randMax),
-    familyMembers: 10, //_.random(0, randMax),
-  },
-  {
-    id: 'Orofacial Cleft: European Ancestry',
-    probands: 30, //_.random(0, randMax),
-    familyMembers: 30, //_.random(0, randMax),
-  },
-  {
-    id: 'Ewing Sarcoma: Genetic Risk',
-    probands: 60, //_.random(0, randMax),
-    familyMembers: 60, //_.random(0, randMax),
-  },
-  {
-    id: 'Syndromic Cranial Dysinnervation',
-    probands: 120, //_.random(0, randMax),
-    familyMembers: 120, // _.random(0, randMax),
-  },
-  {
-    id: 'Congenital Heart Defects',
-    probands: 170, //_.random(0, randMax),
-    familyMembers: 170, // _.random(0, randMax),
-  },
-  {
-    id: 'Adolescent nameiopathic Scoliosis',
-    probands: 240, //_.random(0, randMax),
-    familyMembers: 240, // _.random(0, randMax),
-  },
-  {
-    id: 'Congenital Diaphragmatic Hernia',
-    random: 'xx',
-    probands: 350, //_.random(0, randMax),
-    familyMembers: 350, // _.random(0, randMax),
-  },
-];
-*/
-
-const randMax = 2100;
-const mock = [
-  {
-    id: 'a',
-    probands: 10, //_.random(0, randMax),
-    familyMembers: 10, //_.random(0, randMax),
-  },
-  {
-    id: 'b',
-    probands: 30, //_.random(0, randMax),
-    familyMembers: 30, //_.random(0, randMax),
-  },
-  {
-    id: 'c',
-    probands: 60, //_.random(0, randMax),
-    familyMembers: 60, //_.random(0, randMax),
-  },
-  {
-    id: 'd',
-    probands: 120, //_.random(0, randMax),
-    familyMembers: 120, // _.random(0, randMax),
-  },
-  {
-    id: 'e',
-    probands: 170, //_.random(0, randMax),
-    familyMembers: 170, // _.random(0, randMax),
-  },
-  {
-    id: 'f',
-    probands: 240, //_.random(0, randMax),
-    familyMembers: 240, // _.random(0, randMax),
-  },
-  {
-    id: 'g',
-    random: 'xx',
-    probands: 350, //_.random(0, randMax),
-    familyMembers: 350, // _.random(0, randMax),
-  },
-];
-
 storiesOf('ChartKit/HorizontalBar', module).add('default', () => (
   <FixedHeightWrapper>
     <HorizontalBar
-      data={mock}
+      data={serverMock}
       indexBy="id"
       keys={['probands', 'familyMembers']}
       colors={[chartColors.blue, chartColors.purple]}
       tickInterval={5}
+      xTickTextLength={28}
+      legends={[
+        { title: 'Probands', color: chartColors.blue },
+        { title: 'Family Members', color: chartColors.purple },
+      ]}
+      padding={0.4}
+    />
+  </FixedHeightWrapper>
+));
+
+/**
+ * Examples of merging static data with data from API
+ */
+const localMock = [
+  { id: 'a', url: 'www.google.ca' },
+  { id: 'b', url: 'www.duckduckgo.com' },
+  { id: 'c', url: 'www.bbc.com' },
+];
+
+const mergedMockData = serverMock.map(d => ({ ...d, ..._.find(localMock, { id: d.id }) }));
+
+const onClickMock = barData => {
+  action('Clicking bar');
+};
+
+storiesOf('ChartKit/HorizontalBar', module).add('onClick', () => (
+  <FixedHeightWrapper>
+    <HorizontalBar
+      data={mergedMockData}
+      indexBy="id"
+      keys={['probands', 'familyMembers']}
+      colors={[chartColors.blue, chartColors.purple]}
+      onClick={onClickMock}
+      tickInterval={5}
+      xTickTextLength={28}
+      legends={[
+        { title: 'Probands', color: chartColors.blue },
+        { title: 'Family Members', color: chartColors.purple },
+      ]}
+      padding={0.4}
+    />
+  </FixedHeightWrapper>
+));
+
+storiesOf('ChartKit/HorizontalBar', module).add('Custom Ordering', () => (
+  <FixedHeightWrapper>
+    <HorizontalBar
+      data={randomMock}
+      indexBy="id"
+      keys={['probands', 'familyMembers']}
+      colors={[chartColors.blue, chartColors.purple]}
+      sortBy={(a, b) => {
+        const aTotal = a.probands + a.familyMembers;
+        const bTotal = b.probands + b.familyMembers;
+        return aTotal <= bTotal ? 1 : -1;
+      }}
+      tickInterval={5}
+      xTickTextLength={28}
+      legends={[
+        { title: 'Probands', color: chartColors.blue },
+        { title: 'Family Members', color: chartColors.purple },
+      ]}
+      padding={0.4}
+    />
+  </FixedHeightWrapper>
+));
+
+storiesOf('ChartKit/HorizontalBar', module).add('Large numbers', () => (
+  <FixedHeightWrapper>
+    <HorizontalBar
+      data={largeNumberMock}
+      indexBy="id"
+      tickInterval={5}
+      keys={['probands', 'familyMembers']}
+      colors={[chartColors.blue, chartColors.purple]}
+      sortBy={(a, b) => {
+        const aTotal = a.probands + a.familyMembers;
+        const bTotal = b.probands + b.familyMembers;
+        return aTotal <= bTotal ? 1 : -1;
+      }}
+      xTickTextLength={28}
+      legends={[
+        { title: 'Probands', color: chartColors.blue },
+        { title: 'Family Members', color: chartColors.purple },
+      ]}
+      padding={0.4}
+    />
+  </FixedHeightWrapper>
+));
+
+storiesOf('ChartKit/HorizontalBar', module).add('Small numbers', () => (
+  <FixedHeightWrapper>
+    <HorizontalBar
+      data={smallNumberMock}
+      indexBy="id"
+      keys={['probands', 'familyMembers']}
+      colors={[chartColors.blue, chartColors.purple]}
+      sortBy={(a, b) => {
+        const aTotal = a.probands + a.familyMembers;
+        const bTotal = b.probands + b.familyMembers;
+        return aTotal <= bTotal ? 1 : -1;
+      }}
       xTickTextLength={28}
       legends={[
         { title: 'Probands', color: chartColors.blue },
