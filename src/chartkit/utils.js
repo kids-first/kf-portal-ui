4ae5f61e30553b84155f54ec2b6b7a6b0c05147c
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-11-12 17:37:34 -0500

    Add utils

--- src/chartkit/utils.js
+++ src/chartkit/utils.js
@@ -8,4 +8,32 @@ export const maxValues = (data, keys) => {
   return values;
 };
 
-//export const chartMaxValue = maxValues => console.log(...maxValues);
+export const getChartMaxValue = (data, keys) =>
+  Math.max(...data.map(d => keys.reduce((acc, current) => d[current] + acc, 0)));
+
+export const getTickValues = (interval, maxValue, dataSize) => {
+  const tickValues = [];
+  const divisor = roundTo(maxValue / interval, dataSize);
+  for (let i = 0; i <= maxValue; i = i + divisor) {
+    tickValues.push(i);
+  }
+  return tickValues;
+};
+
+export const getDataRangeSize = maxValue => {
+  if (maxValue >= 1000000) {
+    return 1000000;
+  } else if (maxValue >= 100000) {
+    return 100000;
+  } else if (maxValue >= 10000) {
+    return 10000;
+  } else if (maxValue >= 1000) {
+    return 1000;
+  } else if (maxValue >= 100) {
+    return 100;
+  } else {
+    return 10;
+  }
+};
+
+export const roundTo = (val, rounding) => Math.ceil(val / rounding) * rounding;
