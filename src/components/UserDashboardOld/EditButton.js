df358d000766c6832db189e8f6f601d8209e34d5
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-11-13 00:13:28 -0500

    rename old dash

---
+++ src/components/UserDashboardOld/EditButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PencilIcon from 'react-icons/lib/fa/pencil';
+import { WhiteButton } from '../../uikit/Button';
+
+const EditButton = ({ egoId, theme, ...props }) => (
+  <Link
+    to={{
+      pathname: `/user/${egoId}`,
+      hash: '#aboutMe',
+    }}
+    {...props}
+    style={{ textDecoration: 'none' }}
+  >
+    <WhiteButton mx="4px">
+      <PencilIcon size={12} className="icon" /> Edit Profile
+    </WhiteButton>
+  </Link>
+);
+
+export default EditButton;
---
+++ src/components/UserDashboardOld/QueryBlock.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { distanceInWords } from 'date-fns';
+import TrashIcon from 'react-icons/lib/fa/trash';
+import styled from 'react-emotion';
+import { compose } from 'recompose';
+
+import { injectState } from 'freactal';
+import { trackUserInteraction, TRACKING_EVENTS } from 'services/analyticsTracking';
+import { Box, Flex, Span, Link } from 'uikit/Core';
+import Column from 'uikit/Column';
+import Row from 'uikit/Row';
+import { withTheme } from 'emotion-theming';
+import { withApi } from 'services/api';
+
+const Detail = styled(Span)`
+  color: ${({ theme }) => theme.greyScale1};
+`;
+
+const Query = styled(Flex)`
+  padding: 10px 10px 10px 25px;
+  border-bottom: 1px solid ${({ theme }) => theme.greyScale5};
+  transition-property: opacity;
+  ${({ inactive, theme }) =>
+    inactive
+      ? `
+          opacity: 0.6;
+          pointer-events: none;
+          &:last-child {
+            border-bottom: 1px solid ${({ theme }) => theme.greyScale5};
+          }
+        `
+      : ``};
+`;
+
+const QueryBlock = compose(injectState, withApi, withTheme)(
+  ({ effects: { deleteQuery }, api, query: q, inactive = false, theme, savedTime = true }) => (
+    <Query inactive={inactive}>
+      <Column width="100%">
+        <Row justifyContent="space-between" width="100%">
+          <Link
+            onClick={() => {
+              trackUserInteraction({
+                category: TRACKING_EVENTS.categories.user.dashboard.widgets.savedQueries,
+                action: `${TRACKING_EVENTS.actions.click} Saved Query Title`,
+                label: JSON.stringify(q),
+              });
+            }}
+            fontSize={1}
+            fontWeight="bold"
+            color={theme.primary}
+            to={q.link}
+          >
+            {q.alias}
+          </Link>
+          <Box pr={2} pl={2}>
+            <Span
+              color={theme.primary}
+              hover={{ cursor: 'pointer', color: theme.hover }}
+              onClick={() => {
+                trackUserInteraction({
+                  category: TRACKING_EVENTS.categories.user.dashboard.widgets.savedQueries,
+                  action: TRACKING_EVENTS.actions.query.delete,
+                  label: JSON.stringify(q),
+                });
+                deleteQuery({ api, queryId: q.id });
+              }}
+            >
+              <TrashIcon />
+            </Span>
+          </Box>
+        </Row>
+        <Box mt={2} mb={2} color={theme.greyScale9} fontSize="0.75rem">
+          <Detail>{(q.content.Files || 0).toLocaleString()}</Detail> Files |{' '}
+          <Detail>{(q.content.Participants || 0).toLocaleString()}</Detail> Participants |{' '}
+          <Detail>{(q.content.Families || 0).toLocaleString()}</Detail> Families |{' '}
+          <Detail>{q.content.Size}</Detail>
+        </Box>
+        {savedTime ? (
+          <Box fontSize="0.75rem">
+            Saved {distanceInWords(new Date(), new Date(q.creationDate))} ago
+          </Box>
+        ) : null}
+      </Column>
+    </Query>
+  ),
+);
+
+export default QueryBlock;
