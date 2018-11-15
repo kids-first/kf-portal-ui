5b83eebb11752b2c74a51fa75e3644c00aa77b85
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-11-15 13:31:27 -0500

    Fix chart links to file repo

--- src/components/UserDashboard/charts.js
+++ src/components/UserDashboard/charts.js
@@ -17,41 +17,29 @@ const participantTooltip = data => {
   return `${participants.toLocaleString()} participant${participants > 1 ? 's' : ''}`;
 };
 
-const fileRepoLinks = [
-  {
-    name: 'Pediatric Brain Tumors: CBTTC',
-    url: `/search/file?sqon=%7B%22op%22%3A%22and%22%2C%22content%22%3A%5B%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22participants.study.short_name%22%2C%22value%22%3A%5B%22Pediatric%20Brain%20Tumors%3A%20CBTTC%22%5D%7D%7D%5D%7D`,
-  },
-  {
-    name: 'Orofacial Cleft: European Ancestry',
-    url: `/search/file?sqon=%7B%22op%22%3A%22and%22%2C%22content%22%3A%5B%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22participants.study.short_name%22%2C%22value%22%3A%5B%22Orofacial%20Cleft%3A%20European%20Ancestry%22%5D%7D%7D%5D%7D`,
-  },
-  {
-    name: 'Ewing Sarcoma: Genetic Risk',
-    url: `/search/file?sqon=%7B%22op%22%3A%22and%22%2C%22content%22%3A%5B%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22participants.study.short_name%22%2C%22value%22%3A%5B%22Ewing%20Sarcoma%3A%20Genetic%20Risk%22%5D%7D%7D%5D%7D`,
-  },
-  {
-    name: 'Syndromic Cranial Dysinnervation',
-    url: `/search/file?sqon=%7B%22op%22%3A%22and%22%2C%22content%22%3A%5B%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22participants.study.short_name%22%2C%22value%22%3A%5B%22Syndromic%20Cranial%20Dysinnervation%22%5D%7D%7D%5D%7D`,
-  },
-  {
-    name: 'Congenital Heart Defects',
-    url: `/search/file?sqon=%7B%22op%22%3A%22and%22%2C%22content%22%3A%5B%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22participants.study.short_name%22%2C%22value%22%3A%5B%22Congenital%20Heart%20Defects%22%5D%7D%7D%5D%7D`,
-  },
-  {
-    name: 'Adolescent Idiopathic Scoliosis',
-    url: `/search/file?sqon=%7B%22op%22%3A%22and%22%2C%22content%22%3A%5B%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22participants.study.short_name%22%2C%22value%22%3A%5B%22Adolescent%20Idiopathic%20Scoliosis%22%5D%7D%7D%5D%7D`,
-  },
-  {
-    name: 'Congenital Diaphragmatic Hernia',
-    url: `/search/file?sqon=%7B%22op%22%3A%22and%22%2C%22content%22%3A%5B%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22participants.study.short_name%22%2C%22value%22%3A%5B%22Congenital%20Diaphragmatic%20Hernia%22%5D%7D%7D%5D%7D`,
-  },
-];
+const sqon = {
+  op: 'and',
+  content: [
+    {
+      op: 'in',
+      content: null,
+    },
+  ],
+};
+
+const SHORT_NAME_FIELD = 'participants.study.short_name';
+const TEXT_DIAGNOSES_FIELD = 'participants.diagnoses.source_text_diagnosis';
+
+const getFileRepoURL = (field, value) => {
+  sqon.content[0].content = { field, value };
+  const x = `/search/file?sqon=${encodeURI(JSON.stringify(sqon))}`;
+  return x;
+};
 
 export const StudiesChart = withTheme(({ data, theme }) => {
   const mergedStudyData = data.map(d => ({
     ...d,
-    ..._.find(fileRepoLinks, { name: d.name }),
+    url: getFileRepoURL(SHORT_NAME_FIELD, d.name),
   }));
 
   const onClick = barData => (window.location.href = barData.data.url);
@@ -77,25 +65,6 @@ export const StudiesChart = withTheme(({ data, theme }) => {
   );
 });
 
-export const TopDiagnosesChart = withTheme(({ data, theme }) => (
-  <HorizontalBar
-    data={data}
-    indexBy="name"
-    keys={['probands', 'familyMembers']}
-    tooltipFormatter={participantTooltip}
-    sortBy={sortDescParticipant}
-    tickInterval={4}
-    padding={15}
-    colors={[theme.chartColors.blue, theme.chartColors.purple]}
-    xTickTextLength={28}
-    legends={[
-      { title: 'Probands', color: theme.chartColors.blue },
-      { title: 'Family Members', color: theme.chartColors.purple },
-    ]}
-    padding={0.7}
-  />
-));
-
 export const UserInterestsChart = withTheme(({ data, theme }) => {
   // sort by count then alpha, limit to top 10
   const sortedInterests = _.orderBy(data, ['count', 'name'], ['desc', 'asc'])
@@ -107,4 +76,33 @@ export const UserInterestsChart = withTheme(({ data, theme }) => {
     }));
 
   return <Donut data={sortedInterests} colors={[theme.chartColors.red, '#FFF']} />;
+});
+
+export const TopDiagnosesChart = withTheme(({ data, theme }) => {
+  const mergedData = data.map(d => ({
+    ...d,
+    url: getFileRepoURL(TEXT_DIAGNOSES_FIELD, d.name),
+  }));
+
+  const onClick = barData => (window.location.href = barData.data.url);
+
+  return (
+    <HorizontalBar
+      data={mergedData}
+      indexBy="name"
+      keys={['probands', 'familyMembers']}
+      onClick={onClick}
+      tooltipFormatter={participantTooltip}
+      sortBy={sortDescParticipant}
+      tickInterval={4}
+      padding={15}
+      colors={[theme.chartColors.blue, theme.chartColors.purple]}
+      xTickTextLength={28}
+      legends={[
+        { title: 'Probands', color: theme.chartColors.blue },
+        { title: 'Family Members', color: theme.chartColors.purple },
+      ]}
+      padding={0.7}
+    />
+  );
 });
