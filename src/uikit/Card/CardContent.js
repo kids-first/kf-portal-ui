b8b785b0bb113a540ee521c66515ae86b9d14765
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-11-19 09:26:30 -0500

    Add resp breakpoints

--- src/uikit/Card/CardContent.js
+++ src/uikit/Card/CardContent.js
@@ -3,7 +3,7 @@ import styled from 'react-emotion';
 
 const Content = styled('div')`
   height: 100%;
-  overflow-y: ${props => (props.scrollable ? 'scroll' : 'visible')};
+  overflow-y: ${props => (props.scrollable ? 'auto' : 'visible')};
 `;
 
 const CardContent = ({ children, scrollable }) => (
