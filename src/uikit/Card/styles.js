85a1799a273504d13c003a33c3fda5717f41e0a6
Minh <hlminh2000@gmail.com>
2018-11-20 15:22:09 -0500

    removes unneeded change

--- src/uikit/Card/styles.js
+++ src/uikit/Card/styles.js
@@ -7,7 +7,7 @@ export const CardWrapper = applyDefaultStyles(styled('div')`
   box-shadow: 0 0 9.5px 0.5px rgba(160, 160, 163, 0.25);
   background-color: rgba(255, 255, 255, ${({ inactive }) => (inactive ? '50%' : '100%')});
   padding: 26px 30px;
-  border: solid 1px #e0e1e6;
   display: flex;
   flex-direction: column;
+  border: solid 1px ${({ theme }) => theme.greyScale5};
 `);
