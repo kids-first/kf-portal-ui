995fcb45b9e51be243ffcf4a82486b5d92bee33f
Jon Eubank <joneubank@gmail.com>
2018-11-22 14:59:15 -0500

    Add onClick behaviour to the label ticks

--- src/chartkit/components/HorizontalBar.js
+++ src/chartkit/components/HorizontalBar.js
@@ -71,6 +71,16 @@ class HorizontalBar extends Component {
 
     const highlighted = value === highlightedIndexValue ? { fill: '#2b388f' } : {};
 
+    const onLabelClick = tick => {
+      console.log(`tick: ${JSON.stringify(tick)}`);
+      console.log(`data: ${JSON.stringify(this.data)}`);
+      const data = this.data.find(d => d.name === tick.value);
+
+      console.log(`tickData: ${JSON.stringify(this.tickData)}`);
+
+      data ? this.onClick({ data }) : null;
+    };
+
     return (
       <g
         key={key}
@@ -84,6 +94,7 @@ class HorizontalBar extends Component {
           textAnchor="start"
           alignmentBaseline="middle"
           style={{ ...theme.axis.ticks.text, ...highlighted }}
+          onClick={this.onClick ? () => onLabelClick(tick) : null}
         >
           {text}
         </text>
