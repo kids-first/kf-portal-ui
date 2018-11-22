b869cedbc21a0819b41f72e547a4f99a58ada59f
Jon Eubank <joneubank@gmail.com>
2018-11-22 15:39:45 -0500

    Remove redundant onClick existence checks

--- src/chartkit/components/HorizontalBar.js
+++ src/chartkit/components/HorizontalBar.js
@@ -56,7 +56,7 @@ class HorizontalBar extends Component {
 
   renderAxisLeftTick(tick) {
     const { highlightedIndexValue } = this.state;
-    const { xTickTextLength = 10 } = this.props;
+    const { onClick, xTickTextLength = 10 } = this.props;
     const { format, key, x, y, theme, tickIndex } = tick;
 
     let value = tick.value;
@@ -74,7 +74,7 @@ class HorizontalBar extends Component {
     const onLabelClick = tick => {
       const data = this.data.find(d => d.name === tick.value);
 
-      data ? this.onClick({ data }) : null;
+      data ? onClick({ data }) : null;
     };
 
     return (
@@ -90,7 +90,7 @@ class HorizontalBar extends Component {
           textAnchor="start"
           alignmentBaseline="middle"
           style={{ ...theme.axis.ticks.text, ...highlighted }}
-          onClick={this.onClick ? () => onLabelClick(tick) : null}
+          onClick={() => onLabelClick(tick)}
         >
           {text}
         </text>
