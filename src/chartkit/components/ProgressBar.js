2b9e6bd82b752a63e2b07a54cc490572d199dd75
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-11-23 05:27:11 -0500

    Remove rc-progress, add more basic progress bar

--- src/chartkit/components/ProgressBar.js
+++ src/chartkit/components/ProgressBar.js
@@ -1,15 +1,49 @@
 import React from 'react';
-import { Line } from 'rc-progress';
+import PropTypes from 'prop-types';
 
-import 'rc-progress/assets/index.css';
+const ProgressBar = ({ percent, width, strokeColor, trailColor }) => {
+  const pathStyle = {
+    strokeDasharray: '100px, 100px',
+    strokeDashoffset: `${100 - percent}px`,
+  };
 
-const ProgressBar = ({ numerator, denominator }) => (
-  <Line
-    strokeWidth="2"
-    trailWidth="2"
-    strokeColor={'#f79122'}
-    percent={(numerator / denominator) * 100}
-  />
-);
+  const center = width / 2;
+  const right = 100 - width / 2;
+  const pathString = `M ${center},${center} L ${right},${center}`;
+  const viewBoxString = `0 0 100 ${width}`;
+
+  return (
+    <svg viewBox={viewBoxString} preserveAspectRatio="none">
+      <path
+        d={pathString}
+        strokeLinecap={'round'}
+        stroke={trailColor}
+        strokeWidth={width}
+        fillOpacity="0"
+      />
+      <path
+        d={pathString}
+        strokeLinecap={'round'}
+        stroke={strokeColor}
+        strokeWidth={width}
+        fillOpacity="0"
+        style={pathStyle}
+      />
+    </svg>
+  );
+};
+
+ProgressBar.defaultProps = {
+  percent: 0,
+  strokeColor: '#f79122',
+  trailColor: '#cacbcf',
+  width: 2,
+};
+
+ProgressBar.propTypes = {
+  percent: PropTypes.number,
+  strokeColor: PropTypes.string,
+  width: PropTypes.number,
+};
 
 export default ProgressBar;
