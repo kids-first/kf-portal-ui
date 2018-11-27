edee639f32a3c0215e72cc6fe22fbd5582076d23
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-11-27 10:00:16 -0500

    add progress bar base

--- src/chartkit/components/ProgressBar.js
+++ src/chartkit/components/ProgressBar.js
@@ -1,43 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const ProgressBar = ({ percent, width, onHover, onClick, strokeColor, trailColor }) => {
-  const pathStyle = {
-    strokeDasharray: '100px, 100px',
-    strokeDashoffset: `${100 - percent}px`,
-  };
+class ProgressBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false,
+    };
+  }
 
-  const center = width / 2;
-  const right = 100 - width / 2;
-  const pathString = `M ${center},${center} L ${right},${center}`;
-  const viewBoxString = `0 0 100 ${width}`;
+  render() {
+    const { percent, width, onHover, onClick, strokeColor, trailColor, style } = this.props;
 
-  return (
-    <svg viewBox={viewBoxString} preserveAspectRatio="none" onClick={onClick}>
-      <path
-        d={pathString}
-        strokeLinecap={'round'}
-        stroke={trailColor}
-        strokeWidth={width}
-        fillOpacity="0"
-      />
-      <path
-        d={pathString}
-        strokeLinecap={'round'}
-        stroke={strokeColor}
-        strokeWidth={width}
-        fillOpacity="0"
-        style={pathStyle}
-      />
-    </svg>
-  );
-};
+    const pathStyle = {
+      strokeDasharray: '100px, 100px',
+      strokeDashoffset: `${100 - percent}px`,
+    };
+
+    const center = width / 2;
+    const right = 100 - width / 2;
+    const pathString = `M ${center},${center} L ${right},${center}`;
+    const viewBoxString = `0 0 100 ${width}`;
+
+    return (
+      <svg
+        viewBox={viewBoxString}
+        preserveAspectRatio="none"
+        style={{ pointerEvents: 'all', ...style }}
+        onClick={onClick}
+        onMouseOver={e => this.setState({ hover: true })}
+        onMouseLeave={e => this.setState({ hover: false })}
+      >
+        <path
+          d={pathString}
+          strokeLinecap={'round'}
+          stroke={trailColor}
+          strokeWidth={width}
+          fillOpacity="0"
+        />
+        <path
+          d={pathString}
+          strokeLinecap={'round'}
+          stroke={strokeColor}
+          strokeWidth={width}
+          fillOpacity="0"
+          style={pathStyle}
+        />
+      </svg>
+    );
+  }
+}
 
 ProgressBar.defaultProps = {
   percent: 0,
   strokeColor: '#f79122',
   trailColor: '#cacbcf',
   width: 2,
+  style: {},
 };
 
 ProgressBar.propTypes = {
