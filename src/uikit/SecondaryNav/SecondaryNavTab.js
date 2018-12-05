e3880969709cc5de3b3b9116f96e3d7d75567e38
Jon Eubank <joneubank@gmail.com>
2018-12-05 12:46:33 -0500

    Add Secondary Nav as reusable component

---
+++ src/uikit/SecondaryNav/SecondaryNavTab.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+
+import { Link } from 'react-router-dom';
+
+import Column from '../Column';
+
+const SecondaryNavTab = ({ name, target, location: { hash } = {} }) => {
+  return (
+    <li>
+      <Column>
+        <Link to={`#${target}`} className={hash === `#${target}` ? 'active' : ''}>
+          {name}
+        </Link>
+      </Column>
+    </li>
+  );
+};
+
+SecondaryNavTab.PropTypes = {
+  name: PropTypes.string.isRequired,
+  target: PropTypes.string.isRequired,
+  location: PropTypes.objectOf({ hash: PropTypes.string }).isRequired,
+};
+
+export default SecondaryNavTab;
