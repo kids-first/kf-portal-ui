4dcfc68f357f1eaf859c0c2b38bdbdebefee5935
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-12-10 13:02:35 -0500

    add styled wrapper for react-table lib styles

--- src/uikit/DataTable/Table/Table.js
+++ src/uikit/DataTable/Table/Table.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactTable from 'react-table';
 
-const Table = ({ data, columns }) => <ReactTable data={data} columns={columns} />;
+import StyleWrapper from './StyleWrapper';
+
+const Table = ({ data, columns }) => (
+  <StyleWrapper>
+    <ReactTable data={data} columns={columns} />
+  </StyleWrapper>
+);
 
 export default Table;
