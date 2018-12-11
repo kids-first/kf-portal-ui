c59ea13cddc5ab29014863bf07c7aeb9595e3daf
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-12-11 11:58:21 -0500

    add client side pagination

--- src/uikit/DataTable/BaseDataTable.js
+++ src/uikit/DataTable/BaseDataTable.js
@@ -1,18 +1,39 @@
 import React, { Fragment } from 'react';
+import { compose, withState } from 'recompose';
 
 import Table from './Table';
 import TableToolbar from './TableToolbar';
 
-const BaseDataTable = ({ loading, data, columns, header = true }) => (
+const enhance = compose(
+  withState('pageSize', 'setPageSize', 10),
+  withState('pageIndex', 'setPageIndex', 0),
+);
+
+const BaseDataTable = ({
+  loading,
+  data,
+  columns,
+  pageSize,
+  setPageSize,
+  pageIndex,
+  setPageIndex,
+  header = true,
+}) => (
   <Fragment>
     {header ? (
-      <TableToolbar>
+      <TableToolbar pageSize={pageSize} page={pageIndex} total={data.length}>
         <div>filter</div>
         <div>export</div>
       </TableToolbar>
     ) : null}
-    <Table loading={loading} data={data} columns={columns} />
+    <Table
+      loading={loading}
+      data={data}
+      columns={columns}
+      onPageChange={pageIndex => setPageIndex(pageIndex)}
+      onPageSizeChange={(pageSize, pageIndex) => setPageSize(pageSize)}
+    />
   </Fragment>
 );
 
-export default BaseDataTable;
+export default enhance(BaseDataTable);
--- src/uikit/DataTable/Table/Table.js
+++ src/uikit/DataTable/Table/Table.js
@@ -4,20 +4,24 @@ import { withTheme } from 'emotion-theming';
 
 import StyleWrapper from './StyleWrapper';
 
-const Table = withTheme(({ loading, data, columns, styles, striped = false }) => (
-  <StyleWrapper styles={styles}>
-    <ReactTable
-      loading={loading}
-      data={data}
-      columns={columns}
-      sortable={false}
-      showPagination={false}
-      multiSort={false}
-      resizable={false}
-      className={true ? '-striped' : ''}
-      minRows={1}
-    />
-  </StyleWrapper>
-));
+const Table = withTheme(
+  ({ loading, data, columns, onPageChange, onPageSizeChange, styles, striped = false }) => (
+    <StyleWrapper styles={styles}>
+      <ReactTable
+        loading={loading}
+        data={data}
+        columns={columns}
+        showPagination={data.length > 10}
+        onPageChange={onPageChange}
+        onPageSizeChange={onPageSizeChange}
+        sortable={false}
+        multiSort={false}
+        resizable={false}
+        className={true ? '-striped' : ''}
+        minRows={1}
+      />
+    </StyleWrapper>
+  ),
+);
 
 export default Table;
