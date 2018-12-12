bafb3d52f3fae00a591728a3ff68865bd5fc5d91
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-12-12 03:09:44 -0500

    move utils to folder, add saveTSV

---
+++ src/uikit/DataTable/utils/saveTSV.js
@@ -0,0 +1,19 @@
+import download from './download';
+
+export default async function({ url, files = [], fileName, options = {} }) {
+  return download({
+    url,
+    method: 'POST',
+    ...options,
+    params: {
+      fileName,
+      files: files.map(({ columns, ...file }, i) => {
+        return {
+          ...file,
+          columns: columns.filter(c => c.show),
+        };
+      }),
+      ...options.params,
+    },
+  });
+}
