c3c541fc941b20ac4c16f6b40971001efa35a37e
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2018-12-19 12:18:42 -0500

    fix build errors, remove unused code

--- src/uikit/DataTable/ToolbarButtons/Export.js
+++ src/uikit/DataTable/ToolbarButtons/Export.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { ToolbarItem, ToolbarButton, FileDownloadIcon } from './styles';
-import saveTSV from '../utils/saveTSV';
 
-const Export = ({ exporter = saveTSV, ...props }) => (
+const Export = ({ exporter = x => x, ...props }) => (
   <ToolbarItem {...props}>
     <FileDownloadIcon width="12" height="12px" fill="#008299" style={{ marginRight: '7px' }} />
     <ToolbarButton onClick={x => x}>EXPORT</ToolbarButton>
