c69825e1cf6a7bfc95cfef14358e3eedbc79cbec
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-01-08 09:58:30 -0500

    fix table

---
+++ src/components/EntityPage/File/sequencingProperties.js
@@ -0,0 +1,19 @@
+export const sequencingReadProperties = data => {
+  const experiments = data.sequencing_experiments.hits.edges[0].node;
+
+  const {
+    max_insert_size: maxInsertSize,
+    total_reads: totalReads,
+    mean_depth: meanDepth,
+    mean_insert_size: meanInsertSize,
+    mean_read_length: meanReadLength,
+  } = experiments;
+
+  return [
+    { description: 'Total Reads', value: totalReads },
+    { description: 'Max Insert Size', value: maxInsertSize },
+    { description: 'Mean Depth', value: meanDepth },
+    { description: 'Mean Insert Size', value: meanInsertSize },
+    { description: 'Mean Read Length', value: meanReadLength },
+  ];
+};
