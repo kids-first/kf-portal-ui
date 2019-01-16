564ced1b76d70d9428c3b736e8fa3d568b370685
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-01-16 11:41:02 -0500

    fix click through

--- src/uikit/DataTable/ToolbarButtons/ColumnFilter.js
+++ src/uikit/DataTable/ToolbarButtons/ColumnFilter.js
@@ -1,44 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Downshift from 'downshift';
-
 import { ToolbarItem, DropdownContent, ToolbarButton, ColumnIcon } from './styles';
 
-const ColumnFilter = ({ onChange, columns, ...props }) => (
-  <Downshift
-    itemToString={item => item.Header}
-    aria-label={`Select columns`}
-    onChange={onChange}
-    selectedItem={columns.filter(col => col.show)}
-  >
-    {({ getButtonProps, getItemProps, isOpen, selectedItem }) => (
-      <div style={{ position: 'relative', whiteSpace: 'nowrap' }}>
-        <ToolbarItem {...props}>
-          <ToolbarButton aria-label={`Show columns to select`} {...getButtonProps()}>
-            COLUMNS <ColumnIcon isOpen={isOpen} width="9px" height="9px" />
-          </ToolbarButton>
-          {!isOpen ? null : (
-            <DropdownContent>
-              {columns.map((item, index) => (
-                <div
-                  className="dropDownContentElement"
-                  key={item.index}
-                  {...getItemProps({ item, index })}
-                >
-                  {item.Header}
-                  <input
-                    readOnly
-                    type="checkbox"
-                    checked={selectedItem.indexOf(item) > -1}
-                    aria-label={`Select column ${item.Header}`}
-                  />
-                </div>
-              ))}
-            </DropdownContent>
-          )}
-        </ToolbarItem>
-      </div>
-    )}
-  </Downshift>
-);
+class ColumnFilter extends Component {
+  state = { isOpen: false };
+
+  // keep dropdown open for multi select, close on click away
+  handleStateChange = changes => {
+    const { type } = changes;
+    this.setState({
+      isOpen: type === Downshift.stateChangeTypes.mouseUp ? false : true,
+    });
+  };
+
+  render() {
+    const { onChange, columns, ...props } = this.props;
+    return (
+      <Downshift
+        itemToString={item => item.Header}
+        aria-label={`Select columns`}
+        onChange={onChange}
+        selectedItem={columns.filter(col => col.show)}
+        onStateChange={this.handleStateChange}
+        isOpen={this.state.isOpen}
+      >
+        {({ getButtonProps, getItemProps, isOpen, selectedItem }) => (
+          <div style={{ position: 'relative', whiteSpace: 'nowrap' }}>
+            <ToolbarItem {...props}>
+              <ToolbarButton aria-label={`Show columns to select`} {...getButtonProps()}>
+                COLUMNS <ColumnIcon isOpen={isOpen} width="9px" height="9px" />
+              </ToolbarButton>
+              {!isOpen ? null : (
+                <DropdownContent>
+                  {columns.map((item, index) => (
+                    <div
+                      className="dropDownContentElement"
+                      key={item.index}
+                      {...getItemProps({ item, index })}
+                    >
+                      {item.Header}
+                      <input
+                        readOnly
+                        type="checkbox"
+                        checked={selectedItem.indexOf(item) > -1}
+                        aria-label={`Select column ${item.Header}`}
+                      />
+                    </div>
+                  ))}
+                </DropdownContent>
+              )}
+            </ToolbarItem>
+          </div>
+        )}
+      </Downshift>
+    );
+  }
+}
 
 export default ColumnFilter;
