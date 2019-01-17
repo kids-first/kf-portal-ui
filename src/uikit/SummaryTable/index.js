370f1dc74fbdc3219e57f9d6ec538143b3721fd0
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-01-17 11:05:15 -0500

    add responsive style

--- src/uikit/SummaryTable/index.js
+++ src/uikit/SummaryTable/index.js
@@ -2,17 +2,50 @@ import React from 'react';
 import styled, { css } from 'react-emotion';
 import PropTypes from 'prop-types';
 import Row from 'uikit/Row';
+import { mq } from 'uikit/BreakpointHelper';
+import Column from '../Column';
 
-const SummaryTableWrapper = styled('div')``;
+const SummaryTableWrapper = styled('div')`
+flex-direction: column;
+border: 1px solid ${({ theme }) => theme.greyScale5};
+> div {
+  border: none;
+}
+}
+  ${mq[1]} {
+  display: flex;
+  flex-direction: row;
+  border:none;
+
+  > div {
+    border: 1px solid ${({ theme }) => theme.greyScale5};
+    margin-right: 20px;
+  }
+
+  > div:last-child {
+    margin-right: 0;
+  }
+`;
+
+const Summary = styled(Column)``;
+
 const SummaryRow = styled(Row)`
-  background-color: ${({ index, theme }) => (index % 2 === 0 ? theme.backgroundGrey : '#fff')}
-  justify-content: space-between;
+  background-color: ${({ index, theme }) => (index % 2 === 0 ? theme.backgroundGrey : '#fff')};
 `;
 
 const cell = props => css`
   padding: 7px 12px;
   font-size: 13px;
   text-align: left;
+`;
+
+const SummaryTitle = styled('div')`
+  ${cell};
+  color: ${({ theme }) => theme.secondary};
+  font-weight: 600;
+  font-family: ${({ theme }) => theme.fonts.default};
+  min-width: 180px;
+  padding-right: 0;
 `;
 
 const SummaryContent = styled('div')`
@@ -22,24 +55,25 @@ const SummaryContent = styled('div')`
   margin-right: 40px;
 `;
 
-const SummaryTitle = styled('div')`
-  ${cell};
-  color: ${({ theme }) => theme.secondary};
-  font-weight: 600;
-  font-family: ${({ theme }) => theme.fonts.default};
-  padding-right: 47px;
-  max-width: 150px;
-`;
-
-const SummaryTable = ({ rows }) => {
+const SummaryTable = ({ rows, maxRows }) => {
   return (
     <SummaryTableWrapper>
-      {rows.map((row, i) => (
-        <SummaryRow index={i} key={i}>
-          <SummaryTitle>{row.title}</SummaryTitle>
-          <SummaryContent>{row.summary}</SummaryContent>
-        </SummaryRow>
-      ))}
+      <Summary>
+        {rows.map((row, i) => (
+          <SummaryRow index={i} key={i}>
+            <SummaryTitle>{row.title}</SummaryTitle>
+            <SummaryContent>{row.summary}</SummaryContent>
+          </SummaryRow>
+        ))}
+      </Summary>
+      <Summary>
+        {rows.map((row, i) => (
+          <SummaryRow index={i} key={i}>
+            <SummaryTitle>{row.title}</SummaryTitle>
+            <SummaryContent>{row.summary}</SummaryContent>
+          </SummaryRow>
+        ))}
+      </Summary>
     </SummaryTableWrapper>
   );
 };
