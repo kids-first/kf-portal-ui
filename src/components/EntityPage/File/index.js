d16b4b267be865820595d19da96b59b89b54514d
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-01-21 02:33:04 -0500

    change padding to percent

--- src/components/EntityPage/File/index.js
+++ src/components/EntityPage/File/index.js
@@ -66,10 +66,7 @@ const fileQuery = `query ($sqon: JSON) {
           modified_at
           reference_genome
           size
-          instrument_models
-          experiment_strategies 
-          is_paired_end
-          platforms
+      
           sequencing_experiments {
             hits {
               edges {
