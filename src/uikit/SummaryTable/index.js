93d9950aa084370d8f2ea3ce1e1a14f5f27cf108
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-01-21 02:27:25 -0500

    responsive summary table

--- src/uikit/SummaryTable/index.js
+++ src/uikit/SummaryTable/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import styled, { css } from 'react-emotion';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
-import Row from 'uikit/Row';
 import { mq } from 'uikit/BreakpointHelper';
-import Column from '../Column';
+import { Visible } from 'react-grid-system';
 
 const SummaryTableWrapper = styled('div')`
   height: 100%;
@@ -34,20 +33,18 @@ const SummaryTableWrapper = styled('div')`
   }
 `;
 
-const Table = styled(Column)`
+const Table = styled('div')`
   flex: 1;
-`;
-
-const SummaryRow = styled(Row)`
-  background-color: ${({ index, theme }) => (index % 2 === 0 ? theme.backgroundGrey : '#fff')};
+  align-sef: flex-start;
+  display: grid;
+  grid-template-columns: auto 1fr;
 `;
 
 const cell = props => css`
   padding: 7px 12px;
   font-size: 13px;
   text-align: left;
-  flex: 0 1 50%;
-  min-width: 50%;
+  background-color: ${props.index % 2 === 0 ? props.theme.backgroundGrey : '#fff'};
 `;
 
 const SummaryTitle = styled('div')`
@@ -56,37 +53,49 @@ const SummaryTitle = styled('div')`
   font-weight: 600;
   font-family: ${({ theme }) => theme.fonts.default};
   padding-right: 0;
-  min-width: 200px;
 `;
 
 const SummaryContent = styled('div')`
   ${cell};
   color: ${({ theme }) => theme.greyScale1};
   font-family: ${({ theme }) => theme.fonts.details};
-  min-width: 0;
+  min-width: 100px;
   overflow-wrap: break-word;
   word-wrap: break-word;
+  padding-left: 45px;
 `;
 
-const SummaryTable = ({ rows, maxRows = 6 }) => {
-  // break into seperate tables based on maxRows
-  const tables = _.chunk(rows, maxRows);
+class SummaryTable extends React.Component {
+  renderTables = amount => {
+    const { rows } = this.props;
+    const numOfTables = amount === 1 ? rows.length : rows.length / amount;
+    const tables = _.chunk(rows, numOfTables);
 
-  return (
-    <SummaryTableWrapper>
-      {tables.map(table => (
-        <Table>
-          {table.map((row, i) => (
-            <SummaryRow index={i} key={i}>
-              <SummaryTitle>{row.title}</SummaryTitle>
-              <SummaryContent>{row.summary}</SummaryContent>
-            </SummaryRow>
-          ))}
-        </Table>
-      ))}
-    </SummaryTableWrapper>
-  );
-};
+    return tables.map(table => (
+      <Table>
+        {table.map((row, i) => (
+          <React.Fragment>
+            <SummaryTitle index={i}>{row.title}</SummaryTitle>
+            <SummaryContent index={i}>{row.summary}</SummaryContent>
+          </React.Fragment>
+        ))}
+      </Table>
+    ));
+  };
+
+  render() {
+    return (
+      <SummaryTableWrapper>
+        <Visible xs sm>
+          {this.renderTables(1)}
+        </Visible>
+        <Visible md lg xl>
+          {this.renderTables(2)}
+        </Visible>
+      </SummaryTableWrapper>
+    );
+  }
+}
 
 SummaryTable.propTypes = {
   rows: PropTypes.arrayOf(
