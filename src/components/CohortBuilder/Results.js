61b92c9aae0ea7b5cafd6bf5792e430d718d4071
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-01-24 15:59:37 -0500

    add view links

--- src/components/CohortBuilder/Results.js
+++ src/components/CohortBuilder/Results.js
@@ -3,25 +3,50 @@ import { compose, withState } from 'recompose';
 import ContentSection from './ContentSection';
 import ContentBar from './ContentBar';
 import Summary from './Summary';
+import Row from 'uikit/Row';
+import ViewLink from 'uikit/ViewLink';
+import styled from 'react-emotion';
+import { H2 } from 'uikit/Headings';
 
 const SUMMARY = 'summary';
 const TABLE = 'table';
 
+const ViewLinks = styled(Row)`
+  > div:not(:last-child) {
+    margin-right: 30px;
+  }
+`;
+
+const Left = styled(Row)``;
+
+const Heading = styled(H2)`
+  color: #2b388f;
+`;
+
+const ActiveView = styled('div')`
+  width: 100%;
+  padding: 0 30px;
+`;
+
 const Results = ({ activeView, setActiveView }) => (
-  <ContentSection>
+  <React.Fragment>
     <ContentBar>
-      <div>
-        <h2>Cohort Results</h2>
+      <Left>
+        <Heading>Cohort Results</Heading>
         <div>23232 Participants</div>
         <div> View 24242,222 Files</div>
-      </div>
-      <div>
-        <div onClick={() => setActiveView(SUMMARY)}>Summary View</div>
-        <div onClick={() => setActiveView(TABLE)}>Table View</div>
-      </div>
+      </Left>
+      <ViewLinks>
+        <ViewLink onClick={() => setActiveView(SUMMARY)} active={activeView === SUMMARY}>
+          Summary View
+        </ViewLink>
+        <ViewLink onClick={() => setActiveView(TABLE)} active={activeView === TABLE}>
+          Table View
+        </ViewLink>
+      </ViewLinks>
     </ContentBar>
-    {activeView === SUMMARY ? <Summary /> : <div>Tabel</div>}
-  </ContentSection>
+    <ActiveView>{activeView === SUMMARY ? <Summary /> : <div>Tabele</div>}</ActiveView>
+  </React.Fragment>
 );
 
 export default compose(withState('activeView', 'setActiveView', SUMMARY))(Results);
