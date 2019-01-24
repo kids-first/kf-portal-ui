4ec82f97f398166f5e3adf3123a233829fa39177
Minh <hlminh2000@gmail.com>
2019-01-24 14:59:19 -0500

    adds global css to storybook

--- stories/index.js
+++ stories/index.js
@@ -2,3 +2,9 @@ import './StorybookLogin';
 import './uikit';
 import './ChartKit';
 import './CohortBuilder';
+
+/**
+ * This is to match up the portal's global css
+ */
+import 'index.css';
+import '@arranger/components/public/themeStyles/beagle/beagle.css';
