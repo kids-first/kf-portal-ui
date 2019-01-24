88679786327efcee077adef3e2736fb147a0facc
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-01-24 16:11:04 -0500

    add results layout

--- src/components/CohortBuilder/Summary/index.js
+++ src/components/CohortBuilder/Summary/index.js
@@ -3,52 +3,59 @@ import styled from 'react-emotion';
 import Pie from './pie';
 import { demographicPiesMock } from './pie';
 import Card from 'uikit/Card';
-
-const CardGrid = styled('div')`
-  width: 100%;
-  display: grid;
-  grid-template-rows: repeat(2, 1fr);
-  grid-template-columns: repeat(4, 1fr);
-  grid-gap: 9px;
-`;
+import { Col, Row } from 'react-grid-system';
 
 const CardSlot = styled(Card)`
   height: 305px;
-
-  &.last-full-height {
-    height: 100%;
-    grid-column: 4/5;
-    grid-row: 1/3;
-  }
 `;
 
+const LongCard = styled(Card)`
+  height: 100%;
+`;
+
+const md = 4;
+const lg = 4;
+
 const Summary = () => (
-  <CardGrid>
-    <CardSlot>
-      <Pie data={demographicPiesMock.ethnicity} colors={'purples'} />
-    </CardSlot>
-    <CardSlot>
-      {' '}
-      <Pie data={demographicPiesMock.ethnicity} colors={'purples'} />
-    </CardSlot>
-    <CardSlot>
-      {' '}
-      <Pie data={demographicPiesMock.ethnicity} colors={'purples'} />
-    </CardSlot>
-    <CardSlot>
-      {' '}
-      <Pie data={demographicPiesMock.ethnicity} colors={'purples'} />
-    </CardSlot>
-    <CardSlot>
-      {' '}
-      <Pie data={demographicPiesMock.ethnicity} colors={'purples'} />
-    </CardSlot>
-    <CardSlot>
-      {' '}
-      <Pie data={demographicPiesMock.ethnicity} colors={'purples'} />
-    </CardSlot>
-    <CardSlot className="last-full-height">long</CardSlot>
-  </CardGrid>
+  <Row nogutter>
+    <Col sm={12} md={9} lg={9}>
+      <Row nogutter>
+        <Col sm={12} md={md} lg={lg}>
+          <CardSlot title="Overall Survival">
+            <Pie data={demographicPiesMock.ethnicity} colors={'purples'} />
+          </CardSlot>
+        </Col>
+        <Col sm={12} md={md} lg={lg}>
+          <CardSlot title="Studies">
+            <Pie data={demographicPiesMock.ethnicity} colors={'purples'} />
+          </CardSlot>
+        </Col>
+        <Col sm={12} md={md} lg={lg}>
+          <CardSlot title="Most Frequent Diagnoses">
+            <Pie data={demographicPiesMock.ethnicity} colors={'purples'} />
+          </CardSlot>
+        </Col>
+        <Col sm={12} md={md} lg={lg}>
+          <CardSlot>
+            <Pie data={demographicPiesMock.ethnicity} colors={'purples'} />
+          </CardSlot>
+        </Col>
+        <Col sm={12} md={md} lg={lg}>
+          <CardSlot title="File Breakdown">
+            <Pie data={demographicPiesMock.ethnicity} colors={'purples'} />
+          </CardSlot>
+        </Col>
+        <Col sm={12} md={md} lg={lg}>
+          <CardSlot title="Age at Diagnosis">
+            <Pie data={demographicPiesMock.ethnicity} colors={'purples'} />
+          </CardSlot>
+        </Col>
+      </Row>
+    </Col>
+    <Col sm={12} md={3} lg={3}>
+      <LongCard title="Phenotypes">Long Card</LongCard>
+    </Col>
+  </Row>
 );
 
 export default Summary;
