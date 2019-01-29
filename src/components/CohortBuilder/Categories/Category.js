706ad7431c44d87b19b5514f461ed5c50d9b56ad
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-01-29 16:05:15 -0500

    add cb for on apply to show active filters

--- src/components/CohortBuilder/Categories/Category.js
+++ src/components/CohortBuilder/Categories/Category.js
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 import styled from 'react-emotion';
 import Column from 'uikit/Column';
 import Dropdown from 'uikit/Dropdown';
-import { compose } from 'recompose';
+import { compose, withState } from 'recompose';
 import { items } from './mocks';
 import { withDropdownMultiPane } from 'uikit/Dropdown';
 import Filter from './Filter';
 import CategoryRow from './CategoryRow';
+import { union } from 'lodash';
 
 const Container = styled(Column)`
   flex: 1;
@@ -74,6 +75,8 @@ const Category = ({
   activeIndex,
   setExpanded,
   showExpanded,
+  setAppliedFilters,
+  appliedFilters,
 }) => (
   <React.Fragment>
     <Dropdown
@@ -90,12 +93,20 @@ const Category = ({
         setExpanded,
         showExpanded,
         showArrow: false,
-        items: items.map((item, i) => <CategoryRow active={true} title={item.name} />),
+        items: items.map((item, i) => (
+          <CategoryRow active={appliedFilters.includes(i)} title={item.name} />
+        )),
         expandedItems: items.map((item, i) => (
           <Filter
             onCancel={toggleExpandedDropdown}
             onBack={toggleExpanded}
-            onApply={toggleExpandedDropdown}
+            onApply={active => {
+              toggleExpanded();
+              const newAppliedFilters = active
+                ? union([i], appliedFilters)
+                : appliedFilters.filter(af => af != i);
+              setAppliedFilters(newAppliedFilters);
+            }}
           >
             {item.expanded}
           </Filter>
@@ -118,4 +129,7 @@ Category.propTypes = {
   // /color: PropTypes.string.isRequired,
 };
 
-export default compose(withDropdownMultiPane)(Category);
+export default compose(
+  withDropdownMultiPane,
+  compose(withState('appliedFilters', 'setAppliedFilters', [])),
+)(Category);
