771089db3fcf05fb25c6689cb034da068d57eaed
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-02-04 15:11:17 -0500

    add storybook router decorator

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,10 @@
 import 'babel-polyfill';
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import StoryRouter from 'storybook-react-router';
 
 const loadStories = () => {
   configure(() => require('../stories/index'), module);
 };
 
+addDecorator(StoryRouter());
 loadStories();
