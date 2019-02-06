076cbdb95a9233ad133456cd121aba3648f3d962
Julien Bérubé <julien.berube@gmail.com>
2019-02-06 15:39:20 -0500

    fixes #1107 - column selection was beneath the footer (#1175)

--- src/uikit/DataTable/ToolbarButtons/styles.js
+++ src/uikit/DataTable/ToolbarButtons/styles.js
@@ -47,7 +47,7 @@ export const DropdownContent = styled('div')`
   position: absolute;
   background: white;
   min-width: 100%;
-  z-index: 1;
+  z-index: 200;
   border: 1px solid rgba(0, 0, 0, 0.05);
   box-sizing: border-box;
   cursor: pointer;
