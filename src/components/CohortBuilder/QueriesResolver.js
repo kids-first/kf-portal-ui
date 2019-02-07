00d9044c21bbe85c068ff4a9e4edda37a8859390
Ciarán Schütte <ciaranschutte@gmail.com>
2019-02-07 11:11:28 -0500

    Feature - Demographic Chat Data Provider (#1178)
    * add query resolver
    
    * move demographic chart to own file
    
    * update query resolver
    
    * demographic data transform
    
    * add family composition
    
    * pass data in as array
    
    * remove console.log
    
    * remove mock

---
+++ src/components/CohortBuilder/QueriesResolver.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Component from 'react-component-component';
+import graphql from 'services/arranger';
+
+const QueriesResolver = ({ children, queries, api }) => (
+  <Component
+    initialState={{ data: null, isLoading: true, error: null }}
+    didMount={({ setState }) => {
+      const { query, variables, transform = x => x } = queries[0];
+
+      graphql(api)({
+        query,
+        variables,
+      })
+        .then(data => transform(data))
+        .then(data => setState({ data: data, isLoading: false }))
+        .catch(err => setState({ isLoading: false, error: err }));
+    }}
+  >
+    {({ state }) => children(state)}
+  </Component>
+);
+
+export default QueriesResolver;
+
+QueriesResolver.propTypes = {
+  api: PropTypes.isRequired,
+  queries: PropTypes.arrayOf(
+    PropTypes.shape({
+      query: PropTypes.string.isRequired,
+      variables: PropTypes.isRequired,
+      transform: PropTypes.func,
+    }),
+  ).isRequired,
+};
