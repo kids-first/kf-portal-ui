37af1458992ec795044246bafa28e3101745caa9
Minh <hlminh2000@gmail.com>
2019-02-07 17:03:14 -0500

    strips out the field filters container component

--- src/components/CohortBuilder/Queries/index.js
+++ src/components/CohortBuilder/Queries/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'react-emotion';
+import { omit } from 'lodash';
 import { memoize } from 'lodash';
 import AdvancedSqonBuilder from '@arranger/components/dist/AdvancedSqonBuilder';
 import ExtendedMappingProvider from '@arranger/components/dist/utils/ExtendedMappingProvider';
 import { withApi } from 'services/api';
 import { arrangerProjectId } from 'common/injectGlobals';
+import { FieldFilterContainer } from '../common';
 
 const extendedMappingToDisplayNameMap = memoize(extendedMapping =>
   extendedMapping.reduce((acc, { field, displayName }) => {
@@ -22,7 +24,7 @@ const SqonBuilderContainer = styled('div')`
 /**
  * this component should mimic the AdvancedSqonBuilder's API directly
  **/
-export default withApi(({ api, ...rest }) => {
+const SqonBuilder = withApi(({ api, ...rest }) => {
   const ARRANGER_API_PARTICIPANT_INDEX_NAME = 'participant';
   const onSqonRemoveClick = ({ indexToRemove, dependentIndices }) => {
     if (dependentIndices.length) {
@@ -50,6 +52,9 @@ export default withApi(({ api, ...rest }) => {
               arrangerProjectId={arrangerProjectId}
               arrangerProjectIndex={ARRANGER_API_PARTICIPANT_INDEX_NAME}
               getSqonDeleteConfirmation={onSqonRemoveClick}
+              FieldOpModifierContainer={props => (
+                <FieldFilterContainer showHeader={false} {...props} />
+              )}
               fieldDisplayNameMap={extendedMappingToDisplayNameMap(extendedMapping)}
               {...rest}
             />
@@ -59,3 +64,13 @@ export default withApi(({ api, ...rest }) => {
     </SqonBuilderContainer>
   );
 });
+
+SqonBuilder.propTypes = omit(AdvancedSqonBuilder.propTypes, [
+  'api',
+  'arrangerProjectId',
+  'arrangerProjectIndex',
+  'getSqonDeleteConfirmation',
+  'fieldDisplayNameMap',
+]);
+
+export default SqonBuilder;
---
+++ src/components/CohortBuilder/common.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled from 'react-emotion';
+import Row from 'uikit/Row';
+
+import { TealActionButton, WhiteButton } from 'uikit/Button';
+import LeftIcon from 'react-icons/lib/fa/angle-left';
+
+const FilterCont = styled('div')`
+  color: black;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  left: 0;
+  top: 100%;
+  cursor: default;
+  text-align: left;
+  border-radius: 5px;
+  box-shadow: 0 0 5.9px 0.1px #bbbbbb;
+  border: 1px solid #e0e1e6;
+  z-index: 1;
+  min-width: 315px;
+`;
+
+const Header = styled('div')`
+  background-color: white;
+  border-bottom: 1px solid #d4d6dd;
+`;
+
+const Content = styled('div')`
+  flex: 1;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  &.filterContainer {
+    border: none;
+    box-shadow: none;
+    margin: 0px;
+    width: 100%;
+    border-radius: 0px;
+  }
+`;
+
+const Footer = styled(Row)`
+  justify-content: space-between;
+  background-color: #e0e1e6;
+`;
+
+const WButton = styled(WhiteButton)`
+  margin: 5px;
+`;
+
+const TealButton = styled(TealActionButton)`
+  margin: 5px;
+`;
+
+export const FieldFilterContainer = ({
+  children,
+  onSubmit: onSqonSubmit,
+  onCancel,
+  onBack,
+  showHeader = true,
+}) => (
+  <FilterCont>
+    {showHeader && (
+      <Header>
+        <WButton onClick={onBack}>
+          <LeftIcon />
+          Back
+        </WButton>
+      </Header>
+    )}
+    <Content className="filterContainer">{children}</Content>
+    <Footer>
+      <WButton onClick={onCancel}>Cancel</WButton>
+      <TealButton onClick={onSqonSubmit}>Apply</TealButton>
+    </Footer>
+  </FilterCont>
+);
