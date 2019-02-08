335c10b4267c1bc20ca9901aedcda2a08a7786f3
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-02-08 12:27:52 -0500

    pass loading state

--- src/components/CohortBuilder/QueriesResolver.js
+++ src/components/CohortBuilder/QueriesResolver.js
@@ -56,49 +56,6 @@ class QueriesResolver extends Component {
   }
 }
 
-class QueriesResolver extends Component {
-  state = { data: null, isLoading: true, error: null };
-
-  componentDidMount() {
-    this.fetchQuery();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (!isEqual(this.props.queries, prevProps.queries)) {
-      this.fetchQuery();
-    }
-  }
-
-  fetchQuery = () => {
-    const { queries, api } = this.props;
-
-    const body = JSON.stringify(
-      queries.map(q => ({
-        query: q.query,
-        variables: q.variables,
-      })),
-    );
-
-    api({
-      method: 'POST',
-      url: urlJoin(arrangerApiRoot, `/${arrangerProjectId}/graphql`),
-      body,
-    })
-      .then(data =>
-        data.map((d, i) => {
-          const transform = queries[i].transform;
-          return transform ? transform(d) : d;
-        }),
-      )
-      .then(data => this.setState({ data: data, isLoading: false }))
-      .catch(err => this.setState({ isLoading: false, error: err }));
-  };
-
-  render() {
-    return <div>{this.props.children({ ...this.state, ...this.props })}</div>;
-  }
-}
-
 export default QueriesResolver;
 
 QueriesResolver.propTypes = {
