53a59ac6bb6b77816992cc6be212834380925738
Nyan Of The Moon <nyanofthemoon@gmail.com>
2019-02-13 12:06:23 -0500

    removed unused

--- src/uikit/Table/index.js
+++ src/uikit/Table/index.js
@@ -7,10 +7,3 @@ export const TableHeader = applyDefaultStyles(styled('th')`
   font-weight: 700;
   color: ${({ theme }) => theme.secondary};
 `);
-
-export const TableRow = applyDefaultStyles(styled('tr')`
-  font-size: 13px;
-  font-family: ${({ theme }) => theme.fonts.default};
-  font-weight: 700;
-  color: ${({ theme }) => theme.secondary};
-`);
