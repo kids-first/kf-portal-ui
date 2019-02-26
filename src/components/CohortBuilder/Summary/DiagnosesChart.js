a19ba3500e077e0df8b1b7f291e0c38e08986355
Ciarán Schütte <ciaranschutte@gmail.com>
2019-02-26 11:09:56 -0500

    Feature/#1219 data fetching diagnoses card (#1243)
    * move diagnoses chart into single file and add data fetching
    
    * cleanup. add order and ids to diagnoses chart data
    
    * orderby client side for certainty
    
    * pass diagnosis as variable instead of interpolate

---
+++ src/components/CohortBuilder/Summary/DiagnosesChart.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import { withTheme } from 'emotion-theming';
+import { compose } from 'recompose';
+import { BarChartContainer, CardSlot } from './index';
+import HorizontalBar from 'chartkit/components/HorizontalBar';
+import gql from 'graphql-tag';
+import _, { get, startCase } from 'lodash';
+import QueriesResolver from '../QueriesResolver';
+import { withApi } from 'services/api';
+import LoadingSpinner from 'uikit/LoadingSpinner';
+
+const mostFrequentDiagnosisTooltip = data => {
+  const participants = data.familyMembers + data.probands;
+  return `${participants.toLocaleString()} Participant${participants > 1 ? 's' : ''}`;
+};
+
+const toSingleDiagQueries = ({ topDiagnoses, sqon }) =>
+  topDiagnoses.map(diagnosis => ({
+    query: gql`
+      query($sqon: JSON, $diagnosis: String) {
+        participant {
+          familyMembers: aggregations(
+            aggregations_filter_themselves: true
+            filters: {
+              op: "and"
+              content: [
+                $sqon
+                { op: "in", content: { field: "diagnoses.diagnosis", value: [$diagnosis] } }
+                { op: "in", content: { field: "is_proband", value: ["false"] } }
+              ]
+            }
+          ) {
+            diagnoses__diagnosis {
+              buckets {
+                doc_count
+                key
+              }
+            }
+          }
+          proband: aggregations(
+            aggregations_filter_themselves: true
+            filters: {
+              op: "and"
+              content: [
+                $sqon
+                { op: "in", content: { field: "diagnoses.diagnosis", value: [$diagnosis] } }
+                { op: "in", content: { field: "is_proband", value: ["true"] } }
+              ]
+            }
+          ) {
+            diagnoses__diagnosis {
+              buckets {
+                doc_count
+                key
+              }
+            }
+          }
+        }
+      }
+    `,
+    variables: { sqon, diagnosis },
+    transform: data => ({
+      label: startCase(diagnosis),
+      probands: get(
+        data,
+        'data.participant.familyMembers.diagnoses__diagnosis.buckets[0].doc_count',
+        0,
+      ),
+      familyMembers: get(
+        data,
+        'data.participant.proband.diagnoses__diagnosis.buckets[0].doc_count',
+        0,
+      ),
+    }),
+  }));
+
+const DiagnosesChart = ({ topDiagnoses, sqon, theme, api }) => (
+  <QueriesResolver api={api} queries={toSingleDiagQueries({ topDiagnoses, sqon })}>
+    {({ isLoading, data }) => (
+      <CardSlot title="Most Frequent Diagnoses">
+        {isLoading ? (
+          <LoadingSpinner color={theme.greyScale11} size={'50px'} />
+        ) : !data ? (
+          <div>No data</div>
+        ) : (
+          <BarChartContainer>
+            <HorizontalBar
+              data={_(data)
+                .sortBy(d => d.probands + d.familyMembers)
+                .map((d, i) => ({ ...d, id: i }))
+                .value()}
+              indexBy="label"
+              keys={['probands', 'familyMembers']}
+              tooltipFormatter={mostFrequentDiagnosisTooltip}
+              tickInterval={4}
+              colors={[theme.chartColors.blue, theme.chartColors.purple]}
+              xTickTextLength={28}
+              legends={[
+                { title: 'Probands', color: theme.chartColors.blue },
+                { title: 'Family Members', color: theme.chartColors.purple },
+              ]}
+            />
+          </BarChartContainer>
+        )}
+      </CardSlot>
+    )}
+  </QueriesResolver>
+);
+
+/**
+ * Get the top 10 diagnoses overall
+ * Then get the proband/family member breakdown
+ */
+export const diagnosesQuery = sqon => ({
+  query: gql`
+    query($sqon: JSON) {
+      participant {
+        aggregations(filters: $sqon) {
+          diagnoses__diagnosis {
+            buckets {
+              doc_count
+              key
+            }
+          }
+        }
+      }
+    }
+  `,
+  variables: { sqon },
+  transform: data => {
+    const buckets = get(data, 'data.participant.aggregations.diagnoses__diagnosis.buckets');
+    return _(buckets)
+      .orderBy(bucket => bucket.doc_count, 'desc')
+      .take(10)
+      .map(diag => diag.key)
+      .value();
+  },
+});
+
+export default compose(
+  withApi,
+  withTheme,
+)(DiagnosesChart);
--- src/components/CohortBuilder/Summary/index.js
+++ src/components/CohortBuilder/Summary/index.js
@@ -3,7 +3,6 @@ import styled from 'react-emotion';
 import { compose } from 'recompose';
 import { withTheme } from 'emotion-theming';
 import LoadingSpinner from 'uikit/LoadingSpinner';
-
 import { topDiagnosesBarMock, studiesBarMock, fileBreakdownMock, survivalPlotMock } from './mock';
 import Card from 'uikit/Card';
 import MultiHeader from 'uikit/Multicard/MultiHeader';
@@ -15,11 +14,7 @@ import DemographicChart, { demographicQuery } from './DemographicChart';
 import FileBreakdown from './FileBreakdown';
 import AgeDiagChart, { ageDiagQuery } from './AgeDiagChart';
 import SurvivalChart from './SurvivalChart';
-
-const mostFrequentDiagnosisTooltip = data => {
-  const participants = data.familyMembers + data.probands;
-  return `${participants.toLocaleString()} Participant${participants > 1 ? 's' : ''}`;
-};
+import DiagnosesChart, { diagnosesQuery } from './DiagnosesChart';
 
 const studiesToolTip = data => {
   const { familyMembers, probands, name } = data;
@@ -40,7 +35,7 @@ const sortDescParticipant = (a, b) => {
   return aTotal <= bTotal ? -1 : 1;
 };
 
-const BarChartContainer = styled('div')`
+export const BarChartContainer = styled('div')`
   position: absolute;
   left: 0px;
   right: 0px;
@@ -48,7 +43,7 @@ const BarChartContainer = styled('div')`
   bottom: 0px;
 `;
 
-const CardSlot = styled(Card)`
+export const CardSlot = styled(Card)`
   height: 305px;
 `;
 
@@ -80,16 +75,24 @@ const enhance = compose(
   withTheme,
 );
 
+const defaultSqon = {
+  op: 'and',
+  content: [],
+};
+
 const multiHeader = (
   <MultiHeader
     headings={[{ title: 'Studies', badge: 7 }, { title: 'Participants', badge: 6155 }]}
   />
 );
 
-const Summary = ({ theme, sqon, api }) => (
-  <QueriesResolver api={api} queries={[demographicQuery(sqon), ageDiagQuery(sqon)]}>
+const Summary = ({ theme, sqon = defaultSqon, api }) => (
+  <QueriesResolver
+    api={api}
+    queries={[demographicQuery(sqon), ageDiagQuery(sqon), diagnosesQuery(sqon)]}
+  >
     {({ isLoading, data }) => {
-      const [demographicData, ageDiagData] = data || [];
+      const [demographicData, ageDiagData, topDiagnosesData] = data || [];
 
       return isLoading ? (
         <Row nogutter>
@@ -129,24 +132,11 @@ const Summary = ({ theme, sqon, api }) => (
                 </CardSlot>
               </PaddedColumn>
               <PaddedColumn md={md} lg={lg}>
-                <CardSlot title="Most Frequent Diagnoses">
-                  <BarChartContainer>
-                    <HorizontalBar
-                      data={topDiagnosesBarMock}
-                      indexBy="label"
-                      keys={['probands', 'familyMembers']}
-                      tooltipFormatter={mostFrequentDiagnosisTooltip}
-                      sortByValue={true}
-                      tickInterval={4}
-                      colors={[theme.chartColors.blue, theme.chartColors.purple]}
-                      xTickTextLength={28}
-                      legends={[
-                        { title: 'Probands', color: theme.chartColors.blue },
-                        { title: 'Family Members', color: theme.chartColors.purple },
-                      ]}
-                    />
-                  </BarChartContainer>
-                </CardSlot>
+                <DiagnosesChart
+                  data={topDiagnosesBarMock}
+                  sqon={sqon}
+                  topDiagnoses={topDiagnosesData}
+                />
               </PaddedColumn>
               <PaddedColumn md={md} lg={lg}>
                 <CardSlot showHeader={false}>
