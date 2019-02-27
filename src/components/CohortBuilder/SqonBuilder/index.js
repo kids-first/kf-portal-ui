f646d56150bc1c1cd9801f171f620de9b4fe5699
Minh <hlminh2000@gmail.com>
2019-02-27 16:58:09 -0500

    implements clear all modal

--- src/components/CohortBuilder/SqonBuilder/index.js
+++ src/components/CohortBuilder/SqonBuilder/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import styled from 'react-emotion';
+import { compose } from 'recompose';
+import { css } from 'emotion';
 import { omit } from 'lodash';
 import { memoize } from 'lodash';
+import { injectState } from 'freactal';
 import AdvancedSqonBuilder from '@arranger/components/dist/AdvancedSqonBuilder';
 import ExtendedMappingProvider from '@arranger/components/dist/utils/ExtendedMappingProvider';
 import { withApi } from 'services/api';
 import { arrangerProjectId } from 'common/injectGlobals';
 import { FieldFilterContainer, ARRANGER_API_PARTICIPANT_INDEX_NAME } from '../common';
+import { ModalFooter } from 'components/Modal';
+import { Div } from 'uikit/Core';
 
 const extendedMappingToDisplayNameMap = memoize(extendedMapping =>
   extendedMapping.reduce((acc, { field, displayName }) => {
@@ -26,10 +31,28 @@ const StyledFieldFilterContainer = styled(FieldFilterContainer)`
   right: 0px;
 `;
 
+const ModalContent = styled(Div)`
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
+
+const ClearAllModalContent = ({ onConfirmed }) => (
+  <React.Fragment>
+    <ModalContent>
+      <ModalContent>Are you sure you want to delete all queries?</ModalContent>
+      <ModalContent>This action cannot be undone.</ModalContent>
+    </ModalContent>
+    <ModalFooter submitText="DELETE" handleSubmit={onConfirmed} />
+  </React.Fragment>
+);
+
 /**
  * this component should mimic the AdvancedSqonBuilder's API directly
  **/
-const SqonBuilder = withApi(({ api, ...rest }) => {
+const SqonBuilder = compose(
+  withApi,
+  injectState,
+)(({ api, onChange, state, effects, ...rest }) => {
   const onSqonRemoveClick = ({ indexToRemove, dependentIndices }) => {
     if (dependentIndices.length) {
       // temporary demo until design is available
@@ -42,6 +65,28 @@ The queries ${dependentIndices.map(i => `#${i}`).join(', ')} depend on it`,
         : Promise.reject();
     } else {
       return Promise.resolve();
+    }
+  };
+  const handleAction = async action => {
+    if (action.eventKey === 'CLEAR_ALL') {
+      effects.setModal({
+        title: 'Clear All Queries',
+        classNames: {
+          modal: css`
+            max-width: 500px;
+          `,
+        },
+        component: (
+          <ClearAllModalContent
+            onConfirmed={() => {
+              effects.unsetModal();
+              onChange(action);
+            }}
+          />
+        ),
+      });
+    } else {
+      onChange(action);
     }
   };
   return (
@@ -65,6 +110,7 @@ The queries ${dependentIndices.map(i => `#${i}`).join(', ')} depend on it`,
                 <StyledFieldFilterContainer showHeader={false} {...props} />
               )}
               fieldDisplayNameMap={extendedMappingToDisplayNameMap(extendedMapping)}
+              onChange={handleAction}
               {...rest}
             />
           )
