2c8953853eabd91ee1977c5e395c2e8f8106b266
jaouad-benassila <47750690+jaouad-benassila@users.noreply.github.com>
2019-03-11 10:47:10 -0400

    Fix ui summary section for 0 participant (#1306)
    * Fix ui summary section for 0 participant
    
    * solution to avoid z-index change as much as possible

--- src/components/CohortBuilder/EmptyCohortOverlay.js
+++ src/components/CohortBuilder/EmptyCohortOverlay.js
@@ -8,14 +8,13 @@ const StyleOverlay = styled('div')`
   width: 100vw;
   height: 100%;
   left: 0;
-  z-index: 1001;
+  top: 0px;
   display: flex;
   justify-content: center;
 `;
 
 const StyleMessage = styled('div')`
   margin-top: 100px;
-  z-index: 1002;
   border: 1px solid #ccc;
   border-radius: 5px;
   padding: 15px 35px;
@@ -24,11 +23,12 @@ const StyleMessage = styled('div')`
   max-height: fit-content;
   font-size: 14px;
   font-family: ${({ theme }) => theme.fonts.default}, sans-serif;
+  height: 50px;
 `;
 
 const EmptyCohortOverlay = withTheme(({ theme }) => (
   <StyleOverlay>
-    <StyleMessage>There are 0 participants for this cohort.</StyleMessage>
+    <StyleMessage>There are no participants for this cohort.</StyleMessage>
   </StyleOverlay>
 ));
 
