ec137dd152b023f75215c8323b805ad072eb9f7f
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-03-13 15:25:15 -0400

    update cohorot loader card style

--- src/components/CohortBuilder/LoaderCard.js
+++ src/components/CohortBuilder/LoaderCard.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import { withTheme } from 'emotion-theming';
 import { CohortCard } from './ui';
-import LoadingSpinner from 'uikit/LoadingSpinner';
+import Spinner from 'react-spinkit';
+import Column from 'uikit/Column';
+import styled from 'react-emotion';
+
+const Loader = styled(Column)`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0.5;
+`;
 
 const LoaderCard = ({ theme, ...rest }) => (
   <CohortCard {...rest}>
-    <LoadingSpinner color={theme.greyScale11} size={'50px'} />
+    <Loader>
+      <Spinner
+        name="circle"
+        color={theme.greyScale11}
+        style={{
+          width: 50,
+          height: 50,
+        }}
+      />
+    </Loader>
   </CohortCard>
 );
 
