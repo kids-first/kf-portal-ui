962b42db133058e2c519f00234b7fe10b5900059
Minh <hlminh2000@gmail.com>
2019-03-20 11:34:31 -0400

    change path to cohort finder to /virtualStudies

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -71,7 +71,7 @@ const Header = ({
                     </NavLink>
                   </li>
                   <li>
-                    <NavLink currentPathName={currentPathName} to="/cohort">
+                    <NavLink currentPathName={currentPathName} to="/virtualStudies">
                       <ExploreDataIconStyled /> <Trans>Explore Data</Trans>
                     </NavLink>
                   </li>
