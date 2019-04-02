39f8d1a6f12ac9b5a72cad6f0dd90eacbd8e3589
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2019-04-02 09:02:23 -0400

    remove withDropdownState from withDropdownMultiPane

--- src/uikit/Dropdown/index.js
+++ src/uikit/Dropdown/index.js
@@ -97,20 +97,17 @@ export const withDropdownState = compose(
 );
 
 export const withDropdownMultiPane = compose(
-  withDropdownState,
-  compose(
-    withState('showExpanded', 'setExpanded', false),
-    withState('activeIndex', 'setActiveIndex', null),
-    withHandlers({
-      toggleExpanded: ({ showExpanded, setExpanded }) => e => {
-        setExpanded(!showExpanded);
-      },
-      toggleExpandedDropdown: ({ showExpanded, setExpanded, toggleDropdown }) => e => {
-        setExpanded(!showExpanded);
-        toggleDropdown();
-      },
-    }),
-  ),
+  withState('showExpanded', 'setExpanded', false),
+  withState('activeIndex', 'setActiveIndex', null),
+  withHandlers({
+    toggleExpanded: ({ showExpanded, setExpanded }) => e => {
+      setExpanded(!showExpanded);
+    },
+    toggleExpandedDropdown: ({ showExpanded, setExpanded, toggleDropdown }) => e => {
+      setExpanded(!showExpanded);
+      toggleDropdown();
+    },
+  }),
 );
 
 export default Dropdown;
