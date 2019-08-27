5dd2db9831095e772b8c53477ba2b56a7732fde0
evans-g-crsj <eg.crsj@gmail.com>
2019-08-27 14:29:50 -0400

    Make commit as small as possible to avoid mixing bug fixing and refactoring at the same time (part 2 of 2).

--- src/components/CohortBuilder/ParticipantsTableView/ParticipantsTable.js
+++ src/components/CohortBuilder/ParticipantsTableView/ParticipantsTable.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { compose } from 'recompose';
+import { compose, withState } from 'recompose';
 import { withTheme } from 'emotion-theming';
 import ControlledDataTable from 'uikit/DataTable/ControlledDataTable';
 import { Link } from 'uikit/Core';
@@ -17,8 +17,118 @@ import { configureCols } from 'uikit/DataTable/utils/columns';
 import DownloadButton from 'components/FileRepo/DownloadButton';
 import { arrangerProjectId } from 'common/injectGlobals';
 import { SORTABLE_FIELDS_MAPPING } from './queries';
-import { SelectionCell, CollapsibleMultiLineCell, NbFilesCell } from './cells';
-import { rooTable } from './css';
+import { css } from 'emotion';
+import FileIcon from 'icons/FileIcon';
+import { union, compact } from 'lodash';
+import { MONDOLink } from '../../Utils/DiagnosisAndPhenotypeLinks';
+
+const SelectionCell = ({ value: checked, onCellSelected, row }) => {
+  return (
+    <input
+      type="checkbox"
+      checked={!!checked}
+      onChange={
+        row
+          ? () => onCellSelected(!checked, row)
+          : evt => {
+              onCellSelected(evt.currentTarget.checked);
+            }
+      }
+    />
+  );
+};
+
+const rowCss = css({
+  display: 'flex',
+  flexWrap: 'nowrap',
+  alignItems: 'flex-start',
+  alignContent: 'stretch',
+});
+
+const isMondo = datum => typeof datum === 'string' && datum.includes('MONDO');
+
+const enhance = compose(withState('collapsed', 'setCollapsed', true));
+
+const CollapsibleMultiLineCell = enhance(({ value: data, collapsed, setCollapsed }) => {
+  // Display one row when there is exactly more than one row.
+  // Collapsing a single don't save any space.
+  const cleanedUniquifiedData = compact(union(data));
+
+  const sizeOfCleanData = cleanedUniquifiedData.length;
+
+  const displayedRowCount = collapsed ? 1 : sizeOfCleanData;
+  const displayMoreButton = sizeOfCleanData > 1;
+  const hasManyValues = sizeOfCleanData > 1;
+
+  return (
+    <div className={`${rowCss}`}>
+      <div style={{ display: 'flex', flexDirection: 'column', flex: '4' }}>
+        {cleanedUniquifiedData.slice(0, displayedRowCount).map((datum, index) => {
+          if (isMondo(datum)) {
+            return <MONDOLink key={index} mondo={datum} />;
+          } else {
+            return (
+              <div key={index}>
+                {hasManyValues && '\u2022'}
+                {datum &&
+                  '\u00A0' /* unbreakable space to avoid empty rows from collapsing in height */}
+              </div>
+            );
+          }
+        })}
+      </div>
+      {displayMoreButton && (
+        <div
+          style={{ flex: '1', marginTop: '-8px ' }}
+          onClick={() => {
+            setCollapsed(!collapsed);
+          }}
+        >
+          <div className={`showMore-wrapper ${collapsed ? 'more' : 'less'}`}>
+            {collapsed ? `${sizeOfCleanData - displayedRowCount} ` : ''}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+});
+
+const NbFilesCell = compose(
+  withTheme(({ value: nbFiles, row, theme }) => {
+    const encodedSqon = encodeURI(
+      JSON.stringify(
+        {
+          op: 'and',
+          content: [
+            {
+              op: 'in',
+              content: {
+                field: 'participants.kf_id',
+                value: [row.participantId],
+              },
+            },
+          ],
+        },
+        null,
+        0,
+      ),
+    );
+
+    return (
+      <Link to={`/search/file?sqon=${encodedSqon}`} className="nbFilesLink">
+        <FileIcon
+          className={css`
+            margin-right: 5px;
+          `}
+          width={8}
+          height={13}
+          fill={theme.greyScale11}
+        />
+        {`${nbFiles} Files`}
+      </Link>
+    );
+  }),
+);
 
 const ParticipantIdLink = compose(
   withTheme(({ value: idParticipant }) => {
@@ -122,6 +232,16 @@ const participantsTableViewColumns = (onRowSelected, onAllRowsSelected, dirtyHac
     sortable: false,
   },
 ];
+
+const cssClass = css({
+  '.nbFilesLink img': {
+    top: '2px',
+    position: 'relative',
+  },
+  'div.rt-noData': {
+    display: 'none !important',
+  },
+});
 
 class ParticipantsTable extends Component {
   static defaultProps = {
@@ -251,7 +371,7 @@ class ParticipantsTable extends Component {
           columns={columns}
           data={data}
           loading={loading}
-          className={`${rooTable}`}
+          className={`${cssClass}`}
           onFetchData={onFetchData}
           dataTotalCount={dataTotalCount}
         />
