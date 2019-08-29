ad68b3b83b6cf309b8f021e2bb10750e73772b27
jecos <jecos.crsj@gmail.com>
2019-08-28 21:01:14 -0400

    Use custom error messages for orcid

--- src/components/Login/OrcidRedirect.js
+++ src/components/Login/OrcidRedirect.js
@@ -7,30 +7,14 @@ import autobind from 'auto-bind-es5';
 import { getOrcidToken } from 'services/ego/auth';
 import { ORCID } from 'common/constants';
 
-import GenericErrorDisplay from 'uikit/GenericErrorDisplay';
+import PromptMessage from 'uikit/PromptMessage';
 import LoadingSpinner from 'uikit/LoadingSpinner';
 import { Link } from 'uikit/Core';
 
 import './index.css';
 
-const ORCID_ERRORS = {
-  'email.empty': {
-    message:
-      "You should change the visibility setting of your email address so that it's available publicly.",
-    link:
-      'https://support.orcid.org/hc/en-us/articles/360006894294-How-to-verify-your-email-address',
-  },
-  'email.not.verified': {
-    message: 'You must make sure that your email address has been verified by the ORCID Web site.',
-    link: 'https://support.orcid.org/hc/en-us/articles/360006897614-Visibility-settings',
-  },
-  default: {
-    message: ' ',
-    link: '/',
-  },
-};
-
 export default class OrcidRedirect extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
@@ -47,6 +31,77 @@ export default class OrcidRedirect extends React.Component {
     loginProvider: PropTypes.string,
   };
 
+
+  renderEmailEmpty() {
+    return (
+      <React.Fragment>
+        <p key='error_line_1'>
+          We were unable to log you into Kids First DRC via ORCID because you have not shared your email with trusted parties.
+        </p>
+        <p key='error_line_2'>
+          To update your settings, visit your <a href="https://orcid.org/account" target="_blank" rel="noopener noreferrer">ORCID profile</a> under Account Settings -> Edit "Email and notification preferences" and then choose to share your email address either with "Trusted Parties" or "Everyone". The Kids First DRC is a ORCID trusted partner.
+        </p>
+        <p key='error_line_3'>
+          If you have questions or need further assistance, please email support@kidsfirstdrc.org.
+        </p>
+      </React.Fragment>
+    );
+  }
+
+  renderEmailNotVerified() {
+    return (
+      <React.Fragment>
+        <p key='error_line_1'>
+          We were unable to log you into Kids First DRC via ORCID because you have not valitated your email with ORCID.
+        </p>
+        <p key='error_line_2'>
+          To validate your email, please follow the intructions at <a href="https://support.orcid.org/hc/en-us/articles/360006894294-How-to-verify-your-email-address" target="_blank" rel="noopener noreferrer">https://support.orcid.org/hc/en-us/articles/360006894294-How-to-verify-your-email-address</a>.
+        </p>
+        <p key='error_line_3'>
+          If you have questions or need further assistance, please email support@kidsfirstdrc.org.
+        </p>
+      </React.Fragment>);
+
+  }
+
+  renderDefaultError() {
+    return (
+      <React.Fragment>
+        <p key='error_line_1'>
+          If the error persist, please email support@kidsfirstdrc.org.
+        </p>
+      </React.Fragment>);
+
+  }
+
+  renderErorrMessages(error) {
+    switch (error) {
+      case 'email.empty':
+        return this.renderEmailEmpty();
+      case 'email.not.verified':
+        return this.renderEmailNotVerified();
+      default:
+        return this.renderDefaultError();
+    }
+  }
+
+  renderError(error) {
+    let message = this.renderErorrMessages(error);
+    let content = (
+      <React.Fragment>
+        {message}
+        <Link to="/">Try again</Link>
+      </React.Fragment>
+    )
+    return (
+      <PromptMessage
+        className="generic-error-display"
+        heading="An error occured while trying to log in via ORCID"
+        content={content}
+        error={true}
+      />
+    );
+  }
   componentDidMount() {
     this.code || this.hasOrcidToken() ? this.handleOrcidSuccess() : this.handleOrcidError();
   }
@@ -74,41 +129,21 @@ export default class OrcidRedirect extends React.Component {
       .catch(error => {
         if (typeof error === 'string') {
           this.setState({
-            error: ORCID_ERRORS[error] ? ORCID_ERRORS[error] : ORCID_ERRORS.default,
+            error: this.renderError(error),
           });
         } else {
-          this.setState({
-            error: ORCID_ERRORS.default,
-          });
+          this.handleOrcidError()
         }
       });
   }
 
   handleOrcidError() {
-    this.setState({ error: ORCID_ERRORS.default });
-  }
-
-  renderLink() {
-    const { error } = this.state;
-    const link = error.link || '/';
-    return /^https?:\/\//i.test(link) ? (
-      <a href={link} target="_blank">
-        See the documentation on the ORCID Web site
-      </a>
-    ) : (
-      <Link to={link}>Try again</Link>
-    );
+    this.setState({ error: this.renderError('default') });
   }
 
   render() {
     const { error } = this.state;
-    const content = error ? (
-      <GenericErrorDisplay
-        header={'An error occured while trying to log in via ORCID iD'}
-        error={error.message}
-        footer={this.renderLink()}
-      />
-    ) : (
+    const content = error ? error : (
       <React.Fragment>
         <LoadingSpinner size={96} color={'#a9adc0'} className="login-redirect-loader" />
         <span>Trying to log in via ORCID iD...</span>
