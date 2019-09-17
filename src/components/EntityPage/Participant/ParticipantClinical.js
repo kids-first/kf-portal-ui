8f1b5a09471a6be60b44a40540d5e26b4138afd5
evans-g-crsj <eg.crsj@gmail.com>
2019-09-17 15:14:19 -0400

    prepare ui for new upcoming data

--- src/components/EntityPage/Participant/ParticipantClinical.js
+++ src/components/EntityPage/Participant/ParticipantClinical.js
@@ -19,20 +19,22 @@ import { resetVirtualStudy } from '../../../store/actionCreators/virtualStudies'
 import { store } from '../../../store';
 import prettifyAge from './Utils/prettifyAge';
 import { flatMap } from 'lodash/collection';
-import { HPOLink, SNOMEDLink , MONDOLink, NCITLink} from '../../Utils/DiagnosisAndPhenotypeLinks';
-
+import { HPOLink, SNOMEDLink, MONDOLink, NCITLink } from '../../Utils/DiagnosisAndPhenotypeLinks';
+import ClinicalIcon from 'icons/ClinicalIcon';
+import BiospecimenIcon from 'icons/BiospecimenIcon';
+import { withTheme } from 'emotion-theming';
 //https://kf-qa.netlify.com/participant/PT_C954K04Y#summary tons of phenotypes
 //https://kf-qa.netlify.com/participant/PT_CB55W43A#clinical family has mother and child being affected
 
+const cellBreak = wrapper => (
+  <div style={{ wordBreak: 'break-word', textTransform: 'capitalize' }}>{wrapper.value}</div>
+);
+
 class ParticipantClinical extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { diagnoses: [], phenotypes: [] };
 
-    this.state = { diagnoses: false, phenotypes: false };
-
+  componentDidMount() {
     this.dataIntoState();
-
-    this.updateState = (state, callback) => this.setState({...this.state, ...state}, callback);
   }
 
   diagnosisIntoState(api) {
@@ -43,8 +45,8 @@ class ParticipantClinical extends React.Component {
       });
     }
 
-    let diagnoses = get(this.props.participant, 'diagnoses.hits.edges', []).map(ele =>
-      Object.assign({}, get(ele, 'node', {})) //copy obj
+    let diagnoses = get(this.props.participant, 'diagnoses.hits.edges', []).map(
+      ele => Object.assign({}, get(ele, 'node', {})), //copy obj
     );
 
     Promise.all(
@@ -66,14 +68,17 @@ class ParticipantClinical extends React.Component {
       })(),
     ).then(nums => {
       for (let i = 0; i < nums.length; i++) {
-        diagnoses[i].shared_with = this.prettySharedWith(get(nums[i], 'data.participant.hits.total', '--'));
+        diagnoses[i].shared_with = this.prettySharedWith(
+          get(nums[i], 'data.participant.hits.total', '--'),
+        );
       }
 
-      this.updateState({ diagnoses: sanitize(diagnoses) });  //once we're ready, just tell the state, it'll do the rest
+      this.setState({ diagnoses: sanitize(diagnoses) }); //once we're ready, just tell the state, it'll do the rest
     });
   }
 
-  phenotypeIntoState(api) { //stub for when the phenotypes are available
+  phenotypeIntoState(api) {
+    //stub for when the phenotypes are available
     function callObs(phenotype) {
       return graphql(api)({
         query: `query($sqon: JSON) {participant {hits(filters: $sqon) {total}}}`,
@@ -88,39 +93,44 @@ class ParticipantClinical extends React.Component {
       });
     }
 
-    let phenotypes = get(this.props.participant, 'phenotype.hits.edges', []).map(ele =>
-      Object.assign({}, get(ele, 'node', {})) //copy obj
+    let phenotypes = get(this.props.participant, 'phenotype.hits.edges', []).map(
+      ele => Object.assign({}, get(ele, 'node', {})), //copy obj
     );
 
-    phenotypes = flatMap( phenotypes.sort( (a, b) => a.age_at_event_days-b.age_at_event_days ), pheno => {
-      //transform phenotypes while we wait for the calls
+    phenotypes = flatMap(
+      phenotypes.sort((a, b) => a.age_at_event_days - b.age_at_event_days),
+      pheno => {
+        //transform phenotypes while we wait for the calls
 
-      if(pheno.observed) {
-        pheno.interpretation = "Observed";
-        pheno.hpo = pheno.hpo_phenotype_observed;
-        pheno.snomed = pheno.snomed_phenotype_observed;
-      } else {
-        pheno.interpretation = "Not Observed";
-        pheno.hpo = pheno.hpo_phenotype_not_observed;
-        pheno.snomed = pheno.snomed_phenotype_not_observed;
-      }
+        if (pheno.observed) {
+          pheno.interpretation = 'Observed';
+          pheno.hpo = pheno.hpo_phenotype_observed;
+          pheno.snomed = pheno.snomed_phenotype_observed;
+        } else {
+          pheno.interpretation = 'Not Observed';
+          pheno.hpo = pheno.hpo_phenotype_not_observed;
+          pheno.snomed = pheno.snomed_phenotype_not_observed;
+        }
 
-      pheno.age_at_event_days = prettifyAge(pheno.age_at_event_days);
+        pheno.age_at_event_days = prettifyAge(pheno.age_at_event_days);
 
-      return [pheno];
-    });
+        return [pheno];
+      },
+    );
 
     Promise.all(
       phenotypes.map(pheno => {
         //start ajax calls to know the shared with.
-        return pheno.interpretation === "Observed" ? callObs(pheno.hpo) : callNotObs(pheno.hpo);
-      })
+        return pheno.interpretation === 'Observed' ? callObs(pheno.hpo) : callNotObs(pheno.hpo);
+      }),
     ).then(nums => {
       for (let i = 0; i < nums.length; i++) {
-        phenotypes[i].shared_with = this.prettySharedWith(get(nums[i], 'data.participant.hits.total', '--'));
+        phenotypes[i].shared_with = this.prettySharedWith(
+          get(nums[i], 'data.participant.hits.total', '--'),
+        );
       }
 
-      this.updateState({ phenotypes: sanitize(phenotypes) });  //once we're ready, just tell the state, it'll do the rest
+      this.setState({ phenotypes: sanitize(phenotypes) }); //once we're ready, just tell the state, it'll do the rest
     });
   }
 
@@ -137,68 +147,123 @@ class ParticipantClinical extends React.Component {
   }
 
   prettySharedWith(amount) {
-    if(amount === "--") return amount;
-    else if(amount === 1) return `${amount} participant`;
+    if (amount === '--') return amount;
+    else if (amount === 1) return `${amount} participant`;
     else return `${amount} participants`;
   }
 
-   showParticipantNb = (wrapper , field , value) =>{
-          if(wrapper.value === "0 participants") return <div>0 participants</div>;
+  showParticipantNb = (wrapper, field, value) => {
+    if (wrapper.value === '0 participants') return <div>0 participant</div>;
 
-          const onClick = () => {
-            store.dispatch(resetVirtualStudy());
+    const onClick = () => {
+      store.dispatch(resetVirtualStudy());
 
-            const newSqon = {
-              op: 'in',
-              content: {
-                field: field,
-                value: value,
-              },
-            };
+      const newSqon = {
+        op: 'in',
+        content: {
+          field: field,
+          value: value,
+        },
+      };
 
-            const modifiedSqons = setSqonValueAtIndex(
-              getDefaultSqon(), //virtualStudy.sqons,
-              0, //virtualStudy.activeIndex,
-              newSqon,
-              {
-                operator: MERGE_OPERATOR_STRATEGIES.KEEP_OPERATOR,
-                values: MERGE_VALUES_STRATEGIES.APPEND_VALUES,
-              },
-            );
+      const modifiedSqons = setSqonValueAtIndex(
+        getDefaultSqon(), //virtualStudy.sqons,
+        0, //virtualStudy.activeIndex,
+        newSqon,
+        {
+          operator: MERGE_OPERATOR_STRATEGIES.KEEP_OPERATOR,
+          values: MERGE_VALUES_STRATEGIES.APPEND_VALUES,
+        },
+      );
 
-            store.dispatch(setSqons(modifiedSqons))
-          };
+      store.dispatch(setSqons(modifiedSqons));
+    };
 
-          return <Link to={"/explore"} onClick={onClick}>{wrapper.value}</Link>;
-    }
-
-
+    return (
+      <Link to={'/explore'} onClick={onClick}>
+        {wrapper.value}
+      </Link>
+    );
+  };
 
   render() {
-    const cellBreak = wrapper => <div style={{wordBreak: "break-word", textTransform: "capitalize"}}>{wrapper.value}</div>;
+    const theme = this.props.theme;
 
     const diagHeads = [
+      {
+        Header: '',
+        accessor: 'biospecimensTODO',
+        width: 75,
+        headerStyle: {
+          textAlign: 'center',
+        },
+        style: {
+          textAlign: 'center',
+        },
+        Cell: row => {
+          return row.value ? (
+            <BiospecimenIcon
+              width={18}
+              height={18}
+              fill={theme.biospecimenOrange}
+              alt="histological"
+            />
+          ) : (
+            <ClinicalIcon width={18} height={18} fill={theme.clinicalBlue} alt="clinical" />
+          );
+        },
+      },
       { Header: 'Diagnosis Category', accessor: 'diagnosis_category', Cell: cellBreak },
-      { Header: 'Diagnosis (Mondo)', accessor: 'mondo_id_diagnosis', Cell: (wrapper) => wrapper.value === "--" ? <div>--</div> : <MONDOLink mondo={wrapper.value}/> },
-      { Header: 'Diagnosis (NCIT)', accessor: 'ncit_id_diagnosis',  Cell: (wrapper) => wrapper.value === "--" ? <div>--</div> : <NCITLink ncit={wrapper.value}/> },
+      {
+        Header: 'Diagnosis (Mondo)',
+        accessor: 'mondo_id_diagnosis',
+        Cell: wrapper =>
+          wrapper.value === '--' ? <div>--</div> : <MONDOLink mondo={wrapper.value} />,
+      },
+      {
+        Header: 'Diagnosis (NCIT)',
+        accessor: 'ncit_id_diagnosis',
+        Cell: wrapper =>
+          wrapper.value === '--' ? <div>--</div> : <NCITLink ncit={wrapper.value} />,
+      },
       { Header: 'Diagnosis (Source Text)', accessor: 'source_text_diagnosis', Cell: cellBreak },
       { Header: 'Age at event', accessor: 'age_at_event_days', Cell: cellBreak },
       {
         Header: 'Mondo term shared with',
         accessor: 'shared_with',
         Cell: wrapper => {
+          const participant = this.showParticipantNb(wrapper, 'diagnoses.mondo_id_diagnosis', [
+            wrapper.original.mondo_id_diagnosis,
+          ]);
 
-        const  participant = this.showParticipantNb(wrapper , 'diagnoses.mondo_id_diagnosis' , [wrapper.original.mondo_id_diagnosis])
-
-        return participant
-
+          return participant;
         },
+      },
+      {
+        Header: 'Specimen IDs',
+        accessor: 'biospecimensTODO',
+        headerStyle: {
+          textAlign: 'center',
+        },
+        style: {
+          textAlign: 'center',
+        },
+        Cell: () => <div>--</div>,
       },
     ];
 
     const phenoHeads = [
-      { Header: 'Phenotype (HPO)', accessor: 'hpo', Cell: (wrapper) => wrapper.value === "--" ? <div>--</div> : <HPOLink hpo={wrapper.value}/> },
-      { Header: "Phenotype (SNOMED)", accessor: 'snomed', Cell: (wrapper) => wrapper.value === "--" ? <div>--</div> : <SNOMEDLink snomed={wrapper.value}/> },
+      {
+        Header: 'Phenotype (HPO)',
+        accessor: 'hpo',
+        Cell: wrapper => (wrapper.value === '--' ? <div>--</div> : <HPOLink hpo={wrapper.value} />),
+      },
+      {
+        Header: 'Phenotype (SNOMED)',
+        accessor: 'snomed',
+        Cell: wrapper =>
+          wrapper.value === '--' ? <div>--</div> : <SNOMEDLink snomed={wrapper.value} />,
+      },
       { Header: 'Phenotype (Source Text)', accessor: 'source_text_phenotype', Cell: cellBreak },
       { Header: 'Interpretation', accessor: 'interpretation', Cell: cellBreak },
       { Header: 'Age at event', accessor: 'age_at_event_days', Cell: cellBreak },
@@ -206,10 +271,15 @@ class ParticipantClinical extends React.Component {
         Header: 'HPO term shared with',
         accessor: 'shared_with',
         Cell: wrapper => {
+          const participant = this.showParticipantNb(
+            wrapper,
+            wrapper.original.interpretation === 'Observed'
+              ? 'phenotype.hpo_phenotype_observed'
+              : 'phenotype.hpo_phenotype_not_observed',
+            [wrapper.original.hpo],
+          );
 
-          const  participant = this.showParticipantNb(wrapper , wrapper.original.interpretation === 'Observed' ? "phenotype.hpo_phenotype_observed" : "phenotype.hpo_phenotype_not_observed" , [wrapper.original.hpo])
-
-          return participant
+          return participant;
         },
       },
     ];
@@ -220,31 +290,19 @@ class ParticipantClinical extends React.Component {
 
     return (
       <React.Fragment>
-        {
-          !diagnoses
-            ? ""
-            : diagnoses.length === 0
-              ? ""
-              : (
-                <EntityContentSection title="Diagnoses">
-                  <ParticipantDataTable columns={diagHeads} data={diagnoses} />
-                </EntityContentSection>
-              )
-        }
-        {
-          !phenotypes
-            ? ""
-            : phenotypes.length === 0
-              ? ""
-              : (
-                <EntityContentSection title="Phenotypes">
-                  <ParticipantDataTable columns={phenoHeads} data={phenotypes} />
-                </EntityContentSection>
-              )
-        }
+        {diagnoses && diagnoses.length > 0 && (
+          <EntityContentSection title="Diagnoses">
+            <ParticipantDataTable columns={diagHeads} data={diagnoses} />
+          </EntityContentSection>
+        )}
+        {phenotypes && phenotypes.length > 0 && (
+          <EntityContentSection title="Phenotypes">
+            <ParticipantDataTable columns={phenoHeads} data={phenotypes} />
+          </EntityContentSection>
+        )}
         {participant.family_id && (
           <div>
-            {diagnoses.length === 0 ? "" : <EntityContentDivider /> }
+            {diagnoses.length === 0 ? '' : <EntityContentDivider />}
             <EntityContentSection title={`Family Members (${participant.family_id})`}>
               <FamilyTable participant={participant} />
             </EntityContentSection>
@@ -254,5 +312,8 @@ class ParticipantClinical extends React.Component {
     );
   }
 }
-//: {get(participant, "family.family_composition.hits.edges[0].node.composition", "trio")}
-export default withRouter(ParticipantClinical);
+
+const ParticipantClinicalWithRouter = withRouter(ParticipantClinical);
+const ParticipantClinicalWithRouterAndTheme = withTheme(ParticipantClinicalWithRouter);
+
+export default ParticipantClinicalWithRouterAndTheme;
