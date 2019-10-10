22d6597416e5d23609cfa70f48a83c41d2df3cc1
Adrian Paul <adipaul1981@yahoo.ca>
2019-10-10 16:39:59 -0400

    Fix Pagination

--- src/components/MemberSearchPage/MemberSearchContainer.js
+++ src/components/MemberSearchPage/MemberSearchContainer.js
@@ -5,22 +5,70 @@ import { bindActionCreators } from 'redux';
 import { Col, Icon, Input, Row, Tooltip } from 'antd';
 import MemberTable from './MemberTable';
 
+const PAGE_COUNT = 10;
+
+function updatePage(page) {
+  return (previousState, currentProps) => {
+    return { ...previousState, currentPage: page };
+  };
+}
+
+function updatePageSize(current, size) {
+  return (previousState, currentProps) => {
+    return { ...previousState, membersPerPage: size, currentPage: current };
+  };
+}
+
 class MemberSearchContainer extends Component {
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this)
+    this.state = {
+      queryString: '',
+      currentPage: 1,
+      membersPerPage: 10,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handlePageChange = this.handlePageChange.bind(this);
+    this.handleShowSizeChange = this.handleShowSizeChange.bind(this);
   }
   static propTypes = {};
 
+
+  getCurrentStart = () => {
+    return (this.state.currentPage * this.state.membersPerPage - this.state.membersPerPage)
+  };
+
+  getCurrentEnd = () => {
+    return (this.state.currentPage * this.state.membersPerPage)
+  };
+
   handleChange(e) {
     const {fetchListOfMembers} = this.props;
-    fetchListOfMembers(e.target.value, {start: 0, end: 50})
-  } //FIXME
+    fetchListOfMembers(e.target.value, {start: this.getCurrentStart(), end: this.getCurrentEnd()});
+    this.setState({queryString: e.target.value})
+  }; //FIXME
 
   componentDidMount() {
     const {fetchListOfMembers} = this.props;
-    fetchListOfMembers("", {start: 0, end: 50})
-  }//FIXME
+    fetchListOfMembers(this.state.queryString, {start: this.getCurrentStart(), end: this.getCurrentEnd()})
+  };//FIXME
+
+  handlePageChange = page => {
+    if (page < 1 || page > PAGE_COUNT) return; //FIXME
+    const {fetchListOfMembers} = this.props;
+    this.setState(
+      updatePage(page),
+      () => fetchListOfMembers(this.state.queryString, {start: this.getCurrentStart(), end: this.getCurrentEnd()})
+    );
+  }; //FIXME maybe use componentDidUpdate in lieu...
+
+  handleShowSizeChange(current, pageSize) {
+    const {fetchListOfMembers} = this.props;
+    this.setState(
+      updatePageSize(current, pageSize),
+      () => fetchListOfMembers(this.state.queryString, {start: this.getCurrentStart(), end: this.getCurrentEnd()})
+    );
+  }
 
   render() {
     //FIXME
@@ -35,7 +83,8 @@ class MemberSearchContainer extends Component {
       title: row[0].title,
       interests: row[0].interests,
       institution: row[0].institution,
-      roles: row[0].roles
+      roles: row[0].roles,
+      highlights: row[1].highlight,
     }));
     return (
       <div id={"grid-container"}>
@@ -44,7 +93,6 @@ class MemberSearchContainer extends Component {
             TODO add table
           </Col>
           <Col span={16}>
-            <p>Member Search</p>
             <Input
               style={{borderRadius: 30}}
               onChange={this.handleChange}
@@ -56,19 +104,20 @@ class MemberSearchContainer extends Component {
                 </Tooltip>
               }
             />
-            {console.log("ADRIAN")}
-            {console.log(this.props)}
-            {console.log("ADRIAN")}
-            <MemberTable memberList={memberList} totalCount={this.props.totalCount}/>
+            <MemberTable
+              memberList={memberList}
+              totalCount={this.props.totalCount}
+              currentPage={this.state.currentPage}
+              membersPerPage={this.state.membersPerPage}
+              handlePageChange={this.handlePageChange}
+              handleShowSizeChange={this.handleShowSizeChange}
+            />
           </Col>
         </Row>
-
       </div>
 
     );
   }
-
-
 }
 
 const mapStateToProps = state => ({
--- src/components/MemberSearchPage/MemberTable.js
+++ src/components/MemberSearchPage/MemberTable.js
@@ -1,26 +1,27 @@
 import { Col, List, Row } from 'antd';
 import React from 'react';
-import { find } from 'lodash';
+import { find, trim } from 'lodash';
 import { ROLES } from 'common/constants';
 import { MemberImage } from 'components/MemberSearchPage/ui';
+import md5 from 'md5';
 
 const userRoleDisplayName = (userRole) => find(ROLES, { type: userRole }).displayName;
 
-function renderButton() {
-  if(this.state.mode === 'view') {
-    return (
-      <button onClick={this.handleEdit}>
-        Edit
-      </button>
-    );
-  } else {
-    return (
-      <button onClick={this.handleSave}>
-        Save
-      </button>
-    );
+const formatLabel = (label, value) => {
+  if (!value) {
+    return label;
   }
-}
+  return (<span>
+    { label.split(value)
+      .reduce((prev, current, i) => {
+        if (!i) {
+          return [current];
+        }
+        return prev.concat(<b key={value + current}>{ value }</b>, current);
+      }, [])
+    }
+  </span>);
+};
 
 const MemberTable = (props) => {
   return (
@@ -30,7 +31,19 @@ const MemberTable = (props) => {
         header={
           `Showing 1-10 of ${props.memberList.length} (${props.totalCount} members matching)`
         }
-        pagination={{defaultPageSize: 10, showSizeChanger: true, pageSizeOptions: ['10', '20', '50'], defaultCurrent: 1, total: props.memberList.length}}
+
+        pagination={{
+          defaultPageSize: 10,
+          showSizeChanger: true,
+          pageSizeOptions: ['10', '20', '50'],
+          defaultCurrent: 1,
+          // current: props.currentPage,
+          total: 80, //TODO
+          // pageSize: props.membersPerPage,
+          onChange: props.handlePageChange,
+          onShowSizeChange: props.handleShowSizeChange,
+        }}
+
         dataSource={props.memberList}
         // loading={true}
 
@@ -38,7 +51,7 @@ const MemberTable = (props) => {
           <List.Item key={item.title}>
             <Row>
               <Col span={6}>
-                <Row type="flex" justify="space-around" align="middle">
+                <Row type="flex" justify="space-around" align="middle"  >
                   <Col span={6}>
                     <MemberImage email={item.email || ''} d={"mp"}/>
                   </Col>
@@ -48,7 +61,8 @@ const MemberTable = (props) => {
                 </Row>
               </Col>
               <Col span={18}>
-                <a>{(item.title ? item.title.toUpperCase() + ' ' : '') + item.firstName + ' ' + item.lastName}</a>
+                <div>{formatLabel(item.firstName, 'Ad')}</div>
+                <a>{(item.title ? item.title.toUpperCase() + ' ' : '') + formatLabel(item.firstName, 'Ad')+ ' ' + item.lastName}</a>
                 <div>Address: {[item.city, item.state, item.country].filter(Boolean).join(", ")}</div>
                 <div>Research Interests: {item.interests.join(", ")}</div>
               </Col>
