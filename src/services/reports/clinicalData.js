737fe0dee750894fef68d5fc8f8d8e0da4cab5c1
Julien Bérubé <julien.berube@gmail.com>
2019-10-23 15:31:12 -0400

    feat/2031 send desired filename of the report

--- src/services/reports/clinicalData.js
+++ src/services/reports/clinicalData.js
@@ -1,3 +1,5 @@
+import { format } from 'date-fns';
+
 import { EGO_JWT_KEY } from 'common/constants';
 import downloader from 'common/downloader';
 import { arrangerProjectId, reportsApiRoot } from 'common/injectGlobals';
@@ -6,6 +8,7 @@ const url = `${reportsApiRoot}/reports/clinical-data`;
 
 export default sqon => {
   const egoJwt = localStorage.getItem(EGO_JWT_KEY);
+  const filename = format(new Date(), `[participants_clinical_]YYYYMMDD[.xlsx]`);
   return downloader({
     url,
     method: 'POST',
@@ -13,10 +16,12 @@ export default sqon => {
     data: {
       sqon,
       projectId: arrangerProjectId,
+      filename,
     },
     headers: {
       Authorization: `Bearer ${egoJwt}`,
       Accept: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'Content-Type': 'application/json',
     },
   });
 };
