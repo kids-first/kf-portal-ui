4c51a611f5fc935452093c6f626c44056d6bab6b
Julien Bérubé <julien.berube@gmail.com>
2019-11-14 15:36:03 -0500

    fix : Top Studies chart is cropped after resize

--- src/components/UserDashboard/index.js
+++ src/components/UserDashboard/index.js
@@ -3,28 +3,24 @@ import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import { compose, branch, renderComponent } from 'recompose';
 import { injectState } from 'freactal';
-import { startCase } from 'lodash';
 // [NEXT] replace by antd's grid system
 import { Row, Col } from 'react-grid-system';
 
-import ChartLoadGate from 'chartkit/components/ChartLoadGate';
-import DataProvider from 'chartkit/components/DataProvider';
-import MultiHeader from 'uikit/Multicard/MultiHeader';
-import CardContentSpinner from 'components/Charts/ChartContentSpinner';
-
-import { StudiesChart } from './charts';
 import { withApi } from 'services/api';
-import { publicStatsApiRoot, arrangerProjectId } from 'common/injectGlobals';
 
 import SavedQueries from './SavedQueries';
 import AuthorizedStudies from './AuthorizedStudies';
 import CavaticaProjects from './CavaticaProjects';
 
-import { DashboardCard, DashboardMulticard } from './styles';
+import { DashboardCard } from './styles';
 import { SizeProvider } from 'components/Utils';
-import DashboardCardError from './DashboardCardError';
 
-import { MostFrequentDiagnosesChart, MemberResearchInterestsChart } from 'components/Charts';
+import {
+  MostFrequentDiagnosesChart,
+  MemberResearchInterestsChart,
+  MostParticipantsStudiesChart,
+} from 'components/Charts';
+
 import { userDashboardContainer, dashboardTitle } from './UserDashboard.module.css';
 
 const Container = ({ className = '', children }) => (
@@ -81,46 +77,9 @@ export default compose(
             <CavaticaProjects />
           </CardSlot>
           <CardSlot sm={12} md={6} lg={6} xl={4}>
-            <DataProvider
-              url={`${publicStatsApiRoot}${arrangerProjectId}/studies`}
-              api={api}
-              transform={data =>
-                (data.studies || []).map(study => ({ ...study, label: startCase(study.name) }))
-              }
-            >
-              {fetchedState => {
-                const data = fetchedState.data;
-                const studies = data && data.length;
-                const participants = data
-                  ? data.reduce((prev, el) => prev + (el.familyMembers + el.probands), 0)
-                  : null;
-
-                return (
-                  <DashboardMulticard
-                    tabs={[
-                      {
-                        headerComponent: cardProps => (
-                          <MultiHeader
-                            headings={[
-                              { title: 'Studies', badge: studies },
-                              { title: 'Participants', badge: participants },
-                            ]}
-                          />
-                        ),
-                        component: cardProps => (
-                          <ChartLoadGate
-                            Error={DashboardCardError}
-                            Loader={CardContentSpinner}
-                            fetchedState={fetchedState}
-                            Chart={StudiesChart}
-                          />
-                        ),
-                      },
-                    ]}
-                  />
-                );
-              }}
-            </DataProvider>
+            <DashboardCard showHeader={false}>
+              <MostParticipantsStudiesChart />
+            </DashboardCard>
           </CardSlot>
           <CardSlot sm={12} md={6} lg={6} xl={4}>
             <DashboardCard title="Member Research Interests">
--- src/uikit/Card/CardHeader.js
+++ src/uikit/Card/CardHeader.js
@@ -10,11 +10,10 @@ const Heading = styleComponent('h2', cardHeading);
 
 export const Badge = styleComponent('div', cardBadge);
 
-const CardHeader = ({ title, badge = null, children, className, ...props }) => (
-  <HeaderRow className={className}>
-    <Heading {...props}>{title}</Heading>
-    {badge && <Badge>{badge}</Badge>}
-  </HeaderRow>
-);
-
-export default CardHeader;
+export default ({ title, badge = null, children, className, ...props }) =>
+  title ? (
+    <HeaderRow className={className}>
+      <Heading {...props}>{title}</Heading>
+      {badge && <Badge>{badge}</Badge>}
+    </HeaderRow>
+  ) : null;
