7e68c8d20e3d4549ec78652daac3521127776bad
Julien Bérubé <julien.berube@gmail.com>
2019-11-27 15:12:15 -0500

    ref/build re-adding routes with few dependencies ...wip

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { compose } from 'recompose';
 import { injectState } from 'freactal';
 import { Route, Switch, Redirect } from 'react-router-dom';
-// import Toast from 'uikit/Toast';
+import Toast from 'uikit/Toast';
 // import { Dashboard as ArrangerDashboardLegacy } from '@kfarranger/components/dist';
 
-// import Modal from 'components/Modal';
-// import GlobalModal from 'components/Modal/GlobalModal';
+import Modal from 'components/Modal';
+import GlobalModal from 'components/Modal/GlobalModal';
 // import UserProfile from 'components/UserProfile';
 import UserDashboard from 'components/UserDashboard';
 // import FileRepo from 'components/FileRepo';
@@ -17,13 +17,13 @@ import LoginFooter from 'components/Login/LoginFooter';
 // import ParticipantEntity from './components/EntityPage/Participant';
 // import CohortBuilder from './components/CohortBuilder';
 // import MemberSearchPage from './components/MemberSearchPage';
-// import AuthRedirect from 'components/AuthRedirect';
+import AuthRedirect from 'components/AuthRedirect';
 import SideImagePage from 'components/SideImagePage';
 import Page from 'components/Page';
 // import { FixedFooterPage } from 'components/Page';
 import ContextProvider from 'components/ContextProvider';
-// import Error from 'components/Error';
-// import FenceAuthRedirect from 'components/Fence/FenceAuthRedirect';
+import Error from 'components/Error';
+import FenceAuthRedirect from 'components/Fence/FenceAuthRedirect';
 
 import loginImage from 'assets/smiling-girl.jpg';
 // import joinImage from 'assets/smiling-boy.jpg';
@@ -32,7 +32,7 @@ import logo from 'assets/logo-kids-first-data-portal.svg';
 import { requireLogin } from './common/injectGlobals';
 import { withApi } from 'services/api';
 // import { initializeApi, ApiContext } from 'services/api';
-// import { DCF, GEN3 } from 'common/constants';
+import { DCF, GEN3 } from 'common/constants';
 // import ArrangerAdmin from 'components/ArrangerAdmin';
 import ErrorBoundary from 'ErrorBoundary';
 import ROUTES from 'common/routes';
@@ -69,7 +69,7 @@ const App = compose(
   injectState,
   withApi,
 )(({ state, api }) => {
-  const { loggedInUser, /* toast,*/ isLoadingUser } = state;
+  const { loggedInUser, toast, isLoadingUser } = state;
 
   const showDashboardIfLoggedIn = props => {
     return forceSelectRole({
@@ -131,7 +131,9 @@ const App = compose(
             })
           }
         />
+        */}
         <Route path={ROUTES.authRedirect} exact component={AuthRedirect} />
+        {/*
         <Route
           path={ROUTES.orcid}
           exact
@@ -294,15 +296,15 @@ const App = compose(
             return showDashboardIfLoggedIn(props);
           }}
         />
-        {/*
+
         <Route path={ROUTES.gen3Redirect} exact render={() => <FenceAuthRedirect fence={GEN3} />} />
         <Route path={ROUTES.dcfRedirect} exact render={() => <FenceAuthRedirect fence={DCF} />} />
-        <Route path={ROUTES.error} exact render={() => <Error />} /> */}
+        <Route path={ROUTES.error} exact render={() => <Error />} />
         <Redirect from="*" to={ROUTES.dashboard} />
       </Switch>
-      {/* <Modal /> */}
-      {/* <GlobalModal /> */}
-      {/* <Toast {...toast}>{toast.component}</Toast> */}
+      {<Modal />}
+      {<GlobalModal />}
+      {<Toast {...toast}>{toast.component}</Toast>}
     </div>
   );
 });
