48f74ef0de7d54610658de561687ea6bf32ab316
evans-g-crsj <eg.crsj@gmail.com>
2020-01-20 18:03:28 -0500

    remove code containing libs related to 'emotion' (react-emotion, emotion, emotion-theming).

--- src/components/DownloadManifestModal.js
+++ src/components/DownloadManifestModal.js
@@ -8,35 +8,12 @@ import { TealActionButton } from 'uikit/Button';
 import { ModalFooter, ModalWarning } from './Modal';
 import LoadingOnClick from 'components/LoadingOnClick';
 
-// taken out of commission temporarily
-// import GenerateManifestSet from './GenerateManifestSet';
-
 export const DownloadManifestModalFooter = ({
   downloadLoading,
   onDownloadClick,
-  // ----- the following props are out of commission temporarily -----
-  // sqon,
-  // projectId,
-  // setWarning,
-  // api,
-  // setId,
-  // setSetId,
-  // onManifestGenerated = () => { },
 }) => (
   <ModalFooter showSubmit={false}>
     <Row style={{ flex: '1', height: '100%' }} center>
-      {/* // NOTE: feature disabled temporarily, likely to return so we are leaving this here
-          <GenerateManifestSet
-          {...{
-            sqon,
-            projectId,
-            setWarning,
-            onManifestGenerated,
-            api,
-            setId,
-            setSetId,
-          }}
-        /> */}
     </Row>
     <LoadingOnClick
       onClick={onDownloadClick}
