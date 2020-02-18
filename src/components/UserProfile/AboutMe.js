aff7eaafed32e83c507ded05afd7716ededa5bb5
evans-g-crsj <eg.crsj@gmail.com>
2020-02-18 16:43:03 -0500

    Peer Review

--- src/components/UserProfile/AboutMe.js
+++ src/components/UserProfile/AboutMe.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Row } from 'antd';
 import ContactReadOnly from 'components/UserProfile/ContactReadOnly';
@@ -7,35 +7,32 @@ import EditToggle from 'components/UserProfile/EditToggle';
 import ProfileReadOnly from './ProfileReadOnly';
 import ProfileEditable from './ProfileEditable';
 
-const AboutMe = props => {
-  const { canEdit, profile, updateProfileCb, isProfileUpdating, loggedInUser } = props;
-  return (
-    <React.Fragment>
-      <Row align={'middle'} className={'am-profile-row'}>
-        <EditToggle
-          isProfileUpdating={isProfileUpdating}
-          data={profile}
-          canEdit={canEdit}
-          loggedInUser={loggedInUser}
-          ReadOnlyComponent={ProfileReadOnly}
-          EditableComponent={ProfileEditable}
-          updateProfileCb={updateProfileCb}
-        />
-      </Row>
-      <Row>
-        <EditToggle
-          isProfileUpdating={isProfileUpdating}
-          data={profile}
-          canEdit={canEdit}
-          loggedInUser={loggedInUser}
-          ReadOnlyComponent={ContactReadOnly}
-          EditableComponent={ContactEditForm}
-          updateProfileCb={updateProfileCb}
-        />
-      </Row>
-    </React.Fragment>
-  );
-};
+const AboutMe = ({ canEdit, profile, updateProfileCb, isProfileUpdating, loggedInUser }) => (
+  <Fragment>
+    <Row align={'middle'} className={'am-profile-row'}>
+      <EditToggle
+        isProfileUpdating={isProfileUpdating}
+        data={profile}
+        canEdit={canEdit}
+        loggedInUser={loggedInUser}
+        ReadOnlyComponent={ProfileReadOnly}
+        EditableComponent={ProfileEditable}
+        updateProfileCb={updateProfileCb}
+      />
+    </Row>
+    <Row>
+      <EditToggle
+        isProfileUpdating={isProfileUpdating}
+        data={profile}
+        canEdit={canEdit}
+        loggedInUser={loggedInUser}
+        ReadOnlyComponent={ContactReadOnly}
+        EditableComponent={ContactEditForm}
+        updateProfileCb={updateProfileCb}
+      />
+    </Row>
+  </Fragment>
+);
 
 AboutMe.propTypes = {
   canEdit: PropTypes.bool.isRequired,
