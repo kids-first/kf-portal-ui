e8f49e214f0bc4d6172bfea71d9bd8b735f56891
Francis Lavoie <francisl.crsj@gmail.com>
2020-02-18 15:51:52 -0500

    fix: 2285 - Improve design of the result header

--- src/components/CohortBuilder/Results.js
+++ src/components/CohortBuilder/Results.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -6,39 +6,37 @@ import gql from 'graphql-tag';
 import { compose } from 'recompose';
 import isEmpty from 'lodash/isEmpty';
 import get from 'lodash/get';
+import { injectState } from 'freactal';
+import saveSet from '@kfarranger/components/dist/utils/saveSet';
 
-import ContentBar from './ContentBar';
-import Summary from './Summary';
 import Row from 'uikit/Row';
 import ViewLink from 'uikit/ViewLink';
 import { H2 } from 'uikit/Headings';
-import ParticipantsTableView from './ParticipantsTableView';
+import LoadingSpinner from 'uikit/LoadingSpinner';
+import LinkWithLoader from 'uikit/LinkWithLoader';
+
 import SummaryIcon from 'icons/AllAppsMenuIcon';
 import TableViewIcon from 'icons/TableViewIcon';
 import DemographicIcon from 'icons/DemographicIcon';
-import { withApi } from 'services/api';
-import TableErrorView from './ParticipantsTableView/TableErrorView';
-import QueriesResolver from './QueriesResolver';
-import LoadingSpinner from 'uikit/LoadingSpinner';
-import EmptyCohortOverlay from './EmptyCohortOverlay';
-import LinkWithLoader from 'uikit/LinkWithLoader';
-import { createFileRepoLink } from './util';
-import { injectState } from 'freactal';
-import saveSet from '@kfarranger/components/dist/utils/saveSet';
-import graphql from 'services/arranger';
 import FilesIcon from 'icons/FilesIcon';
 import familyMembers from 'assets/icon-families-grey.svg';
-import { setActiveView } from './actionCreators';
 
-import { styleComponent } from 'components/Utils';
+import { withApi } from 'services/api';
+import graphql from 'services/arranger';
+
+import TableErrorView from './ParticipantsTableView/TableErrorView';
+import ParticipantsTableView from './ParticipantsTableView';
+import QueriesResolver from './QueriesResolver';
+import EmptyCohortOverlay from './EmptyCohortOverlay';
+import { createFileRepoLink } from './util';
+import ContentBar from './ContentBar';
+import Summary from './Summary';
+import { setActiveView } from './actionCreators';
 
 import './Results.css';
 
 const SUMMARY = 'summary';
 const TABLE = 'table';
-
-const PurpleLinkWithLoader = styleComponent(LinkWithLoader, 'cb-purple-link cb-sub-heading');
-const PurpleLink = styleComponent(Link, 'cb-purple-link cb-sub-heading');
 
 const generateAllFilesLink = async (user, api, files) => {
   const sqon = {
@@ -120,139 +118,135 @@ const cohortResultsQuery = sqon => ({
   },
 });
 
-class Results extends React.Component {
-  static propTypes = {
-    activeSqonIndex: PropTypes.number.isRequired,
-    sqon: PropTypes.object,
-    onRemoveFromCohort: PropTypes.func.isRequired,
-    setActiveView: PropTypes.func.isRequired,
-    activeView: PropTypes.string.isRequired,
-  };
+const Results = ({
+  activeView,
+  activeSqonIndex,
+  setActiveView,
+  sqon = { op: 'and', content: [] },
+  api,
+  state,
+  onRemoveFromCohort,
+}) => (
+  <QueriesResolver name="GQL_RESULT_QUERIES" api={api} queries={[cohortResultsQuery(sqon)]}>
+    {({ isLoading, data, error }) => {
+      const resultsData = data[0];
+      const participantCount = get(resultsData, 'participantCount', null);
+      const familiesCount = get(resultsData, 'familiesCount', null);
+      const cohortIsEmpty = (!isLoading && !resultsData) || participantCount === 0;
 
-  render() {
-    const {
-      activeView,
-      activeSqonIndex,
-      setActiveView,
-      sqon = { op: 'and', content: [] },
-      api,
-      state,
-      onRemoveFromCohort,
-    } = this.props;
+      const filesCountHeading = resultsData
+        ? `${Number(data[0].filesCount || 0).toLocaleString()}`
+        : '';
 
-    return (
-      <QueriesResolver name="GQL_RESULT_QUERIES" api={api} queries={[cohortResultsQuery(sqon)]}>
-        {({ isLoading, data, error }) => {
-          const resultsData = data[0];
-          const participantCount = get(resultsData, 'participantCount', null);
-          const familiesCount = get(resultsData, 'familiesCount', null);
-          const cohortIsEmpty = (!isLoading && !resultsData) || participantCount === 0;
+      const hasNoFile = resultsData ? data[0].filesCount === 0 : true;
 
-          const filesCountHeading = resultsData
-            ? `${Number(data[0].filesCount || 0).toLocaleString()}`
-            : '';
-
-          const hasNoFile = resultsData ? data[0].filesCount === 0 : true;
-
-          return error ? (
-            <TableErrorView error={error} />
-          ) : (
-            <React.Fragment>
-              <ContentBar style={{ padding: '0 30px 0 34px' }}>
-                <Row className="cb-detail">
-                  <div style={{ display: 'flex', marginRight: '14px' }}>
-                    {isEmpty(sqon.content) ? (
-                      <H2>All Data</H2>
-                    ) : (
-                      <React.Fragment>
-                        <H2>Cohort Results</H2>
-                        <h3 className="cb-sub-heading" style={{ fontWeight: 'normal' }}>
-                          for Query {activeSqonIndex + 1}
-                        </h3>
-                      </React.Fragment>
-                    )}
-                  </div>{' '}
-                  {isLoading ? (
-                    <LoadingSpinner />
-                  ) : (
-                    <div className="cb-summary">
-                      <div className="cb-summary-entity">
-                        <h3 className="cb-sub-heading">
-                          <DemographicIcon width="14px" height="17px" />
-                          {Number(participantCount || 0).toLocaleString()}{' '}
-                          {participantCount === 1 ? 'Participant' : 'Participants'}
-                        </h3>
-                      </div>
-                      <div className="cb-summary-entity">
-                        <h3 className="cb-sub-heading">
-                          <img src={familyMembers} alt="" height="13px" />{' '}
-                          {Number(familiesCount || 0).toLocaleString()}{' '}
-                          {familiesCount === 1 ? 'Family' : 'Families'}
-                        </h3>
-                      </div>
-                      <div className="cb-summary-entity">
-                        <h3 className="cb-sub-heading">
-                          <div className="cb-summary-files">
-                            {hasNoFile ? (
-                              <div>
-                                <FilesIcon style={{ marginRight: '6px' }} /> {'0 File'}
-                              </div>
-                            ) : (
-                              <React.Fragment>
-                                <div>
-                                  <FilesIcon />
-                                  {isEmpty(sqon.content) ? (
-                                    <PurpleLink to="/search/file">{filesCountHeading} </PurpleLink>
-                                  ) : (
-                                    <PurpleLinkWithLoader
-                                      replaceText={false}
-                                      getLink={() =>
-                                        generateAllFilesLink(state.loggedInUser, api, data[0].files)
-                                      }
-                                    >
-                                      {filesCountHeading}
-                                    </PurpleLinkWithLoader>
-                                  )}
-                                </div>
-                                <div>Files</div>
-                              </React.Fragment>
-                            )}
+      return error ? (
+        <TableErrorView error={error} />
+      ) : (
+        <Fragment>
+          <ContentBar style={{ padding: '0 30px 0 34px' }}>
+            <Row className="cb-view-links">
+              <ViewLink
+                onClick={() => setActiveView(SUMMARY)}
+                active={activeView === SUMMARY}
+                Icon={SummaryIcon}
+              >
+                Summary View
+              </ViewLink>
+              <ViewLink
+                onClick={() => setActiveView(TABLE)}
+                active={activeView === TABLE}
+                Icon={TableViewIcon}
+              >
+                Table View
+              </ViewLink>
+            </Row>
+            <Row className="cb-detail">
+              {isEmpty(sqon.content) ? (
+                <H2>All Data</H2>
+              ) : (
+                <Fragment>
+                  <H2>Cohort Results</H2>
+                  <h3 className="cb-sub-heading" style={{ fontWeight: 'normal' }}>
+                    for Query {activeSqonIndex + 1}
+                  </h3>
+                </Fragment>
+              )}
+              {isLoading ? (
+                <LoadingSpinner />
+              ) : (
+                <div className="cb-summary">
+                  <div className="cb-summary-entity">
+                    <h3 className="cb-sub-heading">
+                      <DemographicIcon width="14px" height="17px" />
+                      {`${Number(participantCount || 0).toLocaleString()} ${
+                        participantCount === 1 ? 'Participant' : 'Participants'
+                      }`}
+                    </h3>
+                  </div>
+                  <div className="cb-summary-entity">
+                    <h3 className="cb-sub-heading">
+                      <img src={familyMembers} alt="" height="13px" />
+                      {`${Number(familiesCount || 0).toLocaleString()} ${
+                        familiesCount === 1 ? 'Family' : 'Families'
+                      }`}
+                    </h3>
+                  </div>
+                  <div className="cb-summary-entity">
+                    <h3 className="cb-sub-heading">
+                      <div className="cb-summary-files">
+                        {hasNoFile ? (
+                          <div>
+                            <FilesIcon style={{ marginRight: '6px' }} /> {'0 File'}
                           </div>
-                        </h3>
+                        ) : (
+                          <Fragment>
+                            <div>
+                              <FilesIcon />
+                              {isEmpty(sqon.content) ? (
+                                <Link className="cb-purple-link cb-sub-heading" to="/search/file">
+                                  {filesCountHeading}{' '}
+                                </Link>
+                              ) : (
+                                <LinkWithLoader
+                                  className="cb-purple-link cb-sub-heading"
+                                  replaceText={false}
+                                  getLink={() =>
+                                    generateAllFilesLink(state.loggedInUser, api, data[0].files)
+                                  }
+                                >
+                                  {filesCountHeading}
+                                </LinkWithLoader>
+                              )}
+                            </div>
+                            <div>Files</div>
+                          </Fragment>
+                        )}
                       </div>
-                    </div>
-                  )}
-                </Row>
+                    </h3>
+                  </div>
+                </div>
+              )}
+            </Row>
+          </ContentBar>
+          <div className={`cb-active-view ${activeView}`}>
+            <Summary sqon={sqon} />
+            <ParticipantsTableView sqon={sqon} onRemoveFromCohort={onRemoveFromCohort} />
+            {cohortIsEmpty ? <EmptyCohortOverlay /> : null}
+          </div>
+        </Fragment>
+      );
+    }}
+  </QueriesResolver>
+);
 
-                <Row className="cb-view-links">
-                  <ViewLink
-                    onClick={() => setActiveView(SUMMARY)}
-                    active={activeView === SUMMARY}
-                    Icon={SummaryIcon}
-                  >
-                    Summary View
-                  </ViewLink>
-                  <ViewLink
-                    onClick={() => setActiveView(TABLE)}
-                    active={activeView === TABLE}
-                    Icon={TableViewIcon}
-                  >
-                    Table View
-                  </ViewLink>
-                </Row>
-              </ContentBar>
-              <div className={`cb-active-view ${activeView}`}>
-                <Summary sqon={sqon} />
-                <ParticipantsTableView sqon={sqon} onRemoveFromCohort={onRemoveFromCohort} />
-                {cohortIsEmpty ? <EmptyCohortOverlay /> : null}
-              </div>
-            </React.Fragment>
-          );
-        }}
-      </QueriesResolver>
-    );
-  }
-}
+Results.propTypes = {
+  activeSqonIndex: PropTypes.number.isRequired,
+  sqon: PropTypes.object,
+  onRemoveFromCohort: PropTypes.func.isRequired,
+  setActiveView: PropTypes.func.isRequired,
+  activeView: PropTypes.string.isRequired,
+};
 
 const mapStateToProps = state => {
   const { activeView } = state.ui.cohortBuilderPage;
