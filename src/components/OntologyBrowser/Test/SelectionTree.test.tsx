5147982fc808a4ce7f36ec9cc15f516337758d91
evans-g-crsj <eg.crsj@gmail.com>
2020-04-30 15:58:58 -0400

    Style(Ontology Browser): corrected merge conflict

--- src/components/OntologyBrowser/Test/SelectionTree.test.tsx
+++ src/components/OntologyBrowser/Test/SelectionTree.test.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { treeData } from './mockData';
+import { jestPatchMatchMedia } from '../../../utils';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -14,21 +15,7 @@ describe('In SelectionTree', () => {
   const onItemSelectAll = jest.fn();
   const dataSource: TreeNode[] = treeData;
 
-  beforeAll(() => {
-    Object.defineProperty(window, 'matchMedia', {
-      writable: true,
-      value: jest.fn().mockImplementation((query) => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(), // deprecated
-        removeListener: jest.fn(), // deprecated
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-      })),
-    });
-  });
+  beforeAll(() => jestPatchMatchMedia());
 
   let wrapper: Enzyme.ShallowWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
 
