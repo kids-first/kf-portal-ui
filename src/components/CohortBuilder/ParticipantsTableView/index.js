6d08f82045a31ab9fe1ff661679fcdc853a59a0b
Adrian Paul <adipaul1981@yahoo.ca>
2020-05-13 11:42:10 -0400

    feat(ontologyBrowser): #2464 mondo browser toggle

--- src/components/CohortBuilder/ParticipantsTableView/index.js
+++ src/components/CohortBuilder/ParticipantsTableView/index.js
@@ -28,88 +28,86 @@ const ParticipantsTableView = ({
   sort,
   setSort,
   loggedInUser,
-}) => {
-  return (
-    <QueriesResolver
-      name="GQL_PARTICIPANTS_TABLE"
-      api={api}
-      queries={[participantsQuery(sqon, sort, pageSize, pageIndex)]}
-    >
-      {({ isLoading, data, error }) => {
-        if (error) {
-          return (
-            <Card>
-              <TableErrorView error={error} />
-            </Card>
-          );
-        }
-        const isRowSelected = (node) =>
-          allRowsSelected || selectedRows.some((row) => row === node.participantId);
-
-        const dataWithRowSelection = data[0]
-          ? data[0].nodes.map((node) => ({ ...node, selected: isRowSelected(node) }))
-          : [];
-
-        const selectionSQON = selectedRows.length
-          ? {
-              op: 'and',
-              content: [{ op: 'in', content: { field: 'kf_id', value: selectedRows } }],
-            }
-          : sqon;
-
+}) => (
+  <QueriesResolver
+    name="GQL_PARTICIPANTS_TABLE"
+    api={api}
+    queries={[participantsQuery(sqon, sort, pageSize, pageIndex)]}
+  >
+    {({ isLoading, data, error }) => {
+      if (error) {
         return (
-          <Card showHeader={false}>
-            <ParticipantsTable
-              sqon={selectionSQON}
-              loading={isLoading}
-              data={dataWithRowSelection}
-              api={api}
-              sort={sort}
-              dataTotalCount={data[0] ? data[0].total : 0}
-              downloadName={'participant-table'}
-              onFetchData={({ page, pageSize, sorted }) => {
-                const sorting = sorted
-                  .filter((s) => SORTABLE_FIELDS_MAPPING.has(s.id))
-                  .map((s) => ({
-                    field: SORTABLE_FIELDS_MAPPING.get(s.id),
-                    order: s.desc ? 'desc' : 'asc',
-                  }));
-                setPageIndex(page);
-                setPageSize(pageSize);
-                setSort(sorting);
-              }}
-              onRowSelected={(checked, row) => {
-                const rowId = row.participantId;
-                if (checked) {
-                  setSelectedRows((s) => s.concat(rowId));
-                  return;
-                }
-                setSelectedRows((s) => s.filter((row) => row !== rowId));
-              }}
-              onAllRowsSelected={(checked) => {
-                // don't keep individual rows selected when "select all" is checked
-                //  to avoid having them selected after "unselect all"
-                setAllRowsSelected(() => checked);
-                setSelectedRows(() => []);
-              }}
-              onClearSelected={() => {
-                setAllRowsSelected(() => false);
-                setSelectedRows(() => []);
-              }}
-              selectedRows={selectedRows}
-              allRowsSelected={allRowsSelected}
-              loggedInUser={loggedInUser}
-            />
+          <Card>
+            <TableErrorView error={error} />
           </Card>
         );
-      }}
-    </QueriesResolver>
-  );
-};
+      }
+      const isRowSelected = (node) =>
+        allRowsSelected || selectedRows.some((row) => row === node.participantId);
+
+      const dataWithRowSelection = data[0]
+        ? data[0].nodes.map((node) => ({ ...node, selected: isRowSelected(node) }))
+        : [];
+
+      const selectionSQON = selectedRows.length
+        ? {
+            op: 'and',
+            content: [{ op: 'in', content: { field: 'kf_id', value: selectedRows } }],
+          }
+        : sqon;
+
+      return (
+        <Card showHeader={false}>
+          <ParticipantsTable
+            sqon={selectionSQON}
+            loading={isLoading}
+            data={dataWithRowSelection}
+            api={api}
+            sort={sort}
+            dataTotalCount={data[0] ? data[0].total : 0}
+            downloadName={'participant-table'}
+            onFetchData={({ page, pageSize, sorted }) => {
+              const sorting = sorted
+                .filter((s) => SORTABLE_FIELDS_MAPPING.has(s.id))
+                .map((s) => ({
+                  field: SORTABLE_FIELDS_MAPPING.get(s.id),
+                  order: s.desc ? 'desc' : 'asc',
+                }));
+              setPageIndex(page);
+              setPageSize(pageSize);
+              setSort(sorting);
+            }}
+            onRowSelected={(checked, row) => {
+              const rowId = row.participantId;
+              if (checked) {
+                setSelectedRows((s) => s.concat(rowId));
+                return;
+              }
+              setSelectedRows((s) => s.filter((row) => row !== rowId));
+            }}
+            onAllRowsSelected={(checked) => {
+              // don't keep individual rows selected when "select all" is checked
+              //  to avoid having them selected after "unselect all"
+              setAllRowsSelected(() => checked);
+              setSelectedRows(() => []);
+            }}
+            onClearSelected={() => {
+              setAllRowsSelected(() => false);
+              setSelectedRows(() => []);
+            }}
+            selectedRows={selectedRows}
+            allRowsSelected={allRowsSelected}
+            loggedInUser={loggedInUser}
+          />
+        </Card>
+      );
+    }}
+  </QueriesResolver>
+);
 
 ParticipantsTableView.propTypes = {
   sqon: PropTypes.object.isRequired,
-  loggedInUser: PropTypes.object.isRequired,
+  loggedInUser: PropTypes.object,
 };
 
 const mapStateToProps = (state) => ({
