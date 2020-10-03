654c900c92cefb8ae441ec07a3c33fecba0934cd
evans-g-crsj <eg.crsj@gmail.com>
2020-10-02 23:14:53 -0400

    fix + refactor save query modal (fileRepo)

--- src/components/LoadShareSaveDeleteQuery/SaveQuery.js
+++ src/components/LoadShareSaveDeleteQuery/SaveQuery.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import Component from 'react-component-component';
-import { Route } from 'react-router-dom';
-import urlJoin from 'url-join';
-import Spinner from 'react-spinkit';
-import Tooltip from 'uikit/Tooltip';
-import { ModalFooter } from 'components/Modal';
-import { arrangerApiRoot } from 'common/injectGlobals';
 import sqonToName from 'common/sqonToName';
 import shortenApi from './shortenApi';
-import { Button } from 'antd';
+import { Button, Modal, notification } from 'antd';
 import { trackUserInteraction, TRACKING_EVENTS } from 'services/analyticsTracking';
-import { niceWhiteButton } from 'theme/tempTheme.module.css';
 import './LoadShareSaveDeleteQuery.css';
 import { SaveOutlined } from '@ant-design/icons';
 import PropTypes from 'prop-types';
+import { Input, Typography } from 'antd';
+import { withRouter } from 'react-router-dom';
 
-export default class SaveQuery extends React.Component {
+const { Text } = Typography;
+
+class SaveQuery extends React.Component {
   state = {
-    link: null,
     loading: false,
     queryName: sqonToName({ filters: this.props.sqon }),
     open: false,
+    openModal: false,
   };
 
   static propTypes = {
@@ -29,144 +25,128 @@ export default class SaveQuery extends React.Component {
     disabled: PropTypes.bool,
     sqon: PropTypes.object,
     api: PropTypes.func.isRequired,
+    history: PropTypes.array.isRequired,
   };
 
   componentDidUpdate(prevProps) {
-    if (prevProps.sqon !== this.props.sqon) {
-      this.setState({ queryName: sqonToName({ filters: this.props.sqon }) });
+    const { sqon } = this.props;
+    if (prevProps.sqon !== sqon) {
+      this.setState({ queryName: sqonToName({ filters: sqon }) });
     }
   }
 
-  save = () => {
-    const { stats, sqon, api, loggedInUser } = this.props;
+  onCloseModal = () => this.setState({ openModal: false, loading: false });
+
+  onOpenModal = () => this.setState({ openModal: true });
+
+  save = async () => {
+    const { stats, sqon, api, loggedInUser, history } = this.props;
+    const { queryName } = this.state;
+
     this.setState({ loading: true });
-    shortenApi({
-      stats,
-      sqon,
-      queryName: this.state.queryName,
-      loggedInUser,
-      api,
-      sharedPublicly: false,
-    })
-      .then((data) => {
-        this.setState({
-          loading: false,
-          link: urlJoin(arrangerApiRoot, 's', data.id),
-        });
-        const trackingSqon = { ...sqon, id: data.id };
-        let savedQueryInteraction = {
-          category: TRACKING_EVENTS.categories.fileRepo.dataTable,
-          action: TRACKING_EVENTS.actions.query.save,
-          label: JSON.stringify(trackingSqon),
-        };
-        trackUserInteraction(savedQueryInteraction);
-      })
-      .catch(() => {
-        this.setState({ error: true, loading: false });
-        trackUserInteraction({
-          category: TRACKING_EVENTS.categories.fileRepo.dataTable,
-          action: TRACKING_EVENTS.actions.query.save + ' FAILED',
-        });
+    try {
+      const data = await shortenApi({
+        stats,
+        sqon,
+        queryName,
+        loggedInUser,
+        api,
+        sharedPublicly: false,
       });
+
+      await trackUserInteraction({
+        category: TRACKING_EVENTS.categories.fileRepo.dataTable,
+        action: TRACKING_EVENTS.actions.query.save,
+        label: JSON.stringify({ ...sqon, id: data.id }),
+      });
+
+      notification.success({
+        key: 'view_in_my_saved_queries',
+        message: 'Query saved succesfully!',
+        description: (
+          <div>
+            <Button
+              type={'link'}
+              onClick={async () => {
+                await trackUserInteraction({
+                  category: TRACKING_EVENTS.categories.fileRepo.dataTable,
+                  action: 'View in My Saved Queries',
+                });
+                notification.close('view_in_my_saved_queries');
+                history.push('/dashboard');
+              }}
+            >
+              View in My Saved Queries
+            </Button>
+          </div>
+        ),
+        duration: 5,
+      });
+      this.onCloseModal();
+    } catch (e) {
+      await trackUserInteraction({
+        category: TRACKING_EVENTS.categories.fileRepo.dataTable,
+        action: TRACKING_EVENTS.actions.query.save + ' FAILED',
+      });
+
+      this.onCloseModal();
+      console.error(e);
+      notification.error({
+        message: 'Error',
+        description:
+          'An error occured while trying to save your query. Please, try again or contact our support',
+        duration: 5,
+      });
+    } finally {
+      this.setState({ loading: false });
+    }
   };
+
+  onChange = (e) => this.setState({ queryName: e.target.value });
 
   render() {
     const { disabled, loggedInUser } = this.props;
+    const { openModal, loading, queryName } = this.state;
+
+    if (!loggedInUser) {
+      return null;
+    }
+
     return (
-      !!loggedInUser && (
-        <Button
-          icon={<SaveOutlined />}
-          disabled={disabled}
-          onClick={() =>
-            !disabled &&
-            this.setState({ open: true }, () => {
-              // so hacky, but couldn't get any reasonable approach to work
-              let id = setInterval(() => {
-                if (this.input) {
-                  this.input.focus();
-                  clearInterval(id);
-                }
-              });
-            })
-          }
+      <>
+        <Modal
+          visible={openModal}
+          title={'Save Query'}
+          onCancel={this.onCloseModal}
+          footer={[
+            <Button key="cancel" onClick={this.onCloseModal}>
+              Cancel
+            </Button>,
+            <Button
+              key="save"
+              type="primary"
+              maxLength={300}
+              disabled={disabled || !queryName}
+              loading={loading}
+              onClick={this.save}
+            >
+              Save
+            </Button>,
+          ]}
         >
-          <Route>
-            {({ history }) => (
-              <Tooltip
-                position="bottom"
-                onRequestClose={() => {
-                  this.setState({ open: false, link: null });
-                }}
-                interactive
-                open={this.state.open}
-                html={
-                  <Component
-                    initialState={{ saved: false }}
-                    className="saveQuery-tooltip-container"
-                    render={({ state, setState }) => (
-                      <div className="saveQuery-tooltip-content">
-                        <div
-                          className={`saveQuery-tooltip-confirmation ${
-                            state.saved && this.state.link ? 'saved' : ''
-                          }`}
-                        >
-                          <div className="textWrapper">Query saved succesfully!</div>
-                          <div
-                            onClick={() => {
-                              trackUserInteraction({
-                                category: TRACKING_EVENTS.categories.fileRepo.dataTable,
-                                action: 'View in My Saved Queries',
-                              });
-                              history.push('/dashboard');
-                            }}
-                          >
-                            <button className={niceWhiteButton}>View in My Saved Queries</button>
-                          </div>
-                        </div>
-                        <h3 className="saveQuery-heading">
-                          Save Query
-                          {this.state.loading && (
-                            <Spinner
-                              fadeIn="none"
-                              name="circle"
-                              color="purple"
-                              style={{
-                                width: 15,
-                                height: 15,
-                                marginRight: 9,
-                                marginLeft: 'auto',
-                              }}
-                            />
-                          )}
-                        </h3>
-                        <div className="someText1">Save the current configuration of filters</div>
-                        <div className="someText2">Enter a name for your saved query:</div>
-                        <div style={{ marginBottom: '85px' }}>
-                          <input
-                            className="someText3"
-                            type="text"
-                            value={this.state.queryName}
-                            ref={(input) => {
-                              this.input = input;
-                            }}
-                            onChange={(e) => this.setState({ queryName: e.target.value })}
-                          />
-                        </div>
-                        <ModalFooter
-                          handleSubmit={() => setState({ saved: true }, this.save)}
-                          handleCancelClick={() => this.setState({ open: false })}
-                        />
-                      </div>
-                    )}
-                  />
-                }
-              >
-                &nbsp;Save
-              </Tooltip>
-            )}
-          </Route>
+          <Text>Save the current configuration of filters</Text>
+          <Input
+            value={queryName}
+            placeholder="Enter a name for your saved query"
+            onChange={this.onChange}
+          />
+        </Modal>
+        <Button icon={<SaveOutlined />} disabled={disabled} onClick={this.onOpenModal}>
+          Save
         </Button>
-      )
+      </>
     );
   }
 }
+
+export default withRouter(SaveQuery);
