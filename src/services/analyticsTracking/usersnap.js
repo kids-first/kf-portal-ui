9f701245258d7c3f7d7df8f73ee5957c882395b9
evans-g-crsj <eg.crsj@gmail.com>
2020-11-30 11:44:58 -0500

    incorporate script given by administrator

--- src/services/analyticsTracking/usersnap.js
+++ src/services/analyticsTracking/usersnap.js
@@ -1,22 +1,15 @@
 //  ref https://help.usersnap.com/docs/developers-api-documentation#section-basics-for-usersnap-classic-widget
 import { userSnapApiKey } from '../../common/injectGlobals';
-import colors from 'style/themes/default/_colors.scss';
 
 export const initUserSnap = () => {
   if (!userSnapApiKey) {
     return;
   }
-  window.onUsersnapLoad = function (api) {
-    api.init({
-      colors: {
-        primary: colors.btnPrimaryBg,
-        secondary: '#AFFEEE',
-      },
-    });
-    window.Usersnap = api;
+  window.onUsersnapCXLoad = function (api) {
+    api.init();
   };
   const script = document.createElement('script');
   script.async = true;
-  script.src = `https://api.usersnap.com/load/${userSnapApiKey}.js?onload=onUsersnapLoad`;
+  script.src = `https://widget.usersnap.com/load/${userSnapApiKey}?onload=onUsersnapCXLoad`;
   document.head.append(script);
 };
