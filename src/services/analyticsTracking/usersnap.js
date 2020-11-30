c4cf973c8f47341e88db5cb920c627343c96ba48
Evans Girard <54366437+evans-g-crsj@users.noreply.github.com>
2020-11-30 14:23:51 -0500

    remove hotjar script (#2848)

--- src/services/analyticsTracking/usersnap.js
+++ src/services/analyticsTracking/usersnap.js
@@ -1,13 +1,14 @@
-//  ref https://help.usersnap.com/docs/developers-api-documentation#section-basics-for-usersnap-classic-widget
 import { userSnapApiKey } from '../../common/injectGlobals';
 
 export const initUserSnap = () => {
   if (!userSnapApiKey) {
     return;
   }
+
   window.onUsersnapCXLoad = function (api) {
     api.init();
   };
+
   const script = document.createElement('script');
   script.async = true;
   script.src = `https://widget.usersnap.com/load/${userSnapApiKey}?onload=onUsersnapCXLoad`;
