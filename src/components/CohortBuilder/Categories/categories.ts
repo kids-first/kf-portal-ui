50d95d04d4a4d1825f1f831d0f02d39286dd95e6
adipaul1981 <adipaul1981@yahoo.ca>
2021-02-09 11:01:18 -0500

    revert volumeul filter (#2942)

--- src/components/CohortBuilder/Categories/categories.ts
+++ src/components/CohortBuilder/Categories/categories.ts
@@ -52,9 +52,6 @@ export const CATEGORY_FIELDS = {
     'biospecimens.source_text_tissue_type',
     'biospecimens.ncit_id_tissue_type',
     'biospecimens.source_text_tumor_descriptor',
-    'biospecimens.volume_ul',
-    'biospecimens.shipment_origin',
-    'biospecimens.concentration_mg_per_ml',
 
     // Available Data
     'available_data_types',
@@ -106,9 +103,6 @@ export const CATEGORY_FIELDS = {
     'biospecimens.source_text_tissue_type',
     'biospecimens.ncit_id_tissue_type',
     'biospecimens.source_text_tumor_descriptor',
-    'biospecimens.volume_ul',
-    'biospecimens.shipment_origin',
-    'biospecimens.concentration_mg_per_ml',
   ],
   availableData: [
     'available_data_types',
