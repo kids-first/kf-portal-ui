62a7e38001baa3c1f5e8586ef479b309e00cecdc
Adrian Paul <adipaul1981@yahoo.ca>
2021-03-31 14:41:50 -0400

    feat: #3020 fix varaint page container

--- src/components/EntityPage/Variant/actions.tsx
+++ src/components/EntityPage/Variant/actions.tsx
@@ -2,13 +2,10 @@ import { useLazyResultQuery } from 'store/graphql/utils/query';
 import { VARIANT_QUERY } from './queries';
 import { VariantEntityResults } from '../../../store/graphql/variants/models';
 
-
 export const useGetVariantEntityPageData = (variables: any): VariantEntityResults => {
   const { loading, result } = useLazyResultQuery<any>(VARIANT_QUERY, {
     variables: variables,
   });
-
-  console.log(result, 'R')
 
   return {
     loading,
