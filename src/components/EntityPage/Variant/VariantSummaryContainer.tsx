ac6fec7999cec6daac79a84c50fdc115727f186e
Adrian Paul <adipaul1981@yahoo.ca>
2021-04-01 13:57:44 -0400

    feat: #3020 fix css container

--- src/components/EntityPage/Variant/VariantSummaryContainer.tsx
+++ src/components/EntityPage/Variant/VariantSummaryContainer.tsx
@@ -1,10 +1,13 @@
 import React, { FC } from 'react';
 import Card from '@ferlab/ui/core/view/GridCard';
+import './VariantSummaryContainer.module.scss';
 
 const VariantSummaryContainer: FC = () => (
-  <div>
+  <div className={'content-container'}>
     <Card>
-      <div></div>
+      <div>
+        <div>rre</div> <div>33</div>
+      </div>
     </Card>
     <Card>
       <div></div>
