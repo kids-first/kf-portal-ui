8ddeb33ff40601dc8e06dd1ba96f1ab91fd75e32
Adrian Paul <adipaul1981@yahoo.ca>
2021-04-01 15:38:40 -0400

    wip variant page

--- src/components/EntityPage/Variant/VariantSummaryContainer.tsx
+++ src/components/EntityPage/Variant/VariantSummaryContainer.tsx
@@ -1,12 +1,42 @@
 import React, { FC } from 'react';
 import Card from '@ferlab/ui/core/view/GridCard';
-import './VariantSummaryContainer.module.scss';
+import './VariantSummaryContainer.css';
+import { VariantEntityResults } from '../../../store/graphql/variants/models';
 
-const VariantSummaryContainer: FC = () => (
+type PageContentProps = {
+  results: VariantEntityResults;
+};
+
+const VariantSummaryContainer: FC<PageContentProps> = () => (
   <div className={'content-container'}>
     <Card>
-      <div>
-        <div>rre</div> <div>33</div>
+      <div className={'grid-top-container'}>
+        <div className={'grid-top-container-item'}>
+          <div style={{ display: 'flex' }}>
+            <div style={{ width: '25%' }}>Chr</div>
+            <div style={{ width: '75%' }}>33</div>
+          </div>
+          <div style={{ display: 'flex' }}>
+            <div style={{ width: '25%' }}>Start</div>
+            <div style={{ width: '75%' }}>33</div>
+          </div>
+          <div style={{ display: 'flex' }}>
+            <div style={{ width: '25%' }}>Allele Alt.</div>
+            <div style={{ width: '75%' }}>33</div>
+          </div>
+          <div style={{ display: 'flex' }}>
+            <div style={{ width: '25%' }}>Allele Réf.</div>
+            <div style={{ width: '75%' }}>33</div>
+          </div>
+        </div>
+        <div className={'grid-top-container-item'}>
+          <div className={'grid-top-container-item'}>
+            <div style={{ display: 'flex' }}>
+              <div style={{ width: '25%' }}>rre</div>
+              <div style={{ width: '75%' }}>33</div>
+            </div>
+          </div>
+        </div>
       </div>
     </Card>
     <Card>
