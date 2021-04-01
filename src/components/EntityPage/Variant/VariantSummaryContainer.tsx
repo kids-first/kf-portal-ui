a856dc7e79fcb9d78925493a5bc745aad222a41b
Adrian Paul <adipaul1981@yahoo.ca>
2021-04-01 16:57:39 -0400

    review comments

--- src/components/EntityPage/Variant/VariantSummaryContainer.tsx
+++ src/components/EntityPage/Variant/VariantSummaryContainer.tsx
@@ -1,48 +1,54 @@
 import React, { FC } from 'react';
 import Card from '@ferlab/ui/core/view/GridCard';
 import './VariantSummaryContainer.css';
-import { VariantEntityResults } from '../../../store/graphql/variants/models';
+import { VariantEntityResults } from 'store/graphql/variants/models';
 
 type PageContentProps = {
   results: VariantEntityResults;
 };
 
-const VariantSummaryContainer: FC<PageContentProps> = () => (
-  <div className={'content-container'}>
-    <Card>
-      <div className={'grid-top-container'}>
-        <div className={'grid-top-container-item'}>
-          <div style={{ display: 'flex' }}>
-            <div style={{ width: '25%' }}>Chr</div>
-            <div style={{ width: '75%' }}>33</div>
-          </div>
-          <div style={{ display: 'flex' }}>
-            <div style={{ width: '25%' }}>Start</div>
-            <div style={{ width: '75%' }}>33</div>
-          </div>
-          <div style={{ display: 'flex' }}>
-            <div style={{ width: '25%' }}>Allele Alt.</div>
-            <div style={{ width: '75%' }}>33</div>
-          </div>
-          <div style={{ display: 'flex' }}>
-            <div style={{ width: '25%' }}>Allele Réf.</div>
-            <div style={{ width: '75%' }}>33</div>
-          </div>
-        </div>
-        <div className={'grid-top-container-item'}>
+const VariantSummaryContainer: FC<PageContentProps> = (studiesResults) => {
+  const variant = studiesResults.results.data?.hits.edges[0];
+  const aggs = studiesResults.results.data?.aggregations;
+  return (
+    <div className={'content-container'}>
+      <Card>
+        <div className={'grid-top-container'}>
           <div className={'grid-top-container-item'}>
             <div style={{ display: 'flex' }}>
-              <div style={{ width: '25%' }}>rre</div>
+              <div style={{ width: '25%' }}>Chr</div>
+              <div style={{ width: '75%' }}>
+                {studiesResults.results.data?.hits.edges[0].node.hgvsg}
+              </div>
+            </div>
+            <div style={{ display: 'flex' }}>
+              <div style={{ width: '25%' }}>Start</div>
+              <div style={{ width: '75%' }}>33</div>
+            </div>
+            <div style={{ display: 'flex' }}>
+              <div style={{ width: '25%' }}>Allele Alt.</div>
+              <div style={{ width: '75%' }}>33</div>
+            </div>
+            <div style={{ display: 'flex' }}>
+              <div style={{ width: '25%' }}>Allele Réf.</div>
               <div style={{ width: '75%' }}>33</div>
             </div>
           </div>
+          <div className={'grid-top-container-item'}>
+            <div className={'grid-top-container-item'}>
+              <div style={{ display: 'flex' }}>
+                <div style={{ width: '25%' }}>rre</div>
+                <div style={{ width: '75%' }}>33</div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </Card>
-    <Card>
-      <div></div>
-    </Card>
-  </div>
-);
+      </Card>
+      <Card>
+        <div></div>
+      </Card>
+    </div>
+  );
+};
 
 export default VariantSummaryContainer;
