c270ad16dcc92cfbc3b7a1813688db6f8485f423
Evans Girard <54366437+evans-g-crsj@users.noreply.github.com>
2021-06-18 13:22:35 -0400

    refactor: #3320 remove unused multicard (#3337)

--- src/components/UserDashboard/styles.js
+++ src/components/UserDashboard/styles.js
@@ -1,22 +1,18 @@
-import Card from 'uikit/Card';
-import {
-  PromptMessageContainer as PMCont,
-  PromptMessageHeading as PMHeading,
-  PromptMessageContent as PMContent,
-} from 'uikit/PromptMessage';
-import Multicard from 'uikit/Multicard';
 import { styleComponent } from 'components/Utils';
 import {
-  dashboardCard,
+  PromptMessageContainer as PMCont,
+  PromptMessageContent as PMContent,
+  PromptMessageHeading as PMHeading,
+} from 'uikit/PromptMessage';
+
+import {
+  cardLink,
+  noteList,
   promptMessageContainer,
   promptMessageContent,
   promptMessageHeading,
-  cardLink,
-  noteList,
 } from './UserDashboard.module.css';
 
-export const DashboardCard = styleComponent(Card, dashboardCard);
-export const DashboardMulticard = styleComponent(Multicard, dashboardCard);
 export const CardLink = styleComponent('a', cardLink);
 export const NoteList = styleComponent('ul', noteList);
 export const PromptMessageContainer = styleComponent(PMCont, promptMessageContainer);
