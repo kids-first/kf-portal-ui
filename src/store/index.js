ecced880b5ec1c411faf98820446beef0cdeea05
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2021-07-22 16:55:28 -0400

    fix(reduxdev): #SKFP-56 use existing dep

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-import { devDebug } from 'common/injectGlobals';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 import subscribe from './subscribers';
@@ -9,11 +9,17 @@ export { default as getPreloadedState } from './statePreloader';
 
 export let store = null;
 export const initStore = (preloadedState = {}) => {
-  store = configureStore({
-    reducer: rootReducer,
-    preloadedState: preloadedState,
-    devTools: devDebug,
+  const composeEnhancers = composeWithDevTools({
+    // Specify extension’s options like name,
+    //  actionsBlacklist, actionsCreators, serialize...
   });
+
+  const enhancer = composeEnhancers(
+    applyMiddleware(thunk),
+    // other store enhancers if any
+  );
+
+  store = createStore(rootReducer, preloadedState, enhancer);
 
   // Hooks the subscribers to the store
   subscribe(store);
