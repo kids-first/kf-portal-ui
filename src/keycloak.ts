67010ff5e70d7c6af825857ad0a3a1c0cc6bf985
evans-g-crsj <eg.crsj@gmail.com>
2021-08-26 16:46:05 -0400

    feat(auth): #skfp9 remove comment

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,7 +1,6 @@
 import Keycloak from 'keycloak-js';
 
 import { kcAuthUrl, kcClientId, kcRealm } from './common/injectGlobals';
-//FIXME env var + get auth
 
 const keycloak = Keycloak({
   realm: kcRealm,
