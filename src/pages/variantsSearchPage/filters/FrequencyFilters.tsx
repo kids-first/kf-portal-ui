87cbf95a82bfeb85707d7546de72c3957e930703
Adrian Paul <adipaul1981@yahoo.ca>
2021-09-20 14:45:08 -0400

    fix: #SKFP-152 fix order frequency filters add collopase_expant feature to filters default col

--- src/pages/variantsSearchPage/filters/FrequencyFilters.tsx
+++ src/pages/variantsSearchPage/filters/FrequencyFilters.tsx
@@ -1,7 +1,7 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { getQueryBuilderCache, useFilters } from '@ferlab/ui/core/data/filters/utils';
 import { resolveSyntheticSqon } from '@ferlab/ui/core/data/sqon/utils';
-import { Layout, Spin } from 'antd';
+import { Button, Layout, Spin } from 'antd';
 
 import { generateFilters } from 'components/Utils/utils';
 import { VARIANT_INDEX, VARIANT_REPO_CACHE_KEY } from 'pages/variantsSearchPage/constants';
@@ -17,15 +17,16 @@ type OwnProps = {
 //order in list reflects order in UI
 const INPUT_FILTER_LIST: string[] = [
   'frequencies__internal__upper_bound_kf__af',
-  'frequencies__gnomad_exomes_2_1__af',
+  'frequencies__gnomad_genomes_2_1__af',
   'frequencies__gnomad_genomes_3_0__af',
   'frequencies__gnomad_genomes_3_1_1__af',
-  'frequencies__gnomad_genomes_2_1__af',
+  'frequencies__gnomad_exomes_2_1__af',
   'frequencies__topmed__af',
   'frequencies__one_thousand_genomes__af',
 ];
 
 const FrequencyFilters: FunctionComponent<OwnProps> = ({ mappingResults }) => {
+  const [filtersOpen, setFiltersOpen] = useState(false);
   const { filters } = useFilters();
   const allSqons = getQueryBuilderCache(VARIANT_REPO_CACHE_KEY).state;
   const results = useGetFilterBuckets(
@@ -37,11 +38,18 @@ const FrequencyFilters: FunctionComponent<OwnProps> = ({ mappingResults }) => {
   );
 
   return (
-    <Spin size="large" spinning={results.loading}>
-      <Layout className={styles.variantFilterWrapper}>
-        {generateFilters(results, mappingResults, styles.variantFilterContainer)}
-      </Layout>
-    </Spin>
+    <>
+      <div className={styles.expandButtonContainerVariant}>
+        <Button onClick={() => setFiltersOpen(!filtersOpen)} type="link">
+          {filtersOpen ? 'Collapse all' : 'Expand all'}
+        </Button>
+      </div>
+      <Spin size="large" spinning={results.loading}>
+        <Layout className={styles.variantFilterWrapper}>
+          {generateFilters(results, mappingResults, styles.variantFilterContainer, filtersOpen)}
+        </Layout>
+      </Spin>
+    </>
   );
 };
 
