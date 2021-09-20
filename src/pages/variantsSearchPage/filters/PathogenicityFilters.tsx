544d73c2714c7e4ab44a9c8765e2ee8113cccf32
adipaul1981 <adipaul1981@yahoo.ca>
2021-09-20 16:00:28 -0400

    fix: #SKFP-152 fix order frequency filters add collopase_expant feature to filters default col (#3403)

--- src/pages/variantsSearchPage/filters/PathogenicityFilters.tsx
+++ src/pages/variantsSearchPage/filters/PathogenicityFilters.tsx
@@ -1,7 +1,7 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { getQueryBuilderCache, useFilters } from '@ferlab/ui/core/data/filters/utils';
 import { resolveSyntheticSqon } from '@ferlab/ui/core/data/sqon/utils';
-import { Layout, Spin } from 'antd';
+import { Button, Layout, Spin } from 'antd';
 
 import { generateFilters } from 'components/Utils/utils';
 import { VARIANT_INDEX, VARIANT_REPO_CACHE_KEY } from 'pages/variantsSearchPage/constants';
@@ -29,6 +29,7 @@ const INPUT_FILTER_LIST = [
 
 const PathogenicityFilters: FunctionComponent<OwnProps> = ({ mappingResults }) => {
   const { filters } = useFilters();
+  const [filtersOpen, setFiltersOpen] = useState(false);
   const allSqons = getQueryBuilderCache(VARIANT_REPO_CACHE_KEY).state;
   const results = useGetFilterBuckets(
     {
@@ -39,11 +40,18 @@ const PathogenicityFilters: FunctionComponent<OwnProps> = ({ mappingResults }) =
   );
 
   return (
-    <Spin size="large" spinning={results.loading}>
-      <Layout className={styles.variantFilterWrapper}>
-        {generateFilters(results, mappingResults, styles.variantFilterContainer)}
-      </Layout>
-    </Spin>
+    <>
+      <div className={styles.expandButtonContainerVariant}>
+        <Button onClick={() => setFiltersOpen(!filtersOpen)} type="link">
+          {filtersOpen ? 'Collapse all' : 'Expand all'}
+        </Button>
+      </div>
+      <Spin size="large" spinning={results.loading}>
+        <Layout className={styles.variantFilterWrapper}>
+          {generateFilters(results, mappingResults, styles.variantFilterContainer, filtersOpen)}
+        </Layout>
+      </Spin>
+    </>
   );
 };
 
