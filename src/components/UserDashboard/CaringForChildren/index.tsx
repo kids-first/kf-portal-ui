cba0483bbd0609ad9dfcf27769b50f818f5bcbef
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2021-10-20 11:56:00 -0400

    fix(hotfix): #SKFP-252 small fix

--- src/components/UserDashboard/CaringForChildren/index.tsx
+++ src/components/UserDashboard/CaringForChildren/index.tsx
@@ -7,14 +7,20 @@ const { Text, Title } = Typography;
 
 const CaringForChildren = () => (
   <Card
-    title={<span className={'title-dashboard-card'}>CARING for Children with COVID Resources</span>}
+    title={
+      <span className={'title-dashboard-card'}>
+        <span className={styles.italic}>CARING</span> for Children with COVID Resources
+      </span>
+    }
     className={styles.caringForChildrenCard}
   >
-    <Text>
+    <Text className={styles.cardDesc}>
       FHIR & Data Resources for NIH’s{' '}
-      <span className={styles.italic}>
-        Collaboration to Assess Risk and Identify LoNG-term outcomes for Children with COVID.
-      </span>
+      <a target="_blank" rel="noopener noreferrer" href="https://caring4kidswithcovid.nih.gov">
+        <span className={styles.italic}>
+          Collaboration to Assess Risk and Identify LoNG-term outcomes for Children with COVID.
+        </span>
+      </a>
     </Text>
     <div className={styles.linkCards}>
       <Row gutter={[8, 8]} align="stretch">
@@ -26,9 +32,12 @@ const CaringForChildren = () => (
             href="https://kf-api-fhir-service.kidsfirstdrc.org/Patient?_total=accurate&_has:ResearchSubject:individual:study=ResearchStudy/4873"
           >
             <Card className={styles.linkCard}>
-              <Title className={styles.linkCardTitle}>CARING Data FHIR API Endpoint</Title>
+              <Title className={styles.linkCardTitle}>
+                <span className={styles.italic}>CARING</span> Data FHIR API Endpoint
+              </Title>
               <Text className={styles.linkCardDesc}>
-                Query the entire CARING dataset via FHIR API parameters
+                Query the entire <span className={styles.italic}>CARING</span> dataset via FHIR API
+                parameters
               </Text>
             </Card>
           </a>
@@ -56,7 +65,8 @@ const CaringForChildren = () => (
             <Card className={styles.linkCard}>
               <Title className={styles.linkCardTitle}>Kids First FHIR Data Dashboard</Title>
               <Text className={styles.linkCardDesc}>
-                Explore the CARING data via dashboard interfaces
+                Explore the <span className={styles.italic}>CARING</span> data via dashboard
+                interfaces
               </Text>
             </Card>
           </a>
