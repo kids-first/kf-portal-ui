c85aee18e12dc24dff4c5d80bf19b98f2706c09a
lflangis <lflangis@ferlab.bio>
2022-09-14 10:47:28 -0400

    :lipstick: feat(dashboard): SKFP-377 add variants tab in saved filters

--- src/services/api/savedFilter/models.ts
+++ src/services/api/savedFilter/models.ts
@@ -7,6 +7,11 @@ export type TUserSavedFilter = ISavedFilter & {
   updated_date: string;
 };
 
+export enum SavedFilterTag {
+  PARTICIPANT = 'participant',
+  VARIANT = 'variants-exploration',
+}
+
 export type TUserSavedFilterInsert = Omit<
   TUserSavedFilter,
   'keycloak_id' | 'updated_date' | 'creation_date'
