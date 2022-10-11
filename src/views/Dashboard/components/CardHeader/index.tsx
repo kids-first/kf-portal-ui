51ce8a40bba6ef3d80c560eaac6089b07ccb6087
lflangis <lflangis@ferlab.bio>
2022-10-11 09:56:46 -0400

    🩹fix(dashboard): SKFP-381 adjust color theme

--- src/views/Dashboard/components/CardHeader/index.tsx
+++ src/views/Dashboard/components/CardHeader/index.tsx
@@ -1,12 +1,10 @@
-import React, { ReactNode } from "react";
-import { Popover, PopoverProps, Space, Typography } from "antd";
-import DragHandle from "@ferlab/ui/core/layout/SortableGrid/DragHandle";
-import { HolderOutlined, InfoCircleOutlined } from "@ant-design/icons";
-import cx from "classnames";
+import { ReactNode } from 'react';
+import { Popover, PopoverProps, Space, Typography } from 'antd';
+import DragHandle from '@ferlab/ui/core/layout/SortableGrid/DragHandle';
+import { HolderOutlined, InfoCircleOutlined } from '@ant-design/icons';
+import cx from 'classnames';
 
-import styles from "./index.module.scss";
-
-// TODO: Move this comoponent in ferlab with GridCard V2
+import styles from './index.module.scss';
 
 interface OwnProps {
   id: string;
@@ -18,29 +16,20 @@ interface OwnProps {
 
 const { Title } = Typography;
 
-const CardHeader = ({
-  id,
-  title,
-  extra = [],
-  withHandle = false,
-  infoPopover,
-}: OwnProps) => (
+const CardHeader = ({ id, title, extra = [], withHandle = false, infoPopover }: OwnProps) => (
   <Title level={4} className={styles.cardHeader}>
     <Space direction="horizontal" size={5} className={styles.title}>
       {withHandle && (
         <DragHandle id={id} className={styles.dragHandle}>
           <HolderOutlined />
         </DragHandle>
-      )}{" "}
+      )}{' '}
       {title}
       {infoPopover && (
         <Popover
           {...infoPopover}
           className={cx(styles.infoPopover, infoPopover.className)}
-          overlayClassName={cx(
-            styles.infoPopoverOverlay,
-            infoPopover.overlayClassName
-          )}
+          overlayClassName={cx(styles.infoPopoverOverlay, infoPopover.overlayClassName)}
         >
           <InfoCircleOutlined className={styles.infoIcon} />
         </Popover>
