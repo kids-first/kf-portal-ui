553917de0b24deaec277c399edc531f6819a7808
Luc-Frédéric Langis <65532894+lflangis@users.noreply.github.com>
2022-11-29 10:03:46 -0500

    🐛fix(profile): SKFP-550 fix 404 when viewing our profile in private mode (#3607)

---
+++ src/views/Profile/View/index.tsx
@@ -0,0 +1,25 @@
+import intl from 'react-intl-universal';
+
+import styles from './index.module.scss';
+import { usePersona } from 'store/persona';
+import CommunityProfile from 'components/uiKit/ComunityProfile';
+import { Result } from 'antd';
+
+const ProfileView = () => {
+  const { personaUserInfo, isLoading } = usePersona();
+
+  if (!isLoading && !personaUserInfo) {
+    return (
+      <Result
+        className={styles.notFoundMember}
+        status="404"
+        title="404"
+        subTitle={intl.get('screen.memberProfile.notFound')}
+      />
+    );
+  }
+
+  return <CommunityProfile profile={personaUserInfo} isOwner={true} loading={isLoading} />;
+};
+
+export default ProfileView;
