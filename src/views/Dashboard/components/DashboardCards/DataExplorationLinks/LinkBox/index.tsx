5db28a9865656a223dc36d3143f38adf9fd40538
Luc-Frédéric Langis <65532894+lflangis@users.noreply.github.com>
2023-02-27 14:22:52 -0500

    🐛fix(link): SKFP-646 remove link on family data (#3691)

--- src/views/Dashboard/components/DashboardCards/DataExplorationLinks/LinkBox/index.tsx
+++ src/views/Dashboard/components/DashboardCards/DataExplorationLinks/LinkBox/index.tsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
-import { Space } from 'antd';
+import { ArrowRightOutlined } from '@ant-design/icons';
 import MultiLabel, {
   MultiLabelIconPositionEnum,
 } from '@ferlab/ui/core/components/labels/MultiLabel';
-import { ArrowRightOutlined } from '@ant-design/icons';
+import { Space } from 'antd';
+import cx from 'classnames';
 
 import styles from './index.module.scss';
 
@@ -13,21 +14,43 @@ interface OwnProps {
   label: string | number;
   subLabel: string;
   href: string;
+  disabled?: boolean;
 }
 
-const LinkBox = ({ multiLabelClassName = '', label, subLabel, icon, href }: OwnProps) => (
-  <Link to={href} className={styles.dataExploBox}>
-    <Space direction="horizontal" size={16} align="start">
-      <MultiLabel
-        iconPosition={MultiLabelIconPositionEnum.Top}
-        label={label}
-        Icon={icon}
-        className={multiLabelClassName}
-        subLabel={subLabel}
-      />
-      <ArrowRightOutlined className={styles.linkArrow} />
-    </Space>
-  </Link>
-);
+const LinkBox = ({
+  multiLabelClassName = '',
+  label,
+  subLabel,
+  icon,
+  href,
+  disabled = false,
+}: OwnProps) =>
+  disabled ? (
+    <div className={cx(styles.dataExploBox, styles.disabled)}>
+      <Space direction="horizontal" size={16} align="start">
+        <MultiLabel
+          iconPosition={MultiLabelIconPositionEnum.Top}
+          label={label}
+          Icon={icon}
+          className={multiLabelClassName}
+          subLabel={subLabel}
+        />
+        <ArrowRightOutlined className={styles.linkArrow} />
+      </Space>
+    </div>
+  ) : (
+    <Link to={href} className={styles.dataExploBox}>
+      <Space direction="horizontal" size={16} align="start">
+        <MultiLabel
+          iconPosition={MultiLabelIconPositionEnum.Top}
+          label={label}
+          Icon={icon}
+          className={multiLabelClassName}
+          subLabel={subLabel}
+        />
+        <ArrowRightOutlined className={styles.linkArrow} />
+      </Space>
+    </Link>
+  );
 
 export default LinkBox;
