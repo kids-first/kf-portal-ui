1a6eefa1c45fee911544c3681bf94d67b845c7c3
lflangis <lflangis@ferlab.bio>
2023-04-21 08:36:50 -0400

    fix(grid): SKFP-664 adjust graph and grid disposition

--- src/views/DataExploration/components/PageContent/tabs/Summary/index.tsx
+++ src/views/DataExploration/components/PageContent/tabs/Summary/index.tsx
@@ -2,11 +2,14 @@ import { useDispatch } from 'react-redux';
 import ResizableGridLayout, {
   TSerializedResizableGridLayoutConfig,
 } from '@ferlab/ui/core/layout/ResizableGridLayout';
+import { Space } from 'antd';
 
 import { useUser } from 'store/user';
 import { updateUserConfig } from 'store/user/thunks';
 
 import { getDefaultLayouts } from './utils/grid';
+
+import styles from './index.module.scss';
 
 const SummaryTab = () => {
   const dispatch = useDispatch();
@@ -14,32 +17,34 @@ const SummaryTab = () => {
   const defaultLayouts = getDefaultLayouts();
 
   return (
-    <ResizableGridLayout
-      defaultLayouts={defaultLayouts}
-      layouts={userInfo?.config.data_exploration?.summary?.layouts}
-      onReset={(layouts: TSerializedResizableGridLayoutConfig[]) => {
-        dispatch(
-          updateUserConfig({
-            data_exploration: {
-              summary: {
-                layouts,
+    <Space className={styles.wrapper} direction="vertical">
+      <ResizableGridLayout
+        defaultLayouts={defaultLayouts}
+        layouts={userInfo?.config.data_exploration?.summary?.layouts}
+        onReset={(layouts: TSerializedResizableGridLayoutConfig[]) => {
+          dispatch(
+            updateUserConfig({
+              data_exploration: {
+                summary: {
+                  layouts,
+                },
               },
-            },
-          }),
-        );
-      }}
-      onConfigUpdate={(layouts: TSerializedResizableGridLayoutConfig[]) => {
-        dispatch(
-          updateUserConfig({
-            data_exploration: {
-              summary: {
-                layouts,
+            }),
+          );
+        }}
+        onConfigUpdate={(layouts: TSerializedResizableGridLayoutConfig[]) => {
+          dispatch(
+            updateUserConfig({
+              data_exploration: {
+                summary: {
+                  layouts,
+                },
               },
-            },
-          }),
-        );
-      }}
-    />
+            }),
+          );
+        }}
+      />
+    </Space>
   );
 };
 
