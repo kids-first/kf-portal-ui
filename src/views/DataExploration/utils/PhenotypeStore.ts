543742bc4fa93bed32bd59fde6654dcdcdad176d
evans-g-crsj <eg.crsj@gmail.com>
2023-09-27 20:06:52 -0400

    :bug: Fix sunburst

--- src/views/DataExploration/utils/PhenotypeStore.ts
+++ src/views/DataExploration/utils/PhenotypeStore.ts
@@ -4,9 +4,9 @@ import { IPhenotypeSource } from 'graphql/summary/models';
 import { ArrangerApi } from 'services/api/arranger';
 import OntologyTree, { lightTreeNodeConstructor, TreeNode, TTitleFormatter } from './OntologyTree';
 
-const ROOT_PHENO = 'All (HP:0000001)';
-
 export const RegexExtractPhenotype = new RegExp(/([A-Za-z].*?\((HP|MONDO):[0-9]+\))/, 'g');
+
+export const TARGET_ROOT_PHENO = 'Phenotypic abnormality (HP:0000118)';
 
 export const generateNavTreeFormKey = (phenotypes: string[]): TreeNode[] => {
   if (!phenotypes.length) {
@@ -47,7 +47,7 @@ export class PhenotypeStore {
     this.tree = undefined;
 
     return this.getPhenotypes(field, sqon, filterThemselves).then((data: IPhenotypeSource[]) => {
-      const ontologyTree = new OntologyTree(this.removeSingleRootNode(data), field, titleFormatter);
+      const ontologyTree = new OntologyTree(data, field, titleFormatter);
       this.phenotypes = ontologyTree.phenotypes;
       this.tree = ontologyTree.tree;
     });
@@ -74,16 +74,4 @@ export class PhenotypeStore {
 
     return data?.data || [];
   };
-
-  removeSingleRootNode = (phenotypes: IPhenotypeSource[]) =>
-    phenotypes
-      .map((p) => (p.key !== ROOT_PHENO ? p : null))
-      .filter((p): p is IPhenotypeSource => p !== null)
-      .map((p) => {
-        const index = p.top_hits.parents.indexOf(ROOT_PHENO);
-        if (!index) {
-          p.top_hits.parents.splice(index, 1);
-        }
-        return p;
-      });
 }
