78a3c7a22e801a5e13154b7f75b267ce0d1876bf
Francis Lavoie <flavoie@ferlab.bio>
2023-12-04 15:36:01 -0500

    fix: SKFP-000 fix jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,74 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-    '^react-native$': 'react-native-web',
-    '^.+\\.module\\.(css|sass|scss)$': '<rootDir>/.jest/identity-obj-proxy-esm.js',
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "collectCoverageFrom": [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts"
+  ],
+  "setupFiles": [
+    "react-app-polyfill/jsdom"
+  ],
+  "setupFilesAfterEnv": [],
+  "testMatch": [
+    "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
+    "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}"
+  ],
+  "testEnvironment": "jsdom",
+  "transform": {
+    "^.+\\.(js|jsx|mjs|cjs|ts|tsx)$": "<rootDir>/config/jest/babelTransform.js",
+    "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
+    "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)": "<rootDir>/config/jest/fileTransform.js"
   },
-  transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
+  "transformIgnorePatterns": [
+    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
+    "^.+\\.module\\.(css|sass|scss)$"
+  ],
+  "modulePaths": [
+    "/Users/francislavoie/workspace/kf/kf-portal-ui/src"
+  ],
+  "moduleNameMapper": {
+    "^react-native$": "react-native-web",
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+    "^uuid$": require.resolve('uuid'),
+    d3: '<rootDir>/node_modules/d3/dist/d3.min.js',
   },
-  collectCoverageFrom: ['./src/**/*.{js,jsx,ts,tsx}'],
-  coverageReporters: ['json', 'lcov', 'text-summary', 'json-summary'],
-  coverageThreshold: {
-    global: {
-      branches: 0,
-      functions: 5,
-      lines: 15,
-      statements: 15,
-    },
-  },
-};
+  "moduleFileExtensions": [
+    "web.js",
+    "js",
+    "web.ts",
+    "ts",
+    "web.tsx",
+    "tsx",
+    "json",
+    "web.jsx",
+    "jsx",
+    "node"
+  ],
+  "watchPlugins": [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname"
+  ],
+  "resetMocks": true
+}
+
+// {
+//   testEnvironment: 'jest-environment-jsdom',
+//   moduleNameMapper: {
+//     '^react-native$': 'react-native-web',
+//     '^.+\\.module\\.(css|sass|scss)$': '<rootDir>/.jest/identity-obj-proxy-esm.js',
+//   },
+//   transform: {
+//     '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
+//   },
+//   collectCoverageFrom: ['./src/**/*.{js,jsx,ts,tsx}'],
+//   coverageReporters: ['json', 'lcov', 'text-summary', 'json-summary'],
+//   coverageThreshold: {
+//     global: {
+//       branches: 0,
+//       functions: 5,
+//       lines: 15,
+//       statements: 15,
+//     },
+//   },
+// };
