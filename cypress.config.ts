8e0d3599ae9867b636a139964e05dee43c723868
Karine St-Onge <kstonge@ferlab.bio>
2024-01-12 13:49:45 -0500

    feat: SKFP-746 Typo

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,7 @@ import { getDateTime } from './cypress/support/utils';
 const { strDate, strTime } = getDateTime();
 
 const getName = (url = '', parallel = '') => {
-    if (url.includes('clin-')) {
+    if (url.includes('skfp-')) {
       return url.replace('https://', '').split('.')[0].split('-').splice(2, 4).join('-')+'/'+parallel;
     } else {
       return 'QA/'+parallel;
