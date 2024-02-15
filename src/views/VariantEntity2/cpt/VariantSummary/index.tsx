6435b90975508096cca07ff3bb9070b22f497328
GaelleA <galtefrohne@ferlab.bio>
2024-02-15 14:04:01 -0500

    feat(variant2): SKFP-900 skeleton fixes

--- src/views/VariantEntity2/cpt/VariantSummary/index.tsx
+++ src/views/VariantEntity2/cpt/VariantSummary/index.tsx
@@ -2,104 +2,60 @@ import { Button, Card, Col, Row, Space } from 'antd';
 
 import style from './index.module.scss';
 import GeneBanner from '../VariantBanner/GeneBanner';
+import { toNodes } from 'graphql/utils/helpers';
 
 const VariantSummary = () => {
   return (
     <Card className={style.card}>
-      {/* <div></div>
-      <div></div>
-      <div></div> */}
-
       <Space className={style.space} direction="vertical" size={'middle'}>
-        <Row justify="space-between" wrap>
-          <Col span={4}>col-4test1</Col>
-          <Col span={4}>col-4test2</Col>
-          <Col span={4}>col-4test3</Col>
-          <Col span={4}>col-4test4</Col>
-          <Col span={4}>col-4test5</Col>
-        </Row>
-
         <div className={style.bannerWrapper}>
-          <Row justify="space-between" wrap>
-            <Col xs={2} sm={2} md={4} lg={4} xl={4} xxl={4}>
-              <div>title</div>
-              <div>value value value</div>
-            </Col>
-            <Col xs={2} sm={2} md={4} lg={4} xl={4} xxl={4}>
-              <div>title</div>
-              <div>value value value</div>
-            </Col>
-            <Col xs={2} sm={2} md={4} lg={4} xl={4} xxl={4}>
-              <div>title</div>
-              <div>value value value</div>
-            </Col>
-            <Col xs={2} sm={2} md={4} lg={4} xl={4} xxl={4}>
-              <div>title</div>
-              <div>value value value</div>
-            </Col>
-            <Col xs={2} sm={2} md={4} lg={4} xl={4} xxl={4}>
-              <div>title</div>
-              <div>value value value</div>
-            </Col>
-          </Row>
+          <div>
+            <div>title</div>
+            <div>value value value</div>
+          </div>
+          <div>
+            <div>title</div>
+            <div>value value value</div>
+          </div>
+          <div>
+            <div>title</div>
+            <div>value value value</div>
+          </div>
+          <div>
+            <div>title</div>
+            <div>value value value</div>
+          </div>
+          <div>
+            <div>title</div>
+            <div>value value value</div>
+          </div>
         </div>
 
-        <div className={style.bannerWrapper}>
-          <Row justify="space-between" wrap>
-            <Col span={4}>
-              <div>title</div>
-              <div>value value value</div>
-            </Col>
-            <Col span={4}>
-              <div>title</div>
-              <div>value value value</div>
-            </Col>
-            <Col span={4}>
-              <div>title</div>
-              <div>value value value</div>
-            </Col>
-            <Col span={4}>
-              <div>title</div>
-              <div>value value value</div>
-            </Col>
-            <Col span={4}>
-              <div>title</div>
-              <div>value value value</div>
-            </Col>
-          </Row>
+        <div className={style.infoWrapper}>
+          <div>
+            <span>title title title title</span>
+          </div>
+          <div>
+            <span>title</span>
+          </div>
+          <div>
+            <span>title</span>
+          </div>
+          <div>
+            <span>title</span>
+          </div>
+          <div>
+            <span>title</span>
+          </div>
         </div>
 
-        <div className={style.spaceAlignContainer}>
-          <div className={style.spaceAlignBlock}>
-            <Space direction="vertical">
-              <span>title</span>
-              <span>value value value</span>
-            </Space>
+        <div className={style.detailsWrapper}>
+          <div className={style.score}>functional score</div>
+          <div className={style.geneSplice}>
+            <div>gene constraints</div>
+            <div>splice altering</div>
           </div>
-          <div className={style.spaceAlignBlock}>
-            <Space direction="vertical">
-              <span>title</span>
-              <span>value value value</span>
-            </Space>
-          </div>
-          <div className={style.spaceAlignBlock}>
-            <Space direction="vertical">
-              <span>title</span>
-              <span>value value value</span>
-            </Space>
-          </div>
-          <div className={style.spaceAlignBlock}>
-            <Space direction="vertical">
-              <span>title</span>
-              <span>value value value</span>
-            </Space>
-          </div>
-          <div className={style.spaceAlignBlock}>
-            <Space direction="vertical">
-              <span>title</span>
-              <span>value value value</span>
-            </Space>
-          </div>
+          <div className={style.omim}>OMIM</div>
         </div>
       </Space>
     </Card>
