f4094ab6d6cb83ef0aaf882c715c722d25646b92
Alek Perron <alek.perron.hsj@ssss.gouv.qc.ca>
2024-02-27 10:30:33 -0500

    fix: SKFP-904 remove inheritance column and add tag

--- src/views/VariantEntity2/utils/pathogenicity.tsx
+++ src/views/VariantEntity2/utils/pathogenicity.tsx
@@ -1,9 +1,7 @@
 import intl from 'react-intl-universal';
-import { TABLE_EMPTY_PLACE_HOLDER } from '@ferlab/ui/core/common/constants';
 import ColorTag, { ColorTagType } from '@ferlab/ui/core/components/ColorTag';
 import ExternalLink from '@ferlab/ui/core/components/ExternalLink';
 import { ProColumnType } from '@ferlab/ui/core/components/ProTable/types';
-import StackLayout from '@ferlab/ui/core/layout/StackLayout';
 import {
   CosmicConditionCell,
   DddConditionCell,
@@ -18,7 +16,6 @@ import {
   DddConditions,
   HpoConditions,
   IGeneRecord,
-  Inheritance,
   OmimConditions,
   OmimGene,
   OmimInheritance,
@@ -115,59 +112,51 @@ export const getGenePhenotypeColumns = (): ProColumnType[] => [
   {
     key: 'conditions',
     dataIndex: 'conditions',
-    render: (text: Conditions, record: IGeneRecord) => {
+    render: (_text: Conditions, record: IGeneRecord) => {
       switch (record.source) {
         case ClinicalGenesTableSource.omim:
-          return <OmimConditionCell conditions={record.conditions as OmimConditions} />;
+          return (
+            <OmimConditionCell
+              conditions={record.conditions as OmimConditions}
+              withInheritanceTags
+              inheritances={record.inheritance as OmimInheritance}
+            />
+          );
         case ClinicalGenesTableSource.orphanet:
-          return <OrphanetConditionCell conditions={record.conditions as OrphanetConditions} />;
+          return (
+            <OrphanetConditionCell
+              conditions={record.conditions as OrphanetConditions}
+              withInheritanceTags
+              inheritances={record.inheritance as OrphanetInheritance}
+            />
+          );
         case ClinicalGenesTableSource.hpo:
-          return <HpoConditionCell conditions={record.conditions as HpoConditions} />;
+          return (
+            <HpoConditionCell
+              conditions={record.conditions as HpoConditions}
+              withInheritanceTags
+              inheritances={record.inheritance as SingleValuedInheritance}
+            />
+          );
         case ClinicalGenesTableSource.ddd:
-          return <DddConditionCell conditions={record.conditions as DddConditions} />;
+          return (
+            <DddConditionCell
+              conditions={record.conditions as DddConditions}
+              withInheritanceTags
+              inheritances={record.inheritance as SingleValuedInheritance}
+            />
+          );
         default:
-          return <CosmicConditionCell conditions={record.conditions as CosmicConditions} />;
+          return (
+            <CosmicConditionCell
+              conditions={record.conditions as CosmicConditions}
+              withInheritanceTags
+              inheritances={record.inheritance as SingleValuedInheritance}
+            />
+          );
       }
     },
     title: intl.get('screen.variants.pathogenicity.condition'),
-    width: '33%',
-  },
-  {
-    key: 'inheritance',
-    dataIndex: 'inheritance',
-    render: (text: Inheritance, record: IGeneRecord) => {
-      const { source } = record;
-      if (source === ClinicalGenesTableSource.orphanet) {
-        const orphanetInheritance = (record.inheritance || []) as OrphanetInheritance;
-        return (
-          <>
-            {orphanetInheritance.map((inheritance: string[], index: number) => (
-              <StackLayout key={index}>
-                <Typography.Text>
-                  {inheritance ? inheritance.join(', ') : TABLE_EMPTY_PLACE_HOLDER}
-                </Typography.Text>
-              </StackLayout>
-            ))}
-          </>
-        );
-      } else if (source === ClinicalGenesTableSource.omim) {
-        const omimInheritance = record.inheritance as OmimInheritance;
-        return (
-          <>
-            {omimInheritance.map((inheritance: string[], index: number) => (
-              <StackLayout key={index}>
-                <Typography.Text>
-                  {inheritance ? inheritance.join(', ') : TABLE_EMPTY_PLACE_HOLDER}
-                </Typography.Text>
-              </StackLayout>
-            ))}
-          </>
-        );
-      }
-      const inheritance = record.inheritance as SingleValuedInheritance;
-      return inheritance || TABLE_EMPTY_PLACE_HOLDER;
-    },
-    title: intl.get('screen.variants.pathogenicity.inheritance'),
-    width: '33%',
+    width: '60%',
   },
 ];
