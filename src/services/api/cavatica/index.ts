58b3374c0c43cdc3a7bc971bdfb8d17c76ce91aa
Alek Perron <alek.perron.hsj@ssss.gouv.qc.ca>
2024-02-28 15:20:37 -0500

    fix: SKFP-888 cavatica import

--- src/services/api/cavatica/index.ts
+++ src/services/api/cavatica/index.ts
@@ -1,4 +1,4 @@
-// import EnvironmentVariables from 'helpers/EnvVariables';
+import EnvironmentVariables from 'helpers/EnvVariables';
 
 import { sendRequest } from 'services/api';
 
@@ -13,10 +13,9 @@ import {
   ICavaticaProjectNode,
 } from './models';
 
-// const KEY_MANAGER_API_URL = EnvironmentVariables.configFor('KEY_MANAGER_API_URL');
-// const CAVATICA_FENCE_PROXY = EnvironmentVariables.configFor('CAVATICA_FENCE_PROXY');
-// const CAVATICA_PROXY_URL = `${KEY_MANAGER_API_URL}/${CAVATICA_FENCE_PROXY}/v2`;
-const CAVATICA_PROXY_URL = `http://localhost:5000/mock/cavatica2/v2`;
+const KEY_MANAGER_API_URL = EnvironmentVariables.configFor('KEY_MANAGER_API_URL');
+const CAVATICA_FENCE_PROXY = EnvironmentVariables.configFor('CAVATICA_FENCE_PROXY');
+const CAVATICA_PROXY_URL = `${KEY_MANAGER_API_URL}/${CAVATICA_FENCE_PROXY}/v2`;
 
 const headers = () => ({
   'Content-Type': 'application/json',
