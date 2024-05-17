73a66c200a038f55b3c5659e73cd794b4614eece
Alek Perron <alek.perron.hsj@ssss.gouv.qc.ca>
2024-05-17 14:29:27 -0400

    fix: SKFP-1059 add acls to file entity request

--- src/utils/dataFiles.ts
+++ src/utils/dataFiles.ts
@@ -7,38 +7,17 @@ export const userHasAccessToFile = (
   isConnectedToCavatica: boolean = false,
   isConnectedToGen3: boolean = false,
 ) => {
-  console.log('File: ', file);
-  console.log('Is Connected To GEN3: ', isConnectedToGen3);
-  console.log('Is Connected To Cavatica: ', isConnectedToCavatica);
-
   if (!isConnectedToGen3 && !isConnectedToCavatica) {
     return false;
   }
-
-  console.log('Is Controlled Access: ', file.controlled_access === FileAccessType.CONTROLLED);
 
   if (file.controlled_access === FileAccessType.CONTROLLED && !isConnectedToGen3) {
     return false;
   }
 
-  console.log('Is Registered Access: ', file.controlled_access === FileAccessType.REGISTERED);
-
   if (file.controlled_access === FileAccessType.REGISTERED && !isConnectedToCavatica) {
     return false;
   }
-
-  console.log('File ACLs: ', file.acl);
-  console.log('User ACLs: ', userAcls);
-
-  console.log('Intersection of ACLs: ', intersection(userAcls, file.acl));
-
-  console.log(
-    'Has access: ',
-    !file.acl ||
-      file.acl.length === 0 ||
-      intersection(userAcls, file.acl).length > 0 ||
-      file.controlled_access === FileAccessType.REGISTERED,
-  );
 
   return (
     !file.acl ||
