246a598a9076cfe1fd07991e70477dd948fb2c12
Alek Perron <156684667+aperron-ferlab@users.noreply.github.com>
2024-05-27 14:32:58 -0400

    feat: SKFP-1087 update study mappings for pedc bioportal (#4010)

--- src/views/Studies/utils/helper.ts
+++ src/views/Studies/utils/helper.ts
@@ -1,9 +1,10 @@
 export const StudyCodeMap: { [key: string]: string } = {
-  'KF-NBL': 'x01_fy16_nbl_maris',
-  'KF-CHDALL': 'aml_sd_z6mwd3h0_2018',
   'KF-MMC': 'aml_sd_pet7q6f2_2018',
+  'KF-CHDALL': 'bllnos_sd_z6mwd3h0_2018',
   'KF-OS': 'os_sd_zxjffmef_2015',
+  'KF-TALL': 'tll_sd_aq9kvn5p_2019',
   CBTN: 'openpedcan_v15',
+  'KF-NBL': 'openpedcan_v15',
 };
 
 export const mapStudyToPedcBioportal = (studyCode: string) => StudyCodeMap[studyCode] || '';
