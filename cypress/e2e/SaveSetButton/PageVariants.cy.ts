3fda8b551bbaa761bb6cdc462b4c701596e32d41
Karine St-Onge <kstonge@ferlab.bio>
2024-06-06 15:33:40 -0400

    feat: SKFP-1125 Add SaveSetButton Cypress tests

---
+++ cypress/e2e/SaveSetButton/PageVariants.cy.ts
@@ -0,0 +1,83 @@
+/// <reference types="Cypress" />
+import '../../support/commands';
+
+describe('Page Variants - Bouton Save set', () => {
+
+  beforeEach(() => {
+    cy.login();
+    cy.deleteSetIfExists('variants', 'Cypress_New_V');
+    cy.deleteSetIfExists('variants', 'Cypress_V');
+    cy.get('[class*="SavedSets_setTabs"] [data-node-key="variants"]').click({force: true}); // data-cy="Tab_Biospecimens"
+    cy.get('[class*="SavedSets_setTabs"] [class*="ant-tabs-tabpane-active"]').invoke('text').then((invokeText) => {
+      cy.visitVariantsPage();
+      if (!invokeText.includes('Cypress_V')) {
+        cy.saveSetAs('Cypress_V', 0);
+      };
+    });
+  });
+
+  it('Vérifier les informations affichées - Titre de la dropdown', () => {
+    cy.get('[class="ant-table-body"] [class*="ant-table-row"]').eq(0).find('[type="checkbox"]').check({force: true});
+    cy.get('[id="variants-set-dropdown-container"] button').click({force: true});
+
+    cy.get('[class="ant-dropdown-menu-title-content"]').contains('1 variant selected').should('exist');
+  });
+
+  it('Vérifier les informations affichées - Tooltip de la dropdown', () => {
+    cy.get('[class="ant-table-body"] [class*="ant-table-row"]').eq(0).find('[type="checkbox"]').check({force: true});
+    cy.get('[id="variants-set-dropdown-container"] button').click({force: true});
+    cy.get('[class="ant-dropdown-menu-title-content"] [data-icon="info-circle"]').trigger('mouseover', {eventConstructor: 'MouseEvent', force: true});
+
+    cy.get('[class="ant-tooltip-inner"]').should('not.have.class', 'ant-tooltip-hidden');
+    cy.get('[class="ant-tooltip-inner"]').contains('Max. 10,000 items at a time. The first 10,000 will be processed.').should('exist');
+  });
+  
+  it('Valider les fonctionnalités du bouton - Save as new set', () => {
+    cy.saveSetAs('Cypress_New_V', 0);
+
+    cy.get('[class*="ant-notification"]').contains('Your set has been saved.').should('exist');
+    cy.get('[class*="ant-notification"]').contains('You can add your sets to a query from the dashboard.').should('exist');
+    
+    cy.visitDashboard();
+    cy.get('[class*="SavedSets_setTabs"] [data-node-key="variants"]').click({force: true});
+    cy.get('[class*="SavedSets_setTabs"] [class*="ant-tabs-tabpane-active"]').contains('Cypress_New_V').parentsUntil('[class*="ListItem_savedSetListItem"]').parent().find('[class*="ListItem_count"]').contains(/^1$/).click({force: true});
+  });
+
+  it('Valider les fonctionnalités du bouton - Add to existing set', () => {
+    cy.get('[class="ant-table-body"] [class*="ant-table-row"]').eq(1).find('[type="checkbox"]').check({force: true});
+    cy.get('[id*="-set-dropdown-container"] button').click({force: true});
+    cy.get('[data-menu-id*="add_ids"]').click({force: true});
+
+    cy.get('[class*="ant-select-in-form-item"] input').focus().type('{enter}', {force: true});
+    cy.get('[class*="ant-select-dropdown"]').contains('Cypress_V').parentsUntil('[class="ant-select-item-option-content"]').contains(/^1$/).should('exist');
+    cy.get('[class*="ant-select-dropdown"]').contains('Cypress_V').click({force: true});
+    cy.clickAndIntercept('[class="ant-modal-content"] button[class*="ant-btn-primary"]', 'PUT', '**/sets/**', 1);
+    cy.get('form[id="add-remove-set"]').should('not.exist');
+
+    cy.get('[class*="ant-notification"]').contains('Success').should('exist');
+    cy.get('[class*="ant-notification"]').contains('Your set has been updated.').should('exist');
+
+    cy.visitDashboard();
+    cy.get('[class*="SavedSets_setTabs"] [data-node-key="variants"]').click({force: true});
+    cy.get('[class*="SavedSets_setTabs"] [class*="ant-tabs-tabpane-active"]').contains('Cypress_V').parentsUntil('[class*="ListItem_savedSetListItem"]').parent().find('[class*="ListItem_count"]').contains(/^2$/).click({force: true});
+    });
+    
+  it('Valider les fonctionnalités du bouton - Remove from existing set', () => {
+    cy.get('[class="ant-table-body"] [class*="ant-table-row"]').eq(0).find('[type="checkbox"]').check({force: true});
+    cy.get('[id*="-set-dropdown-container"] button').click({force: true});
+    cy.get('[data-menu-id*="remove_ids"]').click({force: true});
+
+    cy.get('[class*="ant-select-in-form-item"] input').focus().type('{enter}', {force: true});
+    cy.get('[class*="ant-select-dropdown"]').contains('Cypress_V').parentsUntil('[class="ant-select-item-option-content"]').contains(/^1$/).should('exist');
+    cy.get('[class*="ant-select-dropdown"]').contains('Cypress_V').click({force: true});
+    cy.clickAndIntercept('[class="ant-modal-content"] button[class*="ant-btn-primary"]', 'PUT', '**/sets/**', 1);
+    cy.get('form[id="add-remove-set"]').should('not.exist');
+
+    cy.get('[class*="ant-notification"]').contains('Success').should('exist');
+    cy.get('[class*="ant-notification"]').contains('Your set has been updated.').should('exist');
+
+    cy.visitDashboard();
+    cy.get('[class*="SavedSets_setTabs"] [data-node-key="variants"]').click({force: true});
+    cy.get('[class*="SavedSets_setTabs"] [class*="ant-tabs-tabpane-active"]').contains('Cypress_V').parentsUntil('[class*="ListItem_savedSetListItem"]').parent().find('[class*="ListItem_count"]').contains(/^0$/).click({force: true});
+  });
+});
