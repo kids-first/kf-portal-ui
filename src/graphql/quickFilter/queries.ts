9a6d576bd5e4ea034daacc04c347ccd540cf4875
GaelleA <galtefrohne@ferlab.bio>
2024-06-13 09:52:19 -0400

    fix(qf): SKFP-1149 apply btn style and clean

--- src/graphql/quickFilter/queries.ts
+++ src/graphql/quickFilter/queries.ts
@@ -1,55 +1,5 @@
 import { gql } from '@apollo/client';
 
-export const dataExploQFFacets = [
-  'study__study_name',
-  'study__study_code',
-  'study__program',
-  'study__domain',
-  'study__external_id',
-  'is_proband',
-  'ethnicity',
-  'sex',
-  'race',
-  'diagnosis__age_at_event_days',
-  'outcomes__age_at_event_days__value',
-  'phenotype__age_at_event_days',
-  'diagnosis__ncit_id_diagnosis',
-  'diagnosis__source_text',
-  'family_type',
-  // 'observed_phenotype__name',
-  'phenotype__hpo_phenotype_not_observed',
-  'phenotype__source_text',
-  'outcomes__vital_status',
-  'files__biospecimens__sample_type',
-  'files__biospecimens__collection_sample_type',
-  'files__biospecimens__age_at_biospecimen_collection',
-  'files__biospecimens__diagnoses__age_at_event__value',
-  'files__biospecimens__status',
-  'files__biospecimens__collection_ncit_anatomy_site_id',
-  'files__biospecimens__collection_anatomy_site',
-  'files__biospecimens__consent_type',
-  'files__biospecimens__dbgap_consent_code',
-  'files__biospecimens__diagnoses__mondo_display_term',
-  // 'files__biospecimens__diagnoses__diagnosis_mondo',
-  'files__biospecimens__diagnoses__diagnosis_ncit',
-  'files__biospecimens__diagnoses__source_text',
-  'files__biospecimens__diagnoses__source_text_tumor_location',
-  'files__biospecimens__collection_method_of_sample_procurement',
-  'files__biospecimens__diagnoses__source_text_tumor_descriptor',
-  'files__controlled_access',
-  'files__data_category',
-  'files__data_type',
-  'files__sequencing_experiment__experiment_strategy',
-  'files__file_format',
-  'files__sequencing_experiment__platform',
-  'files__sequencing_experiment__instrument_model',
-  'files__sequencing_experiment__library_strand',
-  'files__sequencing_experiment__is_paired_end',
-  'files__repository',
-  'files__acl',
-];
-
-// TODO add method to generate this query with facets array dataExploQFFacets
 export const GET_QUICK_FILTER_EXPLO = gql`
   query getQuickFilterExploFacets($sqon: JSON) {
     participant {
@@ -146,13 +96,6 @@ export const GET_QUICK_FILTER_EXPLO = gql`
             doc_count
           }
         }
-        # Browser
-        # observed_phenotype__name {
-        #   buckets {
-        #     key
-        #     doc_count
-        #   }
-        # }
         phenotype__hpo_phenotype_not_observed {
           buckets {
             key
@@ -171,7 +114,6 @@ export const GET_QUICK_FILTER_EXPLO = gql`
             doc_count
           }
         }
-
         files__biospecimens__sample_type {
           buckets {
             key
@@ -232,13 +174,6 @@ export const GET_QUICK_FILTER_EXPLO = gql`
             doc_count
           }
         }
-        # Browser
-        # files__biospecimens__diagnoses__diagnosis_mondo {
-        #   buckets {
-        #     key
-        #     doc_count
-        #   }
-        # }
         files__biospecimens__diagnoses__diagnosis_ncit {
           buckets {
             key
@@ -269,7 +204,6 @@ export const GET_QUICK_FILTER_EXPLO = gql`
             doc_count
           }
         }
-
         files__controlled_access {
           buckets {
             key
