b4d6e41e84c554ce001be37dbb48d985047d3cdc
Luc-Frédéric Langis <65532894+lflangis@users.noreply.github.com>
2024-06-20 13:49:15 -0400

    feat(pedcbioportal): SKFP-1154 add new pedcbioportal rules (#4049)

--- src/views/Studies/utils/helper.ts
+++ src/views/Studies/utils/helper.ts
@@ -1,3 +1,12 @@
+import { IFileEntity } from '../../../graphql/files/models';
+
+const VALID_DATA_TYPES: string[] = [
+  'Somatic Simple Nucleotide Variations',
+  'Raw Gene Fusions',
+  'Gene Expression Quantification',
+  'Somatic Copy Number Variations',
+];
+
 export const StudyCodeMap: { [key: string]: string } = {
   'KF-MMC': 'aml_sd_pet7q6f2_2018',
   'KF-CHDALL': 'bllnos_sd_z6mwd3h0_2018',
@@ -8,3 +17,6 @@ export const StudyCodeMap: { [key: string]: string } = {
 };
 
 export const mapStudyToPedcBioportal = (studyCode: string) => StudyCodeMap[studyCode] || '';
+
+export const areFilesDataTypeValid = (files: IFileEntity[]): boolean =>
+  files.some((file) => VALID_DATA_TYPES.includes(file.data_type));
