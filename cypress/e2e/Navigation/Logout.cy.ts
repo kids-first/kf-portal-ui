b22092905e0946f3346294a8bfd25f0c45c1c5fc
Karine St-Onge <kstonge@ferlab.bio>
2024-09-03 07:59:58 -0400

    feat: SKFP-1265 Adjust Cypress tests related to SKFP-1208

--- cypress/e2e/Navigation/Logout.cy.ts
+++ cypress/e2e/Navigation/Logout.cy.ts
@@ -8,25 +8,20 @@ describe('Page Logout', () => {
     cy.visit('/');
   });
 
-  it('Vérifier les informations affichées', () => {
+  it('Vérifier les informations affichées [SKFP-1208]', () => {
     cy.logout();
 
-    cy.contains('Available Data').should('exist');
-    cy.get('[class*="DataRelease_dataReleaseStatsLabel"]').eq(0).contains(/\d{2}/).should('exist'); // data-cy="DataRelease_Study"
-    cy.get('[class*="DataRelease_dataReleaseStatsLabel"]').eq(0).contains('Studies').should('exist'); // data-cy="DataRelease_Study"
-    cy.get('[class*="DataRelease_dataReleaseStatsLabel"]').eq(1).contains(/(\d{2}\.\d{1}|\d{3})K/).should('exist'); // data-cy="DataRelease_Participant"
-    cy.get('[class*="DataRelease_dataReleaseStatsLabel"]').eq(1).contains('Participants').should('exist'); // data-cy="DataRelease_Participant"
-    cy.get('[class*="DataRelease_dataReleaseStatsLabel"]').eq(2).contains(/(\d{2}\.\d{1}|\d{2,3})K/).should('exist'); // data-cy="DataRelease_Family"
-    cy.get('[class*="DataRelease_dataReleaseStatsLabel"]').eq(2).contains('Families').should('exist'); // data-cy="DataRelease_Family"
-    cy.get('[class*="DataRelease_dataReleaseStatsLabel"]').eq(3).contains(/(\d{2}\.\d{1}|\d{3})K/).should('exist'); // data-cy="DataRelease_Biospecimen"
-    cy.get('[class*="DataRelease_dataReleaseStatsLabel"]').eq(3).contains('Biospecimens').should('exist'); // data-cy="DataRelease_Biospecimen"
-    cy.get('[class*="DataRelease_dataReleaseStatsLabel"]').eq(4).contains(/(\d{2}\.\d{1}|\d{3})K/).should('exist'); // data-cy="DataRelease_File"
-    cy.get('[class*="DataRelease_dataReleaseStatsLabel"]').eq(4).contains('Data Files').should('exist'); // data-cy="DataRelease_File"
-    cy.contains('Kids First Data Resource Portal').should('exist');
-
-    cy.contains('Accelerating research and promoting new discoveries for children affected with cancer and structural birth defects.').should('exist');
-    cy.contains(/Data from over \d{2,3}\,\d{3} samples\, including whole genome sequencing \(WGS\) and RNA\-Sequencing\, is available to empower your research today\./).should('exist');
-    cy.get('[class*="Login"] button[class*="primary"]').contains('Login').should('exist'); // data-cy="Login"
-    cy.get('[class*="Login"] button[class*="default"]').contains('Sign up').should('exist'); // data-cy="Signup"
+    cy.get('[class*="Grid_statsGrid"] [class*="Grid_stats"] [class*="Grid_data"]').eq(0).contains(/\d{2}/).should('exist');
+    cy.get('[class*="Grid_statsGrid"] [class*="Grid_stats"] [class*="Grid_description"]').eq(0).contains('Studies').should('exist');
+    cy.get('[class*="Grid_statsGrid"] [class*="Grid_stats"] [class*="Grid_data"]').eq(1).contains(/(\d{2}\.\d{1}|\d{3})K/).should('exist');
+    cy.get('[class*="Grid_statsGrid"] [class*="Grid_stats"] [class*="Grid_description"]').eq(1).contains('Participants').should('exist');
+    cy.get('[class*="Grid_statsGrid"] [class*="Grid_stats"] [class*="Grid_data"]').eq(2).contains(/\d{2}M+/).should('exist');
+    cy.get('[class*="Grid_statsGrid"] [class*="Grid_stats"] [class*="Grid_description"]').eq(2).contains('Variants').should('exist');
+    cy.get('[class*="Grid_statsGrid"] [class*="Grid_stats"] [class*="Grid_data"]').eq(3).contains(/(\d{2}\.\d{1}|\d{3})K/).should('exist');
+    cy.get('[class*="Grid_statsGrid"] [class*="Grid_stats"] [class*="Grid_description"]').eq(3).contains('Biospecimens').should('exist');
+    cy.get('[class*="Grid_statsGrid"] [class*="Grid_stats"] [class*="Grid_data"]').eq(4).contains(/\d{1}\.\d{2}PB/).should('exist');
+    cy.get('[class*="Grid_statsGrid"] [class*="Grid_stats"] [class*="Grid_description"]').eq(4).contains('Files').should('exist');
+    cy.get('[class*="Grid_statsGrid"] [class*="Grid_stats"] [class*="Grid_data"]').eq(5).contains(/\d{2}\.\d{1}K/).should('exist');
+    cy.get('[class*="Grid_statsGrid"] [class*="Grid_stats"] [class*="Grid_description"]').eq(5).contains('Genomes').should('exist');
   });
 });
