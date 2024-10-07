06c75f2365b9d7586ccbac455f01e47c44102bcb
AltefrohneGaelle <133775440+AltefrohneGaelle@users.noreply.github.com>
2024-10-07 14:12:14 -0400

    feat(biospecimen): SKFP-1262 add columns and facets (#4141)

--- src/graphql/biospecimens/useBiospecimenResolvedSqon.tsx
+++ src/graphql/biospecimens/useBiospecimenResolvedSqon.tsx
@@ -1,6 +1,7 @@
-import { resolveSyntheticSqon } from '@ferlab/ui/core/data/sqon/utils';
-import { mapFilterForBiospecimen } from 'utils/fieldMapper';
 import useQueryBuilderState from '@ferlab/ui/core/components/QueryBuilder/utils/useQueryBuilderState';
+import { resolveSyntheticSqon } from '@ferlab/ui/core/data/sqon/utils';
+
+import { mapFilterForBiospecimen } from 'utils/fieldMapper';
 
 const useBiospecimenResolvedSqon = (queryBuilderId: string) => {
   const { queryList, activeQuery } = useQueryBuilderState(queryBuilderId);
