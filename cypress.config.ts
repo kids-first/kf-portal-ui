6fdedd350e38470526f1a786ae947f85910ad9a2
Karine St-Onge <kstonge@ferlab.bio>
2025-01-09 16:33:25 -0500

    feat: SKFP-1418 Setup Ferlease Cypress execution

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,8 +5,8 @@ import { getDateTime } from './cypress/support/utils';
 const { strDate, strTime } = getDateTime();
 
 const getName = (url = '', parallel = '') => {
-    if (url.includes('skfp-')) {
-      return url.replace('https://', '').split('.')[0].split('-').splice(2, 4).join('-')+'/'+parallel;
+    if (url.includes('netlify')) {
+      return url.replace('https://', '').split('.')[0].split('-')[2]+'/'+parallel;
     } else {
       return 'QA/'+parallel;
     }
