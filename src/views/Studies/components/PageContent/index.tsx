8714de4fe24c74e896565a0b82973377cd451fa4
lflangis <lflangis@ferlab.bio>
2025-03-31 14:28:01 -0400

    feat(studies): SKFP-1492 Add summary

--- src/views/Studies/components/PageContent/index.tsx
+++ src/views/Studies/components/PageContent/index.tsx
@@ -3,7 +3,13 @@ import intl from 'react-intl-universal';
 import { useDispatch } from 'react-redux';
 import ProLabel from '@ferlab/ui/core/components/ProLabel';
 import ProTable from '@ferlab/ui/core/components/ProTable';
-import { ProColumnType } from '@ferlab/ui/core/components/ProTable/types';
+import SummarySumCell from '@ferlab/ui/core/components/ProTable/SummarySumCell';
+import {
+  ProColumnType,
+  TColumnState,
+  TColumnStates,
+  TProTableSummary,
+} from '@ferlab/ui/core/components/ProTable/types';
 import { tieBreaker } from '@ferlab/ui/core/components/ProTable/utils';
 import useQueryBuilderState, {
   defaultQueryBuilderState,
@@ -18,6 +24,7 @@ import GridCard from '@ferlab/ui/core/view/v2/GridCard';
 import { Input, Space, Typography } from 'antd';
 import { INDEXES } from 'graphql/constants';
 import { useStudies } from 'graphql/studies/actions';
+import { IStudyEntity } from 'graphql/studies/models';
 import { cloneDeep } from 'lodash';
 
 import { fetchTsvReport } from 'store/report/thunks';
@@ -65,6 +72,55 @@ const generateMultipleQuery = (searchValue: string, activeQuery: ISyntheticSqon)
   const newQuery: any = activeQuery;
   newQuery.content = [cloneDeep(searchQuery), cloneDeep(activeQuery)];
   return newQuery;
+};
+
+const getSummaryColumns = (
+  data: IStudyEntity[],
+  defaultColumns: ProColumnType<any>[],
+  columnsState?: TColumnStates,
+) => {
+  const summaryColumns: any[] = [];
+
+  (columnsState ?? defaultColumns).forEach((c, index) => {
+    let value: React.ReactNode = '';
+
+    if (c.key === 'participant_count') {
+      value = (
+        <SummarySumCell
+          title={intl.get('screen.studies.participants')}
+          sum={data.reduce((accumulator, d) => accumulator + d.participant_count, 0)}
+        />
+      );
+    } else if (c.key === 'family_count') {
+      value = (
+        <SummarySumCell
+          title={intl.get('screen.studies.families')}
+          sum={data.reduce((accumulator, d) => accumulator + (d.family_count ?? 0), 0)}
+        />
+      );
+    } else if (c.key === 'biospecimen_count') {
+      value = (
+        <SummarySumCell
+          title={intl.get('screen.studies.biospecimens')}
+          sum={data.reduce((accumulator, d) => accumulator + d.biospecimen_count, 0)}
+        />
+      );
+    }
+
+    if (columnsState) {
+      if ((c as TColumnState).visible) {
+        summaryColumns.push({ index, value });
+      }
+    } else if (!(c as ProColumnType<any>).defaultHidden) {
+      summaryColumns.push({ index, value, bordered: false });
+    }
+  });
+
+  if (summaryColumns.filter((sc) => sc.value != '').length > 0) {
+    return summaryColumns as TProTableSummary[];
+  }
+
+  return [];
 };
 
 const PageContent = ({ defaultColumns = [] }: OwnProps) => {
@@ -187,6 +243,11 @@ const PageContent = ({ defaultColumns = [] }: OwnProps) => {
             size="small"
             dataSource={data.map((i) => ({ ...i, key: i.id }))}
             dictionary={getProTableDictionary()}
+            summaryColumns={getSummaryColumns(
+              data,
+              defaultColumns,
+              userInfo?.config.study?.tables?.study?.columns,
+            )}
           />
         }
       />
